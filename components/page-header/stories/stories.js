c4e9fa7581fe99644c734d55d4ae01342bb3305e
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-08 09:59:19 +1000

    adds page header component

--- components/page-header/stories/stories.js
+++ components/page-header/stories/stories.js
@@ -24,7 +24,15 @@ storiesOf('Page Header', module)
           <h2>Page header Level 1</h2>
           <pre>
             <code>
-
+              &lt;header class=&quot;banner banner--l1&quot;&gt;
+                &lt;div class=&quot;banner__inner max&quot;&gt;
+                  &lt;a class=&quot;logo&quot; href=&quot;https://www.unimelb.edu.au&quot;&gt;&lt;img alt=&quot;The University of Melbourne homepage&quot; class=&quot;banner__logo&quot; height=&quot;117&quot; src=&quot;http://about-us-unimelb.netlify.com/images/logo-unhoused.svg&quot; width=&quot;117&quot;&gt;&lt;/a&gt;
+                  &lt;h1 class=&quot;banner__heading&quot;&gt;
+                    &lt;!-- Header Title --&gt;
+                  &lt;/h1&gt;
+                &lt;/div&gt;
+                &lt;div class=&quot;banner__img banner__aperture&quot; style=&quot;background-image: url(https://placeimg.com/990/530/arch);&quot;&gt;&lt;/div&gt;
+              &lt;/header&gt;
             </code>
           </pre>
 
@@ -32,20 +40,94 @@ storiesOf('Page Header', module)
       `
     )
     (() => {
-      // const title = text('Title', 'Example Title');
-      // const paragraph = text('Paragraph text', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias, cumque, esse incidunt consequatur, accusantium odit blanditiis ipsam dolorem repellendus ut corporis earum, illum a maiores optio voluptate dicta.');
-      // const small = boolean('Smaller width', false);
+      const title = text('Title', 'About Us');
       return {
         template: `
           <header class="banner banner--l1">
             <div class="banner__inner max">
               <a class="logo" href="https://www.unimelb.edu.au"><img alt="The University of Melbourne homepage" class="banner__logo" height="117" src="http://about-us-unimelb.netlify.com/images/logo-unhoused.svg" width="117"></a>
               <h1 class="banner__heading">
-                About us
+                ${title}
               </h1>
             </div>
             <div class="banner__img banner__aperture" style="background-image: url(https://placeimg.com/990/530/arch);"></div>
           </header>
+        `
+      }
+    }
+  ))
+  .add(
+    'Level 2',
+    withNotes(
+      `
+        <div>
+          <style>pre {  background-color: #444;  color: #eee; } </style>
+          <h2>Page header Level 2</h2>
+          <pre>
+            <code>
+              &lt;header class=&quot;banner banner--l2&quot;&gt;
+                &lt;div class=&quot;banner__inner max&quot;&gt;
+                  &lt;a class=&quot;logo&quot; href=&quot;https://www.unimelb.edu.au&quot;&gt;&lt;img alt=&quot;The University of Melbourne homepage&quot; class=&quot;banner__logo&quot; height=&quot;117&quot; src=&quot;http://about-us-unimelb.netlify.com/images/logo-unhoused.svg&quot; width=&quot;117&quot;&gt;&lt;/a&gt;
+                  &lt;h1 class=&quot;banner__heading&quot;&gt;
+                    &lt;!-- Header Title --&gt;
+                  &lt;/h1&gt;
+                &lt;/div&gt;
+                &lt;div class=&quot;banner__img banner__aperture&quot; style=&quot;background-image: url(https://placeimg.com/990/530/arch);&quot;&gt;&lt;/div&gt;
+              &lt;/header&gt;
+            </code>
+          </pre>
+
+        </div>
+      `
+    )
+    (() => {
+      const title = text('Title', 'About Us');
+      return {
+        template: `
+          <header class="banner banner--l2">
+            <div class="banner__inner max">
+              <a class="logo" href="https://www.unimelb.edu.au"><img alt="The University of Melbourne homepage" class="banner__logo" height="117" src="http://about-us-unimelb.netlify.com/images/logo-unhoused.svg" width="117"></a>
+              <h1 class="banner__heading">
+                ${title}
+              </h1>
+            </div>
+            <div class="banner__img banner__aperture" style="background-image: url(https://placeimg.com/990/530/arch);"></div>
+          </header>
+        `
+      }
+    }
+  ))
+  .add(
+    'Level 3',
+    withNotes(
+      `
+        <div>
+          <style>pre {  background-color: #444;  color: #eee; } </style>
+          <h2>Page header Level 3</h2>
+          <pre>
+            <code>
+              &lt;header class=&quot;banner banner--l3&quot;&gt;
+                &lt;div class=&quot;banner__shape&quot;&gt;&lt;/div&gt;
+                &lt;div class=&quot;banner__inner max&quot;&gt;
+                  &lt;a class=&quot;logo&quot; href=&quot;https://www.unimelb.edu.au&quot;&gt;&lt;img alt=&quot;The University of Melbourne homepage&quot; class=&quot;banner__logo&quot; height=&quot;117&quot; src=&quot;http://about-us-unimelb.netlify.com/images/logo-unhoused.svg&quot; width=&quot;117&quot;&gt;&lt;/a&gt;
+                &lt;/div&gt;
+              &lt;/header&gt;
+            </code>
+          </pre>
+
+        </div>
+      `
+    )
+    (() => {
+      const title = text('Title', 'About Us');
+      return {
+        template: `
+        <header class="banner banner--l3">
+          <div class="banner__shape"></div>
+          <div class="banner__inner max">
+            <a class="logo" href="https://www.unimelb.edu.au"><img alt="The University of Melbourne homepage" class="banner__logo" height="117" src="http://about-us-unimelb.netlify.com/images/logo-unhoused.svg" width="117"></a>
+          </div>
+        </header>
         `
       }
     }
