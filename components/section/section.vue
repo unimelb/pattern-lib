c7032c068856aee288e5193d940045ac83bbd50e
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-07 11:23:19 +1000

    adds section component and stories WIP

---
+++ components/section/section.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    <div class="section__inner max">
+      <!-- Content goes here -->
+      <slot>
+        <h3>Default Content</h3>
+        <p>this is the inner paragraph</p>
+      </slot>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Section',
+    data: {
+      alt: false,
+      inverted: false,
+    }
+  } 
+</script>
