5d3f2587c1815da8a2a581a108a68465e801cae0
Axel Bocciarelli <axelboc@gmail.com>
2017-09-08 10:46:05 +1000

    heading elements and classes

---
+++ components/typography/stories/HeadingClasses.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="c in classes" :key="c">
+      <h1 :class="c">"{{ c }}" on &lt;h1&gt;</h1>
+      <h2 :class="c">"{{ c }}" on &lt;h2&gt;</h2>
+      <h3 :class="c">"{{ c }}" on &lt;h3&gt;</h3>
+      <h4 :class="c">"{{ c }}" on &lt;h4&gt;</h4>
+      <h5 :class="c">"{{ c }}" on &lt;h5&gt;</h5>
+      <h6 :class="c">"{{ c }}" on &lt;h6&gt;</h6>
+      <h6 :class="c">"{{ c }}" on &lt;h6&gt;</h6>
+      <p :class="c">"{{ c }}" on &lt;p&gt;</p>
+      <strong :class="c">"{{ c }}" on &lt;strong&gt;</strong>
+      <span :class="c">"{{ c }}" on &lt;span&gt;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      classes: Array
+    }
+  }
+</script>
