177a9c22f7cf029219d37acb6e1d29dc91f55a5b
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-13 17:10:24 +1000

    adds a special pathfinder version of cards as its own component

---
+++ components/pathfinder/stories/pathfinder.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="section bg-inverted-dark" :style="`background-image: url(${image});`">
+    <div class="section__inner">
+      <div class="listing listing--three listing--center">
+        <ListItem>
+          <CardPathfinder title="Engagement outcomes" />
+        </ListItem>
+        <ListItem>
+          <CardPathfinder title="Study health at Melbourne" />
+        </ListItem>
+        <ListItem>
+          <CardPathfinder title="World class research" />
+        </ListItem>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import ListItem from '../../listing/stories/listitem.vue';
+  import CardPathfinder from '../../card/stories/card-pathfinder.vue';
+
+  export default {
+    name: 'Pathfinder',
+    components: {CardPathfinder, ListItem},
+    props: {
+      image: {
+        type: String,
+        default: "http://via.placeholder.com/400x200"
+      },
+      boxes: {
+        type: Object,
+        default: [{
+          title: "test"
+        }]
+      }
+    }
+  }
+</script>
