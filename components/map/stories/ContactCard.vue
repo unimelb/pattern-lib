d951bd20d2c13e8a1bca6bcf1d98bf96a742c6fc
Jason Smith <jason@waitingallday.com>
2017-09-13 11:34:22 +1000

    switch to divs and spans

--- components/map/stories/ContactCard.vue
+++ components/map/stories/ContactCard.vue
@@ -7,7 +7,7 @@
       data-height="360"
       data-zoom="17"></div>
     <div class="contact-card__lower">
-      <dl class="contact-card__col1_3" v-for="n in columns-0" :key="n" v-html="content[n-1]"></dl>
+      <div class="contact-card__col1_3" v-for="n in columns-0" :key="n" v-html="content[n-1]"></div>
     </div>
   </div>
 </template>
@@ -22,41 +22,41 @@
     data: () => {
       return {
         content: [`
-              <dt class="contact-card__title">Address</dt>
-              <dd class="contact-card__detail">
+              <div class="contact-card__title">Address</div>
+              <div class="contact-card__detail">
                 9th Floor, Raymond Priestley Building
                 <br>
                 The University of Melbourne
                 <br>
                 Victoria 3010 Australia
-              </dd>
+              </div>
 
-              <dt class="contact-card__detail--inline">Email:</dt>
-              <dd class="contact-card__detail--inline"><a href="mailto:vc@unimelb.edu.au">vc@unimelb.edu.au</a></dd>
+              <span class="contact-card__detail--inline">Email:</span>
+              <span class="contact-card__detail--inline"><a href="mailto:vc@unimelb.edu.au">vc@unimelb.edu.au</a></span>
               <br>
-              <dt class="contact-card__detail--inline">Phone:</dt>
-              <dd class="contact-card__detail--inline"><a href="tel:613834461341">+61 3 8344 6134</a></dd>
+              <span class="contact-card__detail--inline">Phone:</span>
+              <span class="contact-card__detail--inline"><a href="tel:613834461341">+61 3 8344 6134</a></span>
               <br>
-              <dt class="contact-card__detail--inline">Fax:</dt>
-              <dd class="contact-card__detail--inline"><a href="tel:61393477253">+61 3 9347 7253</a></dd>
+              <span class="contact-card__detail--inline">Fax:</span>
+              <span class="contact-card__detail--inline"><a href="tel:61393477253">+61 3 9347 7253</a></span>
         `, `
-              <dt class="contact-card__title">Executive Liaison</dt>
-              <dd class="contact-card__detail">Ms Sally Hayes</dd>
+              <div class="contact-card__title">Executive Liaison</div>
+              <div class="contact-card__detail">Ms Sally Hayes</div>
 
-              <dt class="contact-card__detail--inline">Email:</dt>
-              <dd class="contact-card__detail--inline"><a href="mailto:sally.hayes@unimelb.edu.au">sally.hayes@unimelb.edu.au</a></dd>
+              <span class="contact-card__detail--inline">Email:</span>
+              <span class="contact-card__detail--inline"><a href="mailto:sally.hayes@unimelb.edu.au">sally.hayes@unimelb.edu.au</a></span>
               <br>
-              <dt class="contact-card__detail--inline">Phone:</dt>
-              <dd class="contact-card__detail--inline"><a href="tel:61383446134">+61 3 8344 6134</a></dd>
+              <span class="contact-card__detail--inline">Phone:</span>
+              <span class="contact-card__detail--inline"><a href="tel:61383446134">+61 3 8344 6134</a></span>
         `, `
-              <dt class="contact-card__title">Executive Support Officer</dt>
-              <dd class="contact-card__detail">Ms Amelia Phipps</dd>
+              <div class="contact-card__title">Executive Support Officer</div>
+              <div class="contact-card__detail">Ms Amelia Phipps</div>
               
-              <dt class="contact-card__detail--inline">Email:</dt>
-              <dd class="contact-card__detail--inline"><a href="mailto:phipps.a@unimelb.edu.au">phipps.a@unimelb.edu.au</a></dd>
+              <span class="contact-card__detail--inline">Email:</span>
+              <span class="contact-card__detail--inline"><a href="mailto:phipps.a@unimelb.edu.au">phipps.a@unimelb.edu.au</a></span>
               <br>
-              <dt class="contact-card__detail--inline">Phone:</dt>
-              <dd class="contact-card__detail--inline"><a href="tel:61383443741">+61 3 8344 3741</a></dd>
+              <span class="contact-card__detail--inline">Phone:</span>
+              <span class="contact-card__detail--inline"><a href="tel:61383443741">+61 3 8344 3741</a></span>
         `]
       };
     }
