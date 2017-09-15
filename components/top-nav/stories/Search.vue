1672d101f808d4095215f063ba2e1c38797ef36b
Jason Smith <jason@waitingallday.com>
2017-09-15 17:16:57 +1000

    use icon vue component in footer and top-nav

--- components/top-nav/stories/Search.vue
+++ components/top-nav/stories/Search.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="header-tools__search">
     <a class="icon-link--vertical link" href="#search">
-      <svg class="icon-link__icon svg" width="24" height="24" role="presentation" focusable="false">
-        <use xlink:href="#icon-search" />
-      </svg>
+      <icon class="icon-link__icon svg" name="search" />
       <span class="icon-link__text">Search</span>
     </a>
     <div class="page-header-search">
       <form action="https://search.unimelb.edu.au" method="get">
         <fieldset class="page-header-search__inline">
           <input class="page-header-search__input" name="q" type="search" placeholder="Search the University" aria-label="Search the University" /><button class="page-header-search__submit" type="submit">
-            <svg width="21" height="21" role="presentation" focusable="false">
-              <use xlink:href="#icon-search" />
-            </svg>
+            <icon class="page-header-search__icon" name="search" />
             <span class="screenreaders-only">Go</span>
           </button>
         </fieldset>
         <a class="page-header-icon icon-link--close" href="#">
-          <svg class="page-header-search__icon" width="24" height="24" role="presentation" focusable="false">
-            <use xlink:href="#icon-close" />
-          </svg>
+          <icon class="page-header-search__icon--close" name="close" />
           Close
         </a>
       </form>
