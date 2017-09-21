164f7bb30ed58f1ff3cefdc842ea809adb61d6e1
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-21 15:07:29 +1000

    adds quotation, closes #121

---
+++ components/typography/stories/Quote.vue
@@ -0,0 +1,16 @@
+<template>
+  <blockquote class="blockquote" :class="{'blockquote--bdr-top' : bdrTop, 'blockquote--bdr-btm' : bdrBtm}">
+    <span><slot /></span>
+    <cite class="blockquote__author">{{author}}</cite>
+  </blockquote>
+</template>
+
+<script>
+  export default {
+    props: {
+      author: String,
+      bdrTop: Boolean,
+      bdrBtm: Boolean,
+    }
+  }
+</script>
