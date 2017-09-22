567c3a7708ec17e5d6fa2e20f93fc15d18a640db
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-22 15:28:49 +1000

    refactor ql-menu into component

--- components/quick-links-menu/stories/index.js
+++ components/quick-links-menu/stories/index.js
@@ -11,73 +11,63 @@ import {
   date,
 } from '@storybook/addon-knobs';
 import ButtonIcon from './../../buttons/stories/ButtonIcon.vue';
+import QuickLinks from './QuickLinks.vue';
+import QuickLinksMenuItem from './QuickLinksMenuItem.vue';
 
 storiesOf('Quick Link Menu', module)
   .addDecorator(withKnobs)
   .add(
     'Menu - Alt A', () => {
       const title = text('Title', 'Australia’s leading university');
+      const btnText = text('Button Text', 'Read more');
       const content = text('Content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur praesentium quam voluptatem qui minus, amet quidem facilis non commodi, distinctio dolore aperiam illum ut libero ad maxime dicta iure deleniti.');
       const menuLinks = array('Menu Links', ['Strategy and governance', 'Leadership', 'Working with us', 'News and resources', 'Campus and maps']);
       const versions = select('Versions', ['ql-menu--l1', 'ql-menu--l2', 'ql-menu--l3', 'ql-menu--l4'], 'ql-menu--l1');
       return {
-        components: {ButtonIcon},
+        components: { ButtonIcon, QuickLinks, QuickLinksMenuItem },
         template: `
-          <section class="section">
-            <div class="max section__inner section__inner--short ql-menu ${versions}">
-              <div class="grid">
-                <div class="cell cell--desk-2of3">
-                  <div class="ql-menu__content">
-                    <h2 class="heading-section">
-                      ${title}
-                    </h2>
-                    <p class="lead">${content}</p>
-                    <button-icon icon="chevron-right" class="btn--xwide" btnText="Welcome from the Vice-Chancellor" />
-                  </div>
-                </div>
-                <div class="cell cell--desk-1of3">
-                  <nav class="ql-menu__nav ">
-                    ${menuLinks.map(item => `<a class="ql-menu__link" href="#"><span class="push-icon">${item}<svg class="push-icon__icon" focusable="false" height="16" role="presentation" width="16"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use></svg></span></a>`  ).join('')}
-                  </nav>
-                </div>
+          <section-wrap class="bg-white">
+            <quick-links>
+              <div slot="content">
+                <h2>
+                  ${title}
+                </h2>
+                <p>${content}</p>
+                <button-icon icon="chevron-right" class="btn--xwide">${btnText}</button-icon>
               </div>
-            </div>
-          </section>
+              ${menuLinks.map(item => `<quick-links-menu-item>${item}</quick-links-menu-item>`).join('')}
+            </quick-links>
+          </section-wrap>
         `
       }
     }
-)
-.add(
-  'Menu - Alt B', () => {
-    const title = text('Title', 'Research');
-    const subTitle = text('Sub Title', 'Explore growing esteem');
-    const content = text('Content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur praesentium quam voluptatem qui minus, amet quidem facilis non commodi, distinctio dolore aperiam illum ut libero ad maxime dicta iure deleniti.');
-    const menuLinks = array('Menu Links', ['Research', 'Learning and teaching', 'Working with us', 'News and resources', 'Campus and maps']);
-    const versions = select('Versions', ['ql-menu--l1', 'ql-menu--l2', 'ql-menu--l3', 'ql-menu--l4'], 'ql-menu--l2');
-    return {
-      template: `
-        <section class="section">
-          <div class="section__inner section__inner ql-menu ${versions}">
-            <div class="grid">
-              <div class="cell cell--desk-2of3">
-                <div class="ql-menu__content">
-                  <h2 class="heading-section">
-                    ${title}
-                  </h2>
-                  <p>${content}</p>
-                </div>
+  )
+  .add(
+    'Menu - Alt B', () => {
+      const title = text('Title', 'Learning and teaching');
+      const subTitle = text('Sub Title', 'Explore growing esteem');
+      const btnText = text('Button Text', 'Read more');
+      const content = text('Content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur praesentium quam voluptatem qui minus, amet quidem facilis non commodi, distinctio dolore aperiam illum ut libero ad maxime dicta iure deleniti.');
+      const menuLinks = array('Menu Links', ['Strategy and governance', 'Leadership', 'Working with us', 'News and resources', 'Campus and maps']);
+      const level = select('level', ['ql-menu--l1', 'ql-menu--l2', 'ql-menu--l3', 'ql-menu--l4'], 'ql-menu--l2');
+      return {
+        components: { ButtonIcon, QuickLinks, QuickLinksMenuItem },
+        template: `
+          <section-wrap class="bg-white">
+            <quick-links level="${level}">
+              <div slot="content">
+                <h2>
+                  ${title}
+                </h2>
+                <p>${content}</p>
               </div>
-              <div class="cell cell--desk-1of3">
-                <nav class="ql-menu__nav">
-                  ${subTitle.length > 0 ? `<h3 class="heading-card">${subTitle}</h3>` : ''}
-                  ${menuLinks.map(item => `<a class="ql-menu__link" href="#"><span class="push-icon">${item}<svg class="push-icon__icon" focusable="false" height="16" role="presentation" width="16"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use></svg></span></a>`  ).join('')}
-                </nav>
-              </div>
-            </div>
-          </div>
-        </section>
-      `
+              ${subTitle.length > 0 ? `<h3>${subTitle}</h3>` : ''}
+              ${menuLinks.map(item => `<quick-links-menu-item>${item}</quick-links-menu-item>`).join('')}
+            </quick-links>
+          </section-wrap>
+        `
+      }
     }
-  }
-)
+  )    
+
 
