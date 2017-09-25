4b768fa67f38d01728170ca7fca81358a6abb9dd
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-25 15:43:54 +1000

    adds a doc for google fonts inclusion. closes #46

---
+++ components/typography/docs/typography.md
@@ -0,0 +1,12 @@
+# Typography
+
+
+
+
+### Including Google Fonts
+
+Google fonts should be added by including the link tag below :
+
+```html
+<link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,700%7CSource+Sans+Pro:300,300i,400,400i,600,700" rel="stylesheet">
+```
--- components/typography/stories/index.js
+++ components/typography/stories/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import { storiesOf } from '@storybook/vue';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import withReadme from 'storybook-readme/with-readme';
+
+import TypographyReadme from './../docs/typography.md';
 
 import HeadingElements from './HeadingElements.vue';
 import HeadingClasses from './HeadingClasses.vue';
@@ -13,6 +16,7 @@ import Quote from './Quote.vue';
 
 storiesOf('Typography', module)
   .addDecorator(withKnobs)
+  .addDecorator(withReadme(TypographyReadme))
   .add('Heading elements', () => ({
     components: { HeadingElements },
     template: '<heading-elements />'
