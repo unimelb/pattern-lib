78d3e567d301fc254d9f4aae81463a1b7ef9b340
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-26 16:38:39 +1000

    change svg references to embedded sprite

--- components/breadcrumbs/stories/index.js
+++ components/breadcrumbs/stories/index.js
@@ -9,10 +9,6 @@ storiesOf('Breadcrumbs', module)
       components: { Story },
       template: `
         <div>
-          <svg class="hidden" xmlns="http://www.w3.org/2000/svg">
-            <symbol id="icon-home" viewBox="5 5 38 38"><path d="M20 41V29h8v12h10V25h6L24 7 4 25h6v16h10z"></path></symbol>
-            <symbol id="icon-chevron-right" viewBox="7 3 14 14"><path d="m8,7.42l4.58,4.58l-4.58,4.59l1.41,1.41l6,-6l-6,-6l-1.41,1.42z"></path></symbol>
-          </svg>
           <Story/>
         </div>`,
     };
