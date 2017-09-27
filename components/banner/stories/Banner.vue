3d0839cffc9409b7ba139d036c941cf332bbf2d0
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-09-27 15:28:30 +1000

    refactor banner and include stories for department subheader

--- components/banner/stories/Banner.vue
+++ components/banner/stories/Banner.vue
@@ -4,18 +4,18 @@
     <div class="banner__inner max">
       <a class="link-img link-reset" href="https://www.unimelb.edu.au">
         <img alt="The University of Melbourne homepage" class="banner__logo" height="117" src="https://d2glwx35mhbfwf.cloudfront.net/v0.0.1/logo-unhoused.svg" width="117"></a>
+      <p v-if="subheading.length > 0" class="banner__sub-heading">
+        {{subheading}}
+      </p>
       <h1 class="banner__heading">
         {{title}}
       </h1>
     </div>
-    <div 
-      class="banner__img banner__aperture "
-      :style="`background-image': url('${placeholder}');`" 
-      v-bgimg="{'imgSrc': img, 'placeholder': placeholder}" 
+    <div class="banner__img banner__aperture " :style="`background-image: url('${img}');`" 
     ></div>
   </header>
 
-  <header class="banner banner--l2" :class="subheading.length > 0 ? 'banner--subheading' : ''" v-else-if="level === 2">
+  <header class="banner banner--l2" v-else-if="level === 2">
     <div class="banner__inner max">
       <a class="link-img link-reset" href="https://www.unimelb.edu.au">
         <img alt="The University of Melbourne homepage" class="banner__logo" height="117" src="https://d2glwx35mhbfwf.cloudfront.net/v0.0.2/logo-unhoused.svg" width="117">
