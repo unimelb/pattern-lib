9fdb25f1134b69bcecf50d8707694b1d15739b68
Axel Bocciarelli <axelboc@gmail.com>
2017-10-03 15:18:52 +1100

    add and expose loadScript/Stylesheet utilites

--- components/index.js
+++ components/index.js
@@ -5,8 +5,13 @@ import sprite from './icons/sprite.svg';
 import HeaderTools from './top-nav/header-tools';
 import Frdialogmodal from './modal/dialogmodal.js';
 
-import './shared';
+import * as shared './shared';
 import './progressive-image';
+
+// Expose JS utilities
+window.ui = {
+  utils: shared.utils
+};
 
 // Inject SVG sprite
 const Sprite = new Ike(sprite, version);
--- components/shared/index.js
+++ components/shared/index.js
@@ -1,1 +1,9 @@
 export logo from './logo.svg';
+
+import loadScript from './load-script';
+import loadStylesheet from './load-stylesheet';
+
+export const utils = {
+  loadScript,
+  loadStylesheet
+};
