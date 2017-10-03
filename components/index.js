00820b09a76b06e658e696c9b919a0c9b4b3e0d7
Axel Bocciarelli <axelboc@gmail.com>
2017-10-03 12:07:27 +1100

    inject SVG sprite with Ike.js

--- components/index.js
+++ components/index.js
@@ -1,12 +1,15 @@
-import svg4everbody from 'svg4everybody';
+import Ike from 'ike.js';
+
+import { version } from '../package.json';
+import sprite from './icons/sprite.svg';
 import HeaderTools from './top-nav/header-tools';
 import Frdialogmodal from './modal/dialogmodal.js';
 
-import './icons';
 import './footer';
 import './progressive-image';
 
-svg4everbody();
+// Inject SVG sprite
+const Sprite = new Ike(sprite, version);
 
 const initPage = () => {
   document.documentElement.classList.remove('no-js');
