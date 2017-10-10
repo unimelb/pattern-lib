663abd2b7252376b4955eb7c04ea48818b1f7ad6
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-11 09:57:35 +1100

    Quick fix for pathfinder
    Will need to refactor this too later

---
+++ components/cards/CardPathfinder.vue
@@ -0,0 +1,29 @@
+<template>
+  <a :href="href" class="btn-owner card card--focus-box bg-inverted card--pathfinder">
+    <icon class="card--focus-box__cnr card--focus-box__cnr--top-left" name="focus-top-left" width="54" height="54" />
+    <icon class="card--focus-box__cnr card--focus-box__cnr--btm-right" name="focus-bottom-right" width="54" height="54" />
+    <h3 class="card__header heading-section">{{ title }}</h3>
+    <div class="card__inner">
+      <slot />
+    </div>
+    <div class="card__footer">
+      <button class="btn btn--inverted btn--fullwidth">Find out more</button>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'CardPathfinder',
+  props: {
+    title: {
+      type: String,
+    },
+    href: {
+      type: String,
+      default: '#',
+    },
+  },
+};
+
+</script>
