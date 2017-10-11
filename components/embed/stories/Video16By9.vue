dc24102d9fd39cc8054a1f704828253e6cd3110a
Axel Bocciarelli <axelboc@gmail.com>
2017-10-11 15:55:21 +1100

    refactor Embed with `createStory`

---
+++ components/embed/stories/Video16By9.vue
@@ -0,0 +1,13 @@
+<template>
+  <video-embed src="https://www.youtube.com/embed/nlF7qp5GNPI" />
+</template>
+
+<script>
+import VideoEmbed from '../VideoEmbed.vue';
+import ContentBlock from '../../content-block/ContentBlock.vue';
+
+export default {
+  components: { VideoEmbed },
+  decorator: ContentBlock,
+};
+</script>
