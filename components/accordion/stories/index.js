0850d00c69e7f505931310e078730d015cf1259b
Jason Smith <jason@waitingallday.com>
2017-10-11 16:51:01 +1100

    drop non-working story for now

--- components/accordion/stories/index.js
+++ components/accordion/stories/index.js
@@ -3,11 +3,9 @@ import { createStory } from '.storybook/utils';
 
 import AccordionDefault from './AccordionDefault.vue';
 import AccordionFirstOpen from './AccordionFirstOpen.vue';
-import AccordionSoloSelect from './AccordionSoloSelect.vue';
 import AccordionNoJs from './AccordionNoJs.vue';
 
 storiesOf('Accordion', module)
   .add('Default', createStory(AccordionDefault))
   .add('First open', createStory(AccordionFirstOpen))
-  .add('Solo select', createStory(AccordionSoloSelect))
   .add('No js', createStory(AccordionNoJs));
