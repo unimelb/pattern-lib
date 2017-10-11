aaf8159c6ff97e6795464c9fbe60def95b2cfa56
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-11 16:57:11 +1100

    Adds alias for vue webpack loader

--- components/index.js
+++ components/index.js
@@ -1,12 +1,21 @@
+import Vue from 'vue';
 import Ike from 'ike.js';
 
 import { version } from '../package.json';
 import sprite from './icons/sprite.svg';
 import HeaderTools from './top-nav/header-tools';
 import Frdialogmodal from './modal/dialogmodal';
+import VueProgressiveImage from 'vue-progressive-image';
 
 import * as shared from './shared';
 import './progressive-image';
+
+const vm = new Vue({
+  el: 'main',
+});
+
+vm.use(VueProgressiveImage);
+
 
 // Expose JS utilities
 window.ui = {
@@ -15,6 +24,7 @@ window.ui = {
     new HeaderTools(); // eslint-disable-line no-new
   },
 };
+
 
 // Inject SVG sprite
 new Ike(sprite, version); // eslint-disable-line no-new
