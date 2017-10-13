856191bf5d9e7d006418d7c12118f805180adc18
Axel Bocciarelli <axelboc@gmail.com>
2017-10-13 15:41:20 +1100

    add story for video embed in content block

---
+++ components/embed/stories/VideoInContentBlock.vue
@@ -0,0 +1,14 @@
+<template>
+  <video-embed src="https://www.youtube.com/embed/nlF7qp5GNPI" />
+</template>
+
+<script>
+import VideoEmbed from '../VideoEmbed.vue';
+import ContentBlock from '../../content-block/ContentBlock.vue';
+
+export default {
+  components: { VideoEmbed },
+  decorator: ContentBlock,
+  decoratorProps: { bg: 'inverted', size: 'sml' },
+};
+</script>
