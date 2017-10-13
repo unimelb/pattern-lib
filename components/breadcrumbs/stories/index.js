6c9f4a71000e644a85ef4b915f4149578f9bf38a
Axel Bocciarelli <axelboc@gmail.com>
2017-10-13 14:12:56 +1100

    add extra long breadcrumbs story and fix flexbox wrapping

--- components/breadcrumbs/stories/index.js
+++ components/breadcrumbs/stories/index.js
@@ -4,8 +4,10 @@ import { createStory } from '.storybook/utils';
 import BreadcrumbsLevel1 from './BreadcrumbsLevel1.vue';
 import BreadcrumbsLevel2 from './BreadcrumbsLevel2.vue';
 import BreadcrumbsLevel3 from './BreadcrumbsLevel3.vue';
+import BreadcrumbsExtraLong from './BreadcrumbsExtraLong.vue';
 
 storiesOf('Breadcrumbs', module)
   .add('Level 1', createStory(BreadcrumbsLevel1))
   .add('Level 2', createStory(BreadcrumbsLevel2))
-  .add('Level 3', createStory(BreadcrumbsLevel3));
+  .add('Level 3', createStory(BreadcrumbsLevel3))
+  .add('Extra long', createStory(BreadcrumbsExtraLong));
