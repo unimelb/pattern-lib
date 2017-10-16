6582ac82a243456e93a50c54820020871603f1f1
Axel Bocciarelli <axelboc@gmail.com>
2017-10-16 16:56:48 +1100

    rename BlockQuote to BlockQuotation to remove Vue warning

---
+++ components/block-quotation/stories/index.js
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/vue';
+import { createStory } from '.storybook/utils';
+
+import BlockQuotationDefault from './BlockQuotationDefault.vue';
+import BlockQuotationWithBorders from './BlockQuotationWithBorders.vue';
+import BlockQuotationNoAuthor from './BlockQuotationNoAuthor.vue';
+import BlockQuotationAltBg from './BlockQuotationAltBg.vue';
+import BlockQuotationInverted from './BlockQuotationInverted.vue';
+import BlockQuotationInvertedWithBorders from './BlockQuotationInvertedWithBorders.vue';
+import BlockQuotationRaw from './BlockQuotationRaw.vue';
+
+storiesOf('Block Quotation', module)
+  .add('Default', createStory(BlockQuotationDefault))
+  .add('With borders', createStory(BlockQuotationWithBorders))
+  .add('Without author', createStory(BlockQuotationNoAuthor))
+  .add('Light background', createStory(BlockQuotationAltBg))
+  .add('Inverted', createStory(BlockQuotationInverted))
+  .add('Inverted with borders', createStory(BlockQuotationInvertedWithBorders))
+  .add('Raw (no classes)', createStory(BlockQuotationRaw));
