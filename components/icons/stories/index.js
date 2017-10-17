4a2dbcb758bed528c5393c7cda4b6e99f0b98159
Axel Bocciarelli <axelboc@gmail.com>
2017-10-17 16:39:49 +1100

    refactor Sprite with `createStory`

--- components/icons/stories/index.js
+++ components/icons/stories/index.js
@@ -1,12 +1,8 @@
 import { storiesOf } from '@storybook/vue';
-import withReadme from 'storybook-readme/with-readme';
+import { createStory } from '.storybook/utils';
 
 import Sprite from './Sprite.vue';
 import docs from './docs.md';
 
 storiesOf('Icons', module)
-  .addDecorator(withReadme(docs))
-  .add('Sprite', () => ({
-    components: { Sprite },
-    template: '<sprite />',
-  }));
+  .add('Sprite', createStory(Sprite, { customReadme: docs }));
