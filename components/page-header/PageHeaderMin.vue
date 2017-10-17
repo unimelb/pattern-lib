2a331acae6bef487686da7343ea1b0bc329ce730
Axel Bocciarelli <axelboc@gmail.com>
2017-10-17 14:56:10 +1100

    use bg-saw-tooth class in level-3 header

--- components/page-header/PageHeaderMin.vue
+++ components/page-header/PageHeaderMin.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="page-header page-header--l3">
-    <div class="page-header__shape"></div>
+    <div class="page-header__shape bg-saw-tooth"></div>
     <div class="page-header__inner max">
       <a class="link-img link-reset" href="https://www.unimelb.edu.au/">
         <img
