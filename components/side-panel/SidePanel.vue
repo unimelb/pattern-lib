66c16ff1e3f36c9346bcd1767b5432455db43ab3
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-18 14:26:54 +1100

    wip toggle animation

--- components/side-panel/SidePanel.vue
+++ components/side-panel/SidePanel.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="side-panel">
     <div class="side-panel__inner">
-      <h5 class="side-panel__title">Navigation <button class="side-panel__trigger" v-on:click="toggleOpen"> <SvgIcon width="10" height="10" name="close" /> </button></h5>
-      
-      <ul class="side-panel__nav" v-show="open">
+      <h5 class="side-panel__title">Navigation
+        <button class="side-panel__trigger" @click="toggleOpen">
+          <SvgIcon width="10" height="10" name="close" /> </button>
+      </h5>
+
+      <ul class="side-panel__nav" v-show="open" :class="{'side-panel__nav--closed' : !open}">
         <side-panel-nav-item :right="right" target="#section1">Section 1</side-panel-nav-item>
         <side-panel-nav-item :right="right" target="#section2">Section with a really long heading 2</side-panel-nav-item>
         <side-panel-nav-item :right="right" target="#section3">Section 3</side-panel-nav-item>
@@ -22,13 +25,16 @@ export default {
       type: Boolean,
       default: false,
     },
-    open: {
+    initialOpen: {
       type: Boolean,
       default: true,
     },
   },
+  data() {
+    return { open: this.initialOpen };
+  },
   methods: {
-    toggleOpen: function() {
+    toggleOpen() {
       this.open = !this.open;
     },
   },
