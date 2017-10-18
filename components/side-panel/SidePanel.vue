0cf163ed2b7af1ccd50465bdd6597a94c50332f0
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-18 13:04:22 +1100

    Adds hide functionality on mobile

--- components/side-panel/SidePanel.vue
+++ components/side-panel/SidePanel.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="side-panel">
     <div class="side-panel__inner">
-      <h5 class="side-panel__title">Navigation</h5>
-      <ul class="side-panel__nav">
+      <h5 class="side-panel__title">Navigation <button class="side-panel__trigger" v-on:click="toggleOpen"> <SvgIcon width="10" height="10" name="close" /> </button></h5>
+      
+      <ul class="side-panel__nav" v-show="open">
         <side-panel-nav-item :right="right" target="#section1">Section 1</side-panel-nav-item>
         <side-panel-nav-item :right="right" target="#section2">Section with a really long heading 2</side-panel-nav-item>
         <side-panel-nav-item :right="right" target="#section3">Section 3</side-panel-nav-item>
@@ -20,6 +21,15 @@ export default {
     right: {
       type: Boolean,
       default: false,
+    },
+    open: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  methods: {
+    toggleOpen: function() {
+      this.open = !this.open;
     },
   },
 };
