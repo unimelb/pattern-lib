dbdf85e8e052ddac72d94e412d0aa3668b32b2b3
Jason Smith <jason@waitingallday.com>
2017-10-19 17:25:16 +1100

    fix merge conflict deletion, add comps to lib target

--- targets/lib/index.js
+++ targets/lib/index.js
@@ -5,6 +5,8 @@ import VueProgressiveImage from 'vue-progressive-image';
 import lazyBgImg from '../../components/progressive-image';
 import { AccordionGroup, AccordionPanel } from '../../components/accordion';
 import ModalDialog from '../../components/modal';
+import PageNav from '../../components/page-nav';
+import PageSearch from '../../components/page-search';
 
 import '../../components';
 
@@ -14,6 +16,8 @@ Vue.directive('bgimg', lazyBgImg);
 Vue.component('accordion-group', AccordionGroup);
 Vue.component('accordion-panel', AccordionPanel);
 Vue.component('modal-dialog', ModalDialog);
+Vue.component('page-nav', PageNav);
+Vue.component('page-search', PageSearch);
 
 // Create Vue instance
 new Vue({ el: '#ui' });
