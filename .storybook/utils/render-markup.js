ef2bdbbb12d9b9b6a6c4354ff25fe5a92f4af649
Jason Smith <jason@waitingallday.com>
2017-10-19 11:45:33 +1100

    add pascal to kebab conversion, deal with HTML outer case

--- .storybook/utils/render-markup.js
+++ .storybook/utils/render-markup.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import pretty from 'pretty';
 
+function pascalToKebab(txt) {
+  return txt.replace(/[a-z]([A-Z])+/g, m => (`${m[0]}-${m.substring(1)}`)).toLowerCase();
+}
+
 export default function renderMarkup(Component, opts = {}) {
   // Create component instance without mounting it
   const vm = new Vue({ render: createElement => createElement(Component) });
@@ -15,38 +19,53 @@ export default function renderMarkup(Component, opts = {}) {
   if (opts.shallowRender === true) {
     /* eslint-disable no-underscore-dangle */
     for (let h = 0, rootnodes = vm._vnode.componentInstance.$children.length; h < rootnodes; h += 1) {
-      const recs = vm._vnode.componentInstance.$children[h].$options._renderChildren;
+      let outer;
+      let recs = vm._vnode.componentInstance.$children[h].$options._renderChildren;
       if (recs) {
         const componentProps = vm._vnode.componentInstance.$children[0].$vnode.componentInstance._props;
-        const outer = document.createElement(vm._vnode.componentInstance.$children[0].$vnode.componentOptions.tag);
+        if (/[A-Z]/.test(vm._vnode.componentInstance.$children[0].$vnode.componentOptions.tag)) {
+          outer = document.createElement(pascalToKebab(vm._vnode.componentInstance.$children[0].$vnode.componentOptions.tag));
+        } else {
+          outer = document.createElement(vm._vnode.componentInstance.$children[0].$vnode.componentOptions.tag);
+        }
         for (let attrNames = Object.keys(componentProps), attrVals = Object.values(componentProps), i = attrNames.length - 1; i >= 0; i -= 1) {
           if (attrVals[i]) {
             outer.setAttribute(`:${attrNames[i]}`, attrVals[i]);
           }
         }
-        for (let i = 0, max = recs.length; i < max; i += 1) {
-          if (recs[i].componentInstance && recs[i].componentInstance.$slots) {
-            // Content of slot is another component
-            const inner = document.createElement(recs[i].componentOptions.tag);
-            for (let slots = Object.values(recs[i].componentInstance.$slots), k = slots.length - 1; k >= 0; k -= 1) {
-              for (let j = slots[k].length - 1; j >= 0; j -= 1) {
-                if (slots[k][j].tag) {
-                  const slotcontent = slots[k][j].elm;
-                  if (slots[k][j].data) {
-                    slotcontent.setAttribute('slot', slots[k][j].data.slot);
-                  }
-                  inner.insertBefore(slotcontent, inner.firstChild);
+      } else {
+        // HTML outer
+        recs = vm._vnode.componentInstance._vnode.children;
+        outer = document.createElement('div');
+        outer.classList.add(vm._vnode.componentInstance._vnode.data.staticClass);
+      }
+      for (let i = 0, max = recs.length; i < max; i += 1) {
+        if (recs[i].componentInstance && recs[i].componentInstance.$slots) {
+          // Content of slot is another component
+          let inner;
+          if (/[A-Z]/.test(recs[i].componentOptions.tag)) {
+            inner = document.createElement(pascalToKebab(recs[i].componentOptions.tag));
+          } else {
+            inner = document.createElement(recs[i].componentOptions.tag);
+          }
+          for (let slots = Object.values(recs[i].componentInstance.$slots), k = slots.length - 1; k >= 0; k -= 1) {
+            for (let j = slots[k].length - 1; j >= 0; j -= 1) {
+              if (slots[k][j].tag) {
+                const slotcontent = slots[k][j].elm;
+                if (slots[k][j].data) {
+                  slotcontent.setAttribute('slot', slots[k][j].data.slot);
                 }
+                inner.insertBefore(slotcontent, inner.firstChild);
               }
             }
-            outer.appendChild(inner);
-          } else if (recs[i].tag) {
-            // Content of slot is HTML
-            outer.appendChild(recs[i].elm);
           }
+          outer.appendChild(inner);
+        } else if (recs[i].tag) {
+          // Content of slot is HTML
+          outer.appendChild(recs[i].elm);
         }
-        raw += outer.outerHTML;
       }
+      raw += outer.outerHTML;
     }
     /* eslint-enable no-underscore-dangle */
   } else {
