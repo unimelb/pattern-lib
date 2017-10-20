734c4d852efbe95b02a786a0c297c7ff0c533fba
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-20 14:13:44 +1100

    Refactor documentlist to use createStory

---
+++ components/document-list/stories/Story1.vue
@@ -0,0 +1,16 @@
+<template>
+  <section-wrap small>
+    <DownloadBlock
+      img-src="http://unsplash.it/200/300?random&gravity=center"
+      header="Annual Report"
+      btn-text="Download Item"
+    />
+  </section-wrap>
+</template>
+
+<script>
+import DownloadBlock from './../download-block.vue';
+export default {
+  components: { DownloadBlock },
+};
+</script>
