a6ee789391253481bc6a7bbf74c40659c98cdada
Jason Smith <jason@waitingallday.com>
2017-10-20 16:57:26 +1100

    add nojs variant of modal for testing

--- components/modal/ModalDialog.vue
+++ components/modal/ModalDialog.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <button class="btn fr-dialogmodal-open js-fr-dialogmodal-open" :aria-controls="`modal-dialog-1${this._uid}`">
+    <button class="btn fr-dialogmodal-open js-fr-dialogmodal-open" :aria-controls="`modal-dialog-1${this._uid}`" :disabled="disabled !== 'false' ? 'disabled' : false">
       <span class="push-icon" v-html="trigger"></span>
     </button>
-    <div class="fr-dialogmodal js-fr-dialogmodal fr-dialogmodal--is-ready" :id="`modal-dialog-1${this._uid}`" aria-hidden="true">
+    <div :class="disabled === 'false' ? 'fr-dialogmodal js-fr-dialogmodal fr-dialogmodal--is-ready' : false" :id="`modal-dialog-1${this._uid}`" :aria-hidden="(disabled === 'false')">
       <div class="fr-dialogmodal-modal js-fr-dialogmodal-modal" :aria-labelledby="`modal-dialog-title-1${this._uid}`" role="dialog">
         <div role="document">
           <h2 :id="`modal-dialog-title-1${this._uid}`" v-html="title"></h2>
           <slot></slot>
           <br>
-          <button class="fr-dialogmodal-close js-fr-dialogmodal-close" aria-label="Close Dialog" type="button">&#x2715;</button>
+          <button v-if="disabled === 'false'" class="fr-dialogmodal-close js-fr-dialogmodal-close" aria-label="Close Dialog" type="button">&#x2715;</button>
         </div>
       </div>
     </div>
@@ -22,9 +22,19 @@ import Frdialogmodal from './_fr-dialogmodal';
 export default {
   name: 'modal-dialog',
   props: {
+    disabled: {
+      type: String,
+      default: 'false',
+    },
     title: String,
     trigger: String,
   },
-  mounted: Frdialogmodal,
+  mounted() {
+    /* eslint-disable no-underscore-dangle */
+    if (this._props.disabled !== 'true') {
+      Frdialogmodal();
+    }
+    /* eslint-enable no-underscore-dangle */
+  },
 };
 </script>
