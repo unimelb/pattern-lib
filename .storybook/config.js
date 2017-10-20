2e03e331b00ad0778db49f9e02a9a70909e86bd6
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-20 15:35:20 +1100

    refactor listing to use createStory

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,7 +6,7 @@ import VueProgressiveImage from 'vue-progressive-image';
 import { storyDecorator } from './utils';
 import SvgIcon from 'icons/SvgIcon.vue';
 import SectionWrap from '../components/section/SectionWrap.vue';
-import ListItem from '../components/listing/stories/listitem.vue';
+import ListItem from '../components/listing/ListItem.vue';
 import ButtonIcon from '../components/buttons/ButtonIcon.vue';
 
 import { version } from '../package.json';
---
+++ components/listing/ListingWrap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="classes">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cols: String,
+    center: Boolean,
+  },
+  computed: {
+    classes() {
+      return {
+        listing: true,
+        'listing--center': this.center,
+        [`listing--${this.cols}`]: ['one', 'two', 'three', 'four'].includes(this.cols),
+      };
+    },
+  },
+};
+</script>
