5fe52f67d648ed07df8dce5d05fa12fa0458a384
Jason Smith <jason@waitingallday.com>
2017-10-23 12:02:08 +1100

    modify underlying code to deal with vue root element

--- components/accordion/stories/index.js
+++ components/accordion/stories/index.js
@@ -3,9 +3,11 @@ import { createStory } from '.storybook/utils';
 
 import AccordionDefault from './AccordionDefault.vue';
 import AccordionFirstOpen from './AccordionFirstOpen.vue';
+import AccordionSoloSelect from './AccordionSoloSelect.vue';
 import AccordionNoJs from './AccordionNoJs.vue';
 
 storiesOf('Accordion', module)
   .add('Default', createStory(AccordionDefault, { shallowRender: true }))
-  .add('First open', createStory(AccordionFirstOpen, { shallowRender: true }))
+  .add('First panel open', createStory(AccordionFirstOpen, { shallowRender: true }))
+  .add('Single panel open at a time', createStory(AccordionSoloSelect, { shallowRender: true }))
   .add('No js', createStory(AccordionNoJs, { shallowRender: true }));
