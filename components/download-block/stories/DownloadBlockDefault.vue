96dc1882f3b1357874772beebf6509110ca048b0
Axel Bocciarelli <axelboc@gmail.com>
2017-10-23 13:29:10 +1100

    move Document Block into its own folder + refactor its props and styles

---
+++ components/download-block/stories/DownloadBlockDefault.vue
@@ -0,0 +1,22 @@
+<template>
+  <DownloadBlock
+    href=""
+    header="Annual Report"
+    img="http://unsplash.it/200/300"
+    btn-text="Download the 2017 Annual Report"
+    filename="annual-report.pdf"
+    size="2.2 MB"
+    format="PDF"
+  />
+</template>
+
+<script>
+import DownloadBlock from '../DownloadBlock.vue';
+import SectionWrap from '../../section/SectionWrap.vue';
+
+export default {
+  components: { DownloadBlock },
+  decorator: SectionWrap,
+  decoratorProps: { small: true },
+};
+</script>
---
+++ components/download-block/stories/index.js
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/vue';
+import { createStory } from '.storybook/utils';
+
+import DownloadBlockDefault from './DownloadBlockDefault.vue';
+
+storiesOf('Download Block', module)
+  .add('Default', createStory(DownloadBlockDefault));
