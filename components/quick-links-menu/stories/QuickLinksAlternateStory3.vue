c6276247135cb0dd9974b8eacb400e94f966dddd
Jason Smith <jason@waitingallday.com>
2017-10-23 14:43:11 +1100

    refactor quicklinks with createStory

---
+++ components/quick-links-menu/stories/QuickLinksAlternateStory3.vue
@@ -0,0 +1,33 @@
+<template>
+  <SectionWrap class="bg-white">
+    <QuickLinks level="ql-menu--l4">
+      <div slot="content">
+        <h2>Learning and teaching</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur praesentium quam voluptatem qui minus, amet quidem facilis non commodi, distinctio dolore aperiam illum ut libero ad maxime dicta iure deleniti.</p>
+        <ButtonIcon icon="chevron-right" class="btn--xwide">Read more</ButtonIcon>
+      </div>
+      <h3>Explore growing esteem</h3>
+      <QuickLinksMenuItem href="#" v-for="item in menuLinks" :key="item" v-html="item" />
+    </QuickLinks>
+  </SectionWrap>
+</template>
+
+<script>
+import QuickLinks from './QuickLinks.vue';
+import QuickLinksMenuItem from './QuickLinksMenuItem.vue';
+
+export default {
+  components: { QuickLinks, QuickLinksMenuItem },
+  data() {
+    return {
+      menuLinks: [
+        'Strategy and governance',
+        'Leadership',
+        'Working with us',
+        'News and resources',
+        'Campus and maps',
+      ],
+    };
+  },
+};
+</script>
--- components/quick-links-menu/stories/index.js
+++ components/quick-links-menu/stories/index.js
@@ -1,55 +1,13 @@
 import { storiesOf } from '@storybook/vue';
-import { withKnobs, text, array, select } from '@storybook/addon-knobs';
-import QuickLinks from './QuickLinks.vue';
-import QuickLinksMenuItem from './QuickLinksMenuItem.vue';
+import { createStory } from '.storybook/utils';
+
+import QuickLinksDefault from './QuickLinksDefault.vue';
+import QuickLinksAlternateStory1 from './QuickLinksAlternateStory1.vue';
+import QuickLinksAlternateStory2 from './QuickLinksAlternateStory2.vue';
+import QuickLinksAlternateStory3 from './QuickLinksAlternateStory3.vue';
 
 storiesOf('Quick Link Menu', module)
-  .addDecorator(withKnobs)
-  .add('Menu - Alt A', () => {
-    const title = text('Title', 'Australia’s leading university');
-    const btnText = text('Button Text', 'Read more');
-    const content = text('Content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur praesentium quam voluptatem qui minus, amet quidem facilis non commodi, distinctio dolore aperiam illum ut libero ad maxime dicta iure deleniti.');
-    const menuLinks = array('Menu Links', ['Strategy and governance', 'Leadership', 'Working with us', 'News and resources', 'Campus and maps']);
-    return {
-      components: { QuickLinks, QuickLinksMenuItem },
-      template: `
-        <SectionWrap class="bg-white">
-          <quick-links>
-            <div slot="content">
-              <h2>
-                ${title}
-              </h2>
-              <p>${content}</p>
-              <ButtonIcon icon="chevron-right" class="btn--xwide">${btnText}</ButtonIcon>
-            </div>
-            ${menuLinks.map(item => `<quick-links-menu-item href="#">${item}</quick-links-menu-item>`).join('')}
-          </quick-links>
-        </SectionWrap>
-      `,
-    };
-  })
-  .add('Menu - Alt B', () => {
-    const title = text('Title', 'Learning and teaching');
-    const subTitle = text('Sub Title', 'Explore growing esteem');
-    const content = text('Content', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur praesentium quam voluptatem qui minus, amet quidem facilis non commodi, distinctio dolore aperiam illum ut libero ad maxime dicta iure deleniti.');
-    const menuLinks = array('Menu Links', ['Strategy and governance', 'Leadership', 'Working with us', 'News and resources', 'Campus and maps']);
-    const level = select('level', ['ql-menu--l1', 'ql-menu--l2', 'ql-menu--l3', 'ql-menu--l4'], 'ql-menu--l2');
-    return {
-      components: { QuickLinks, QuickLinksMenuItem },
-      template: `
-        <SectionWrap class="bg-white">
-          <quick-links level="${level}">
-            <div slot="content">
-              <h2>
-                ${title}
-              </h2>
-              <p>${content}</p>
-            </div>
-            ${subTitle.length > 0 ? `<h3>${subTitle}</h3>` : ''}
-            ${menuLinks.map(item => `<quick-links-menu-item href="#">${item}</quick-links-menu-item>`).join('')}
-          </quick-links>
-        </SectionWrap>
-      `,
-    };
-  });
-
+  .add('Default', createStory(QuickLinksDefault))
+  .add('Alternate B', createStory(QuickLinksAlternateStory1))
+  .add('Alternate C', createStory(QuickLinksAlternateStory2))
+  .add('Alternate D', createStory(QuickLinksAlternateStory3));
