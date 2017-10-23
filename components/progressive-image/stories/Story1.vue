790c29bfcb94d1d496fa8d3fa1b926c264081806
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-23 14:28:57 +1100

    Refactor Progressive Images

---
+++ components/progressive-image/stories/Story1.vue
@@ -0,0 +1,14 @@
+<template>
+  <section-wrap small progressive bg-image="https://upload.wikimedia.org/wikipedia/commons/6/62/Starsinthesky.jpg">
+    <CardFocusBox>
+      <h3 class="heading-section">Heading</h3>
+      <p>Rem minus et modi laborum. Maxime modi consequatur ut voluptas qui quidem voluptatum et. Veritatis excepturi velit repellat iusto consequatur ut et labore.</p>
+    </CardFocusBox>
+  </section-wrap>
+</template>
+<script>
+import CardFocusBox from './../../cards/CardFocusBox.vue';
+export default {
+  components: { CardFocusBox },
+};
+</script>
--- components/progressive-image/stories/index.js
+++ components/progressive-image/stories/index.js
@@ -1,65 +1,11 @@
 import { storiesOf } from '@storybook/vue';
-import {
-  withKnobs,
-  text,
-} from '@storybook/addon-knobs';
-import VueProgressiveImage from 'vue-progressive-image';
-import withReadme from 'storybook-readme/with-readme';
+import { createStory } from '.storybook/utils';
 
-import prgimgReadme from './progressive-image.md';
+import Story1 from './Story1.vue';
+import Story2 from './Story2.vue';
 import prgbgReadme from './progressive-background.md';
-import lazybgReadme from './lazybg-directive.md';
+import prgimgReadme from './progressive-image.md';
 
-storiesOf('Progressive Images', module)
-  .addDecorator(withKnobs)
-  .add('Background images - component', withReadme(prgimgReadme, () => {
-    const title = text('Title', 'Using component');
-    const btnText = text('Button Text', 'Example Title');
-
-    return {
-      template: `
-      <progressive-background no-ratio class="section section--image bg-inverted" src="https://upload.wikimedia.org/wikipedia/commons/6/62/Starsinthesky.jpg" placeholder="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Starsinthesky.jpg/220px-Starsinthesky.jpg">
-        <div class="section__inner section__inner--sml">
-          <div class="card card--focus-box card--focus-box--loose bg-white">
-            <SvgIcon class="card--focus-box__cnr card--focus-box__cnr--top-left" width="54px" height="54px" name="focus-top-left" />
-            <SvgIcon class="card--focus-box__cnr card--focus-box__cnr--btm-right" width="54px" height="54px" name="focus-bottom-right" />
-            <div class="card__inner ">
-              <h3 class="heading-section">${title}</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-              <ButtonIcon>${btnText}</ButtonIcon>
-            </div>
-          </div>
-        </div>
-      </progressive-background>`,
-    };
-  }))
-  .add('Background images - directive', withReadme(lazybgReadme, () => {
-    const title = text('Title', 'Using directive');
-    const btnText = text('Button Text', 'Example Title');
-
-    return {
-      template: `
-          <section class="section section--image bg-inverted " v-bgimg="{imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/6/62/Starsinthesky.jpg', placeholder: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Starsinthesky.jpg/220px-Starsinthesky.jpg'}">
-            <div class="section__inner section__inner--sml">
-              <div class="card card--focus-box card--focus-box--loose bg-white">
-                <SvgIcon class="card--focus-box__cnr card--focus-box__cnr--top-left" width="54px" height="54px" name="focus-top-left" />
-                <SvgIcon class="card--focus-box__cnr card--focus-box__cnr--btm-right" width="54px" height="54px" name="focus-bottom-right" />
-                <div class="card__inner ">
-                  <h3 class="heading-section">${title}</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                  <ButtonIcon>${btnText}</ButtonIcon>
-                </div>
-              </div>
-            </div>
-          </section>
-        `,
-    };
-  }))
-  .add('Progressive image component', withReadme(prgbgReadme, () => ({
-    components: { VueProgressiveImage },
-    template: `
-          <section-wrap small>
-            <progressive-img src="https://upload.wikimedia.org/wikipedia/commons/6/62/Starsinthesky.jpg" />
-          </section-wrap>
-        `,
-  })));
+storiesOf('Images / Progressive', module)
+  .add('With Progressive Background Component', createStory(Story1, { customReadme: prgbgReadme }))
+  .add('With Progressive Image Component', createStory(Story2, { customReadme: prgimgReadme }));
