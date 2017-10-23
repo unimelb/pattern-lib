ff1abf47d3921c3dcddefca592511ec28c07adac
Jason Smith <jason@waitingallday.com>
2017-10-23 15:27:42 +1100

    refactor tables to use createStory

---
+++ components/tables/stories/TableStatsWrapped.vue
@@ -0,0 +1,251 @@
+<template>
+  <div class="content-block">
+    <table class="table table--striped" id="statstable">
+        <thead>
+          <tr class="header">
+            <th>
+              Independently published global and program rankings
+            </th>
+            <th class="center">
+              Australia ranking
+            </th>
+            <th class="center">
+              World ranking
+            </th>
+          </tr>
+          <tr class="blank">
+            <th>
+              <a>Times Higher Education World University Rankings 2016&nbsp;– 2017</a>
+            </th>
+            <th class="center">
+              1
+            </th>
+            <th class="center">
+              33
+            </th>
+          </tr>
+          <tr class="blank">
+            <th>
+              <a>Academic Ranking of World Universities</a>
+            </th>
+            <th class="center">
+              1
+            </th>
+            <th class="center">
+              40
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="subheader">
+            <th colspan="3">
+              <a>QS World University Rankings</a>
+            </th>
+          </tr>
+          <tr>
+            <td>
+              Academic reputation
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              17
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Employer reputation
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              11
+            </td>
+          </tr>
+          <tr class="subheader">
+            <th colspan="3">
+              <a>QS World University Rankings (by subject)</a>
+            </th>
+          </tr>
+          <tr>
+            <td>
+              Linguistics
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              24
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Performing Arts
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              26
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Computer Science and Information Systems
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              14
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Electrical Engineering
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              35
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Mechanical Engineering
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              44
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Biological Sciences
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              23
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Dentistry
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              28
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Chemistry
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              45
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Physics
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              48
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Business and Management Studies
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              22
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Education
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              5
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Law
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              11
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Economics and Econometrics
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              25
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Statistics and Operational Research
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              30
+            </td>
+          </tr>
+          <tr class="highlight">
+            <td>
+              Times Higher Education World Reputation Rankings 2016
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              49
+            </td>
+          </tr>
+          <tr class="highlight">
+            <td>
+              US News Best Global Universities
+            </td>
+            <td class="center">
+              1
+            </td>
+            <td class="center">
+              36
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
