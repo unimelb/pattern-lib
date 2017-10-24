405265378f13953d10608fbaafa4f1eb73135f2f
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-24 14:52:25 +1100

    Adds image listing for brand hub story
    Also fixes flex bug on focus image listing

---
+++ components/cards/stories/ImageList/Story5.vue
@@ -0,0 +1,70 @@
+<template>
+  <SectionWrap class="bg-alt">
+    <div class="listing listing--four">
+      <ListItem v-for="item in items" :key="item">
+        <CardImageList
+          :border="false"
+          thumb="https://dummyimage.com/300x200.jpg"
+          :title="item.title"
+          :meta="item.meta"
+          >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim accusamus dolorum labore, odit libero maxime nam inventore neque ratione optio quos sapiente atque ex fuga blanditiis alias, ducimus ut? Voluptatibus.
+        </CardImageList>
+      </ListItem>
+    
+    </div>
+  </SectionWrap>
+</template>
+
+<script>
+import CardImageList from './../../CardImageList.vue';
+
+export default {
+  components: { CardImageList },
+  data: () => ({
+    items: [
+      {
+        title: 'Online synthesizing',
+        meta: 'copy communities metrics',
+      },
+      {
+        title: 'Visionary connecting Movies Designer microchip',
+        meta: 'support methodologies leverage',
+      },
+      {
+        title: 'paradigms Response revolutionary JSON',
+        meta: 'Borders Tasty Shoes',
+      },
+      {
+        title: 'Fuga debitis consequuntur.',
+        meta: 'Enim explicabo fuga.',
+      },
+      {
+        title: 'Molestiae est consequatur autem animi et rerum.',
+        meta: 'Sint animi quisquam.',
+      },
+      {
+        title: 'Sed quisquam tempore minima ut omnis molestiae.',
+        meta: 'Enim et tempore.',
+      },           
+      {
+        title: 'Similique molestiae doloremque perferendis nihil.',
+        meta: 'Consequatur beatae dolores pariatur dolorum natus voluptas molestiae iste.',
+      },
+      {
+        title: 'Laborum eligendi et.',
+        meta: 'Quis rerum aliquam.',
+      },
+      {
+        title: 'Itaque sunt aut quibusdam non.',
+        meta: 'Autem sint voluptatem quia excepturi fuga molestias fugiat.',
+      },
+      {
+        title: 'Ut in deserunt atque quis dolorem.',
+        meta: 'Distinctio quam voluptatem.',
+      },
+    ],
+  }),
+};
+
+</script>
