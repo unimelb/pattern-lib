034918cac8fdfe2a75a5095142e7651c0278e5dd
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-26 10:28:18 +1100

    Reverts #403 and adds list-spaced helper instead

---
+++ components/typography/stories/ListsSpaced.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ol class="list-spaced">
+      <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A quam libero dolore sunt cum blanditiis, laborum modi natus. Nemo reprehenderit architecto dolores repudiandae laudantium consequuntur atque consequatur qui praesentium. Itaque.</li>
+      <li>
+        Iure, excepturi. Soluta:
+        <ul>
+          <li>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            <ol>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                <ol>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+                </ol>
+              </li>
+              <li>Error iure velit quo temporibus eos debitis tempora, pariatur impedit. Necessitatibus facilis doloremque ipsum quos perferendis unde veniam facere accusamus minima pariatur, dolor ipsam exercitationem dolore labore debitis totam nostrum?</li>
+            </ol>
+          </li>
+          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil eveniet rerum iste ipsum hic corporis dolor, vel ex, iusto architecto sed tempora, modi dolore suscipit saepe! Odit perspiciatis in culpa!</li>
+        </ul>
+      </li>
+      <li>Cum omnis veritatis molestias mollitia dolorem reiciendis vero quidem, labore asperiores, dolore veniam est optio aspernatur et quibusdam fugit, porro excepturi blanditiis ratione tenetur. Distinctio ullam in accusamus quibusdam repellendus!</li>
+    </ol>
+    <ul class="list-spaced">
+      <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A quam libero dolore sunt cum blanditiis, laborum modi natus. Nemo reprehenderit architecto dolores repudiandae laudantium consequuntur atque consequatur qui praesentium. Itaque.</li>
+      <li>
+        Iure, excepturi. Soluta:
+        <ol>
+          <li>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            <ul>
+              <li>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                <ul>
+                  <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+                </ul>
+              </li>
+              <li>Error iure velit quo temporibus eos debitis tempora, pariatur impedit. Necessitatibus facilis doloremque ipsum quos perferendis unde veniam facere accusamus minima pariatur, dolor ipsam exercitationem dolore labore debitis totam nostrum?</li>
+            </ul>
+          </li>
+          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil eveniet rerum iste ipsum hic corporis dolor, vel ex, iusto architecto sed tempora, modi dolore suscipit saepe! Odit perspiciatis in culpa!</li>
+        </ol>
+      </li>
+      <li>Cum omnis veritatis molestias mollitia dolorem reiciendis vero quidem, labore asperiores, dolore veniam est optio aspernatur et quibusdam fugit, porro excepturi blanditiis ratione tenetur. Distinctio ullam in accusamus quibusdam repellendus!</li>
+    </ul>
+  </div>
+</template>
