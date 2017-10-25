938bf2a0d601f15c5930f95105ea88c51dc57e48
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-25 17:21:36 +1100

    Section with 2 columns story

---
+++ components/section/SectionTwoCol.vue
@@ -0,0 +1,33 @@
+<template>
+  <SectionWrap>
+    <ListingWrap v-if="direction === 'left'">
+      <ListItem cols="2of3">
+        <slot name="main"></slot>
+      </ListItem>
+      <ListItem cols="1of3">
+        <slot name="side"></slot>
+      </ListItem>
+    </ListingWrap>
+    <ListingWrap v-if="direction === 'right'">
+      <ListItem cols="1of3">
+        <slot name="side"></slot>
+      </ListItem>
+      <ListItem cols="2of3">
+        <slot name="main"></slot>
+      </ListItem>
+    </ListingWrap>
+  </SectionWrap>
+</template>
+
+<script>
+import ListingWrap from './../listing/ListingWrap.vue';
+export default {
+  components: { ListingWrap },
+  props: {
+    direction: {
+      type: String,
+      default: 'left',
+    },
+  },
+};
+</script>
---
+++ components/section/stories/SectionTwo/Story1.vue
@@ -0,0 +1,24 @@
+<template>
+  <SectionTwoCol direction="left">
+    <div slot="main">
+      <h3 class="heading-section">Doloribus in mollitia.</h3>
+      <p>Qui rem enim alias accusantium ut nostrum et iste. A quo voluptatem in culpa repudiandae facere. Veritatis magnam omnis distinctio ea modi. Ipsam aperiam corporis dolorum.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsa neque nemo, nam veritatis quasi ea perspiciatis laudantium aliquid alias velit sed quam tenetur dolorum, nihil quo pariatur cum commodi?</p>  
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut numquam ex perspiciatis harum amet laudantium sint error vitae exercitationem sapiente a nisi debitis ipsum sed voluptates natus, esse aut modi.
+    </div>
+    <div slot="side">
+      <FigureWrap>
+        <img src="https://dummyimage.com/300x200.jpg" alt="">
+      </FigureWrap>
+      <p>Qui rerum laudantium quis hic tempora. Quia suscipit placeat est illo omnis nemo fuga. Nobis suscipit sint odit quidem sit et sed deleniti.</p>
+    </div>
+  </SectionTwoCol>
+</template>
+
+<script>
+import SectionTwoCol from './../../SectionTwoCol.vue';
+import FigureWrap from './../../../figure/FigureWrap.vue';
+export default {
+  components: { SectionTwoCol, FigureWrap },
+};
+</script>
