32c414cb5d153ad0911826d76ea7e625274e6be7
Jason Smith <jason@waitingallday.com>
2017-10-26 15:26:19 +1100

    refactor down to what is actually used

--- components/page/tracking.js
+++ components/page/tracking.js
@@ -1,24 +1,9 @@
 import { version } from '../../package.json';
 
-export default function () {
-  // === GTM ===
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    'gtm.start': new Date().getTime(),
-    event: 'gtm.js',
-  });
-
-  // === Tealium ===
+export default function initTracking() {
   window.utag_data = window.utag_data || {};
   window.utag_data.uom_version = version;
-
-  // Load GTM and Tealium scripts
-  [
-    'https://www.googletagmanager.com/gtm.js?id=GTM-7JB9',
-    'https://tags.tiqcdn.com/utag/unimelb/main/prod/utag.js',
-  ].forEach((src) => {
-    const script = document.createElement('script');
-    script.src = src;
-    document.head.appendChild(script);
-  });
+  const script = document.createElement('script');
+  script.src = 'https://tags.tiqcdn.com/utag/unimelb/main/prod/utag.js';
+  document.head.appendChild(script);
 }
