42d88e331cc9d4dff7df2957885f840c348a5987
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-10-27 16:55:48 +1100

    adds alert messages component

---
+++ components/alert/stories/AlertSuccess.vue
@@ -0,0 +1,11 @@
+<template>
+  <Alert level="success"><span class="text-bold">Alert !</span> Better check yourself before you wreck yourself</Alert>
+</template>
+
+<script>
+import Alert from '../Alert.vue';
+
+export default {
+  components: { Alert },
+};
+</script>
---
+++ components/alert/stories/AlertWarning.vue
@@ -0,0 +1,11 @@
+<template>
+  <Alert level="warning"><span class="text-bold">Alert !</span>Better check yourself before you wreck yourself</Alert>
+</template>
+
+<script>
+import Alert from '../Alert.vue';
+
+export default {
+  components: { Alert },
+};
+</script>
