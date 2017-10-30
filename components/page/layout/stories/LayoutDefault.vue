df087115207d0f5ffe8670fa6df99f999724bb6f
Axel Bocciarelli <axelboc@gmail.com>
2017-10-30 14:55:43 +1100

    create SFC story for layout and use `PadDecorator`

---
+++ components/page/layout/stories/LayoutDefault.vue
@@ -0,0 +1,11 @@
+<template>
+  <p class="heading-card text-italic">See README panel</p>
+</template>
+
+<script>
+import PadDecorator from '.storybook/decorators/PadDecorator.vue';
+
+export default {
+  decorator: PadDecorator,
+};
+</script>
