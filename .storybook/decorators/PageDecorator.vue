691d30d16dc80f674dd188498730d5a78962bdd5
Axel Bocciarelli <axelboc@gmail.com>
2017-10-30 16:46:39 +1100

    abstract out PageDecorator for Side Panel stories

---
+++ .storybook/decorators/PageDecorator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <PageHeader :level="2" title="Strategy and governance" img="https://placeimg.com/990/530/arch/1" />
+    <PageBreadcrumbs :items="breadcrumbsItems" />
+    <slot></slot>
+    <PageFooter />
+  </div>
+</template>
+
+<script>
+import PageHeader from '../../components/page/header/PageHeader.vue';
+import PageFooter from '../../components/page/footer/PageFooter.vue';
+import PageBreadcrumbs from '../../components/page/breadcrumbs/PageBreadcrumbs.vue';
+
+export default {
+  components: { PageHeader, PageFooter, PageBreadcrumbs },
+  data: () => ({
+    breadcrumbsItems: [
+      { href: '/', text: 'About us' },
+      { href: '/strategy', text: 'Strategy and governance' },
+    ],
+  }),
+};
+</script>
+
