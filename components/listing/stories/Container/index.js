1bffed69fd4c359f6ff66da51daaeba6c895c8ac
Axel Bocciarelli <axelboc@gmail.com>
2017-10-30 16:27:13 +1100

    use `SectionWrap` as decorator in Listing Container stories

--- components/listing/stories/Container/index.js
+++ components/listing/stories/Container/index.js
@@ -7,10 +7,6 @@ import Story3 from './Story3.vue';
 import Story4 from './Story4.vue';
 
 storiesOf('Listing/Container', module)
-  .addDecorator(story => ({
-    components: { story },
-    template: '<section-wrap><story/></section-wrap>',
-  }))
   .add('1 Column', createStory(Story1))
   .add('2 Column', createStory(Story2))
   .add('3 Column', createStory(Story3))
