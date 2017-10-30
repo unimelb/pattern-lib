59a4a10a459b54ccc1f3ad708430724ec27b6214
Axel Bocciarelli <axelboc@gmail.com>
2017-10-30 11:41:08 +1100

    switch to static exports

--- targets/vue/index.js
+++ targets/vue/index.js
@@ -1,14 +1,48 @@
-/*  eslint-disable */
-import VueProgressiveImage from 'vue-progressive-image';
-
-const componentsCtx = require.context(
-  '../../components/', true,
-  /^\.\/[a-z0-9-]+\/[A-Za-z1-9]+\.vue$/
-);
-
-const components = componentsCtx.keys().reduce((exp, id) => {
-  exp[id] = componentsCtx(id);
-  return exp;
-}, { ProgressiveImage: VueProgressiveImage });
-
-module.exports = components;
+export { default as ProgressiveImage } from 'vue-progressive-image';
+export { default as AccordionGroup } from '../../components/accordion/AccordionGroup.vue';
+export { default as AccordionPanel } from '../../components/accordion/AccordionPanel.vue';
+export { default as ArticleWrap } from '../../components/article-wrap/ArticleWrap.vue';
+export { default as BlockQuotation } from '../../components/block-quotation/BlockQuotation.vue';
+export { default as ButtonIcon } from '../../components/buttons/ButtonIcon.vue';
+export { default as CardBioHeader } from '../../components/cards/CardBioHeader.vue';
+export { default as CardDivision } from '../../components/cards/CardDivision.vue';
+export { default as CardDivisionHead } from '../../components/cards/CardDivisionHead.vue';
+export { default as CardFacts } from '../../components/cards/CardFacts.vue';
+export { default as CardFocusBox } from '../../components/cards/CardFocusBox.vue';
+export { default as CardFocusImageList } from '../../components/cards/CardFocusImageList.vue';
+export { default as CardImage } from '../../components/cards/CardImage.vue';
+export { default as CardImageList } from '../../components/cards/CardImageList.vue';
+export { default as CardLink } from '../../components/cards/CardLink.vue';
+export { default as CardNews } from '../../components/cards/CardNews.vue';
+export { default as CardPathfinder } from '../../components/cards/CardPathfinder.vue';
+export { default as Cards } from '../../components/cards/Cards.vue';
+export { default as CardStaffList } from '../../components/cards/CardStaffList.vue';
+export { default as CheckList } from '../../components/check-list/CheckList.vue';
+export { default as ContactList } from '../../components/contact-list/ContactList.vue';
+export { default as ContentBlock } from '../../components/content-block/ContentBlock.vue';
+export { default as DownloadBlock } from '../../components/download-block/DownloadBlock.vue';
+export { default as SoundcloudEmbed } from '../../components/embed/SoundcloudEmbed.vue';
+export { default as VideoEmbed } from '../../components/embed/VideoEmbed.vue';
+export { default as FigureWrap } from '../../components/figure/FigureWrap.vue';
+export { default as SvgIcon } from '../../components/icons/SvgIcon.vue';
+export { default as ListingWrap } from '../../components/listing/ListingWrap.vue';
+export { default as ListItem } from '../../components/listing/ListItem.vue';
+export { default as ModalDialog } from '../../components/modal/ModalDialog.vue';
+export { default as PageBreadcrumbs } from '../../components/page/breadcrumbs/PageBreadcrumbs.vue';
+export { default as PageFooter } from '../../components/page/footer/PageFooter.vue';
+export { default as PageHeader } from '../../components/page/header/PageHeader.vue';
+export { default as PageNav } from '../../components/page/nav/PageNav.vue';
+export { default as PageSearch } from '../../components/page/search/PageSearch.vue';
+export { default as Pathfinder } from '../../components/pathfinder/Pathfinder.vue';
+export { default as SectionDivider } from '../../components/section/SectionDivider.vue';
+export { default as SectionTwoCol } from '../../components/section/SectionTwoCol.vue';
+export { default as SectionWrap } from '../../components/section/SectionWrap.vue';
+export { default as SplitSection } from '../../components/section/SplitSection.vue';
+export { default as SidePanel } from '../../components/side-panel/SidePanel.vue';
+export { default as SidePanelNavItem } from '../../components/side-panel/SidePanelNavItem.vue';
+export { default as SocialList } from '../../components/social-list/SocialList.vue';
+export { default as StatsGroup } from '../../components/stats/StatsGroup.vue';
+export { default as StatsItem } from '../../components/stats/StatsItem.vue';
+export { default as SublinkMenu } from '../../components/sublink-menu/SublinkMenu.vue';
+export { default as SublinkMenuItem } from '../../components/sublink-menu/SublinkMenuItem.vue';
+export { default as Welcome } from '../../components/welcome/Welcome.vue';
