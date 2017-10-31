d863318c4c72f7b52cc9e98a9d43681ffc7e4b34
Jason Smith <jason@waitingallday.com>
2017-10-31 15:06:44 +1100

    add timeline component styles, add fill container variant for figure, add a grey between light and mid

---
+++ components/timeline/stories/Story1.vue
@@ -0,0 +1,68 @@
+<template>
+  <dl class="timeline">
+    <dt class="timeline__header">
+      <strong>1505-10</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        The Rothschild Prayer Book is produced in the Ghent-Bruges area of the Netherlands. It is probably bought on the market and not commissioned, the original owner is not known.
+      </p>
+    </dd>
+    <dt class="timeline__header">
+      <strong>1872</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        The Prayer Book appears in a catalogue of the collection of Anselm von Rothschild
+      </p>
+    </dd>
+    <dt class="timeline__header">
+      <strong>1938</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        It is confiscated by the Nazis from his descendant Alphonse von Rothschild in the annexation of Austria.
+      </p>
+    </dd>
+    <dt class="timeline__header">
+      <strong>After the end of WW2</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        The Austrian Government uses legislation to pressure the Rothschilds into "donating" a large number of works to Austrian museums, including the Prayer Book
+      </p>
+    </dd>
+    <dt class="timeline__header">
+      <strong>1999</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        Bowing to international pressure, the Austrian government returns the book and other works of art to the Rothschilds.
+      </p>
+    </dd>
+    <dt class="timeline__header">
+      <strong>July 8, 1999</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        The Rothschilds sell the Prayer Book through Christie's London for a world record price.
+      </p>
+    </dd>
+    <dt class="timeline__header">
+      <strong>January 9, 2014</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        The Prayer Book is sold again, at Christie's New York. The bidder is anonymous.
+      </p>
+    </dd>
+    <dt class="timeline__header">
+      <strong>September 2014</strong>
+    </dt>
+    <dd class="timeline__body">
+      <p>
+        It is revealed that Kerry Stokes, Australian businessman and owner of the Seven Network, had purchased it. The Prayer Book forms part of the Stokes collection in Perth.
+      </p>
+    </dd>
+  </dl>
+</template>
