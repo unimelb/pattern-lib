3f3d2bf77d21605dae7a3c2df3d75749345415da
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-11-09 11:01:42 +1100

    lint fix

--- components/tables/stories/index.js
+++ components/tables/stories/index.js
@@ -6,14 +6,14 @@ import TableComplex from './TableComplex.vue';
 import TableCentered from './TableCentered.vue';
 import TableTight from './TableTight.vue';
 import TableLoose from './TableLoose.vue';
-import TableHover from './TableHover.vue';;
+import TableHover from './TableHover.vue';
 import TableStatsWrapped from './TableStatsWrapped.vue';
 
 storiesOf('Tables', module)
   .add('Default', createStory(TableDefault))
   .add('Complex', createStory(TableComplex))
-  .add('In small section', createStory(TableStatsWrapped))
   .add('Centered', createStory(TableCentered))
+  .add('In small section', createStory(TableStatsWrapped))
   .add('Tight padded', createStory(TableTight))
   .add('Loose padded', createStory(TableLoose))
   .add('Hover', createStory(TableHover));
