f2e09641e27e0cded7d3fd6dfcf4a78652954695
Axel Bocciarelli <axelboc@gmail.com>
2017-11-10 15:56:07 +1100

    set up tracking of Vue render errors

--- components/index.js
+++ components/index.js
@@ -1,17 +1,16 @@
 /*  eslint-disable no-new */
+import Vue from 'vue';
 import Ike from 'ike.js';
-
-import { version } from '../package.json';
-import * as utils from './shared/utils';
 import sprite from './icons/sprite.svg';
+
+import * as utils from './shared/utils';
+import { version } from '../package.json';
 
 import './side-panel';
 import './photo-gallery';
-import initTracking from './page/tracking';
 
-window.ui = {
-  utils,
-};
+// Expose JS utilities
+window.ui = { utils };
 
 // Inject SVG sprite
 new Ike(sprite, version);
@@ -21,3 +20,26 @@ document.addEventListener('DOMContentLoaded', () => {
   document.documentElement.classList.remove('no-js');
   initTracking();
 });
+
+function initTracking() {
+  // Track pattern library version
+  window.utag_data = window.utag_data || {};
+  window.utag_data.uom_version = `v${version}`;
+
+  utils.loadScript('https://tags.tiqcdn.com/utag/unimelb/main/prod/utag.js')
+    .then(() => {
+      // Set up tracking of Vue render errors
+      Vue.config.errorHandler = (err) => {
+        trackError('vue-render', err);
+      };
+    });
+}
+
+function trackError(category, err) {
+  if (!window.utag) return;
+  window.utag.link({
+    ga_EventCategory: category,
+    ga_EventAction: 'error',
+    ga_EventLabel: err.toString(),
+  });
+}
--- components/page/tracking.js
+++
@@ -1,9 +0,0 @@
-import { version } from '../../package.json';
-
-export default function initTracking() {
-  window.utag_data = window.utag_data || {};
-  window.utag_data.uom_version = `v${version}`;
-  const script = document.createElement('script');
-  script.src = 'https://tags.tiqcdn.com/utag/unimelb/main/prod/utag.js';
-  document.head.appendChild(script);
-}
