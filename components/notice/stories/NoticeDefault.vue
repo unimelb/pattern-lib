2234145583aa340d95bd943c548c2e5567acc8ac
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2017-11-21 14:12:39 +1100

    Renames Alert to Notice to match previous ds

---
+++ components/notice/stories/NoticeDefault.vue
@@ -0,0 +1,13 @@
+<template>
+  <Notice>
+    <span> <strong>Notice!</strong> Better check yourself before you wreck yourself</span>
+  </Notice>
+</template>
+
+<script>
+import Notice from '../Notice.vue';
+
+export default {
+  components: { Notice },
+};
+</script>
