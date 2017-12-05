20f52ef0618be4e9e11903ea6f366e4305332f14
Jason Smith <jason@waitingallday.com>
2017-12-05 14:45:59 +1100

    replace unsplash.it with picsum.photos

--- components/figure/stories/Story1.vue
+++ components/figure/stories/Story1.vue
@@ -5,7 +5,7 @@
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit eum cum pariatur excepturi deleniti ipsum architecto possimus? Pariatur vitae, saepe magnam cupiditate earum cum, soluta mollitia repudiandae tempore, modi eos.
       </p>
       <figure-wrap caption="embed test">
-        <img src="https://unsplash.it/800/400?random&gravity=center" alt="">
+        <img src="https://picsum.photos/800/400?random&gravity=center" alt="">
       </figure-wrap>
       <p>Molestiae accusantium ducimus tempora delectus minima mollitia quia dicta alias. Fugiat magnam consequatur voluptatem sunt. Nesciunt est occaecati possimus quis iure architecto sunt. </p>
     </div>
