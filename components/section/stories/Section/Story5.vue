dadbe35085ccc1da0e644226de77feb35b9eba64
Jason Smith <jason@waitingallday.com>
2018-02-05 11:57:30 +1100

    add alternate color path for visited and active on dark bg section

--- components/section/stories/Section/Story5.vue
+++ components/section/stories/Section/Story5.vue
@@ -2,10 +2,10 @@
   <main>
     <SectionWrap short bg-color="white">
       <h2>Section Heading</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
+      <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipisicing</a> elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
     </SectionWrap>
     <SectionWrap short bg-color="alt">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
+      <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipisicing</a> elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
       <p>Ut rem expedita dolore cupiditate laudantium blanditiis voluptatem laborum impedit.</p>
     </SectionWrap>
   </main>
--- components/section/stories/Section/Story6.vue
+++ components/section/stories/Section/Story6.vue
@@ -2,10 +2,10 @@
   <main>
     <SectionWrap nopad bg-color="white">
       <h2>Section Heading</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
+      <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipisicing</a> elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
     </SectionWrap>
     <SectionWrap nopad bg-color="alt">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
+      <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipisicing</a> elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.</p>
     </SectionWrap>
   </main>
 </template>
