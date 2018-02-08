5665503c4d76749bb00700f49340f3ac3778c410
Jason Smith <jason@waitingallday.com>
2018-02-08 17:05:04 +1100

    fix incomplete button style, add card alternates

--- components/cards/stories/Facts/index.js
+++ components/cards/stories/Facts/index.js
@@ -5,6 +5,7 @@ import Story1 from './Story1.vue';
 import Story2 from './Story2.vue';
 import Story3 from './Story3.vue';
 import Story4 from './Story4.vue';
+import Story5 from './Story5.vue';
 /* ##Import story component here */
 
 /* Facts module */
@@ -12,4 +13,5 @@ storiesOf('Cards/Facts', module)
   .add('Size variations', createStory(Story1))
   .add('With Icons', createStory(Story2))
   .add('with Icon Single row', createStory(Story3))
-  .add('Smaller header class', createStory(Story4));
+  .add('Smaller header class', createStory(Story4))
+  .add('Top border, specify colour', createStory(Story5));
