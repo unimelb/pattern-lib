dcd18849462dc6b4f45d303e7330156926d7c766
Jason Smith <jason@waitingallday.com>
2018-02-13 17:13:06 +1100

    lint

--- components/page/header/stories/level-1/Level1HomeCampaignOverlay75.vue
+++ components/page/header/stories/level-1/Level1HomeCampaignOverlay75.vue
@@ -14,7 +14,7 @@
       <div class="max max--xsml text-center shim-pb2">
         <h1 class="shim-mb1">Talent for every<br>possible outcome</h1>
         <h3 class="shim-mb2">Further details about this amazing campaign.</h3>
-        <a class="btn btn--inverted" href=""><span class="push-icon">Call to action <svg width="15px" height="15px" focusable="false" role="presentation" class="push-icon__icon"><use xlink:href="#icon-chevron-right"></use></svg></span></a>
+        <a class="btn btn--inverted" href=""><span class="push-icon">Call to action <svg width="15px" height="15px" focusable="false" role="presentation" class="push-icon__icon"><use xlink:href="#icon-chevron-right" /></svg></span></a>
       </div>
     </header>
   </div>
