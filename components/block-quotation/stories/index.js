5861cef8eadb39fdc844d089ef421f4e079ec1cd
Jason Smith <jason@waitingallday.com>
2018-02-15 16:22:43 +1100

    reduce overall font size, border width and add longer variant

--- components/block-quotation/stories/index.js
+++ components/block-quotation/stories/index.js
@@ -8,11 +8,13 @@ import BlockQuotationAltBg from './BlockQuotationAltBg.vue';
 import BlockQuotationInverted from './BlockQuotationInverted.vue';
 import BlockQuotationInvertedWithBorders from './BlockQuotationInvertedWithBorders.vue';
 import BlockQuotationRaw from './BlockQuotationRaw.vue';
+import BlockQuotationLong from './BlockQuotationLong.vue';
 
 storiesOf('Block Quotation', module)
   .add('Default', createStory(BlockQuotationDefault))
   .add('With borders', createStory(BlockQuotationWithBorders))
   .add('Without author', createStory(BlockQuotationNoAuthor))
+  .add('Long', createStory(BlockQuotationLong))
   .add('Light background', createStory(BlockQuotationAltBg))
   .add('Inverted', createStory(BlockQuotationInverted))
   .add('Inverted with borders', createStory(BlockQuotationInvertedWithBorders))
