2942ef358f95ec41002d4f583c5582790a118708
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2018-02-19 16:28:22 +1100

    Adds backwards compatibility

---
+++ components/pathfinder/stories/Link/Story5.vue
@@ -0,0 +1,33 @@
+<template>
+  <SectionWrap>
+    <div class="grid grid--4col grid--center">
+      <ListItem>
+        <a href="https://coursesearch.unimelb.edu.au" class="btn-owner card card--link card--bdr bg-inverted">
+          <div class="card__thumb" style="background-image: url(&quot;https://about.unimelb.edu.au/__data/assets/image/0017/11717/varieties/thumb.jpg&quot;);"></div>
+          <svg width="15px" height="15px" focusable="false" role="presentation">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
+          </svg>
+          <p class="card__header">Study</p>
+        </a>
+      </ListItem>
+      <ListItem>
+        <a href="https://coursesearch.unimelb.edu.au" class="btn-owner card card--link card--bdr bg-inverted">
+          <div class="card__thumb" style="background-image: url(&quot;https://about.unimelb.edu.au/__data/assets/image/0019/11629/MSD-design-oustide.jpg&quot;);"></div>
+          <svg width="15px" height="15px" focusable="false" role="presentation">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
+          </svg>
+          <p class="card__header">Facts and Figures</p>
+        </a>
+      </ListItem>
+      <ListItem>
+        <a href="https://coursesearch.unimelb.edu.au" class="btn-owner card card--link card--bdr bg-inverted">
+          <div class="card__thumb" style="background-image: url(&quot;https://about.unimelb.edu.au/__data/assets/image/0019/11629/MSD-design-oustide.jpg&quot;);"></div>
+          <svg width="15px" height="15px" focusable="false" role="presentation">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
+          </svg>
+          <p class="card__header">Facts and Figures and something else</p>
+        </a>
+      </ListItem>
+    </div>
+  </SectionWrap>
+</template>
--- components/pathfinder/stories/Link/index.js
+++ components/pathfinder/stories/Link/index.js
@@ -5,11 +5,13 @@ import Story1 from './Story1.vue';
 import Story2 from './Story2.vue';
 import Story3 from './Story3.vue';
 import Story4 from './Story4.vue';
+import Story5 from './Story5.vue';
 /* ##Import story component here */
 
 storiesOf('Pathfinder/Contextual Link', module)
   .add('3 Boxes', createStory(Story1))
   .add('4 Boxes', createStory(Story2))
   .add('Longer titles', createStory(Story3))
-  .add('No Image', createStory(Story4));
+  .add('No Image', createStory(Story4))
+  .add('Legacy markup', createStory(Story5));
 /* ##Story goes here */
