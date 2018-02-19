1cb4ad2813308dac91d66a1a94d577a8987b58ac
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2018-02-19 14:19:51 +1100

    Adds story for link pathfinder without image
    small refactor for card-link

--- components/pathfinder/stories/Link/index.js
+++ components/pathfinder/stories/Link/index.js
@@ -4,10 +4,12 @@ import { createStory } from '.storybook/utils';
 import Story1 from './Story1.vue';
 import Story2 from './Story2.vue';
 import Story3 from './Story3.vue';
+import Story4 from './Story4.vue';
 /* ##Import story component here */
 
 storiesOf('Pathfinder/Contextual Link', module)
   .add('3 Boxes', createStory(Story1))
   .add('4 Boxes', createStory(Story2))
-  .add('Longer titles', createStory(Story3));
+  .add('Longer titles', createStory(Story3))
+  .add('No Image', createStory(Story4));
 /* ##Story goes here */
