f059f50058208526b8e8813589c019091686d28b
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2018-02-19 16:43:05 +1100

    lint fix

--- components/pathfinder/stories/Link/Story5.vue
+++ components/pathfinder/stories/Link/Story5.vue
@@ -5,7 +5,7 @@
         <a href="https://coursesearch.unimelb.edu.au" class="btn-owner card card--link card--bdr bg-inverted">
           <div class="card__thumb" style="background-image: url(&quot;https://about.unimelb.edu.au/__data/assets/image/0017/11717/varieties/thumb.jpg&quot;);"></div>
           <svg width="15px" height="15px" focusable="false" role="presentation">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"/>
           </svg>
           <p class="card__header">Study</p>
         </a>
@@ -14,7 +14,7 @@
         <a href="https://coursesearch.unimelb.edu.au" class="btn-owner card card--link card--bdr bg-inverted">
           <div class="card__thumb" style="background-image: url(&quot;https://about.unimelb.edu.au/__data/assets/image/0019/11629/MSD-design-oustide.jpg&quot;);"></div>
           <svg width="15px" height="15px" focusable="false" role="presentation">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"/>
           </svg>
           <p class="card__header">Facts and Figures</p>
         </a>
@@ -23,7 +23,7 @@
         <a href="https://coursesearch.unimelb.edu.au" class="btn-owner card card--link card--bdr bg-inverted">
           <div class="card__thumb" style="background-image: url(&quot;https://about.unimelb.edu.au/__data/assets/image/0019/11629/MSD-design-oustide.jpg&quot;);"></div>
           <svg width="15px" height="15px" focusable="false" role="presentation">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"></use>
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-chevron-right"/>
           </svg>
           <p class="card__header">Facts and Figures and something else</p>
         </a>
