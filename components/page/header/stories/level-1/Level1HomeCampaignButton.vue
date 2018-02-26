260043f1c3d7148b8e77c9a41713f3579f634740
Jason Smith <jason@waitingallday.com>
2018-02-26 11:38:29 +1100

    reorganise markup in stories

--- components/page/header/stories/level-1/Level1HomeCampaignButton.vue
+++ components/page/header/stories/level-1/Level1HomeCampaignButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="page-header" style="background-image:url(https://placeimg.com/990/530/arch/1);background-size:cover">
+  <header class="page-header page-header--campaign" style="background-image:url(https://placeimg.com/990/530/arch/1);background-size:cover">
     <div class="page-header__inner max">
       <a class="link-img link-reset page-header__logomat" href="https://www.unimelb.edu.au/">
         <img
@@ -10,7 +10,7 @@
         >
       </a>
     </div>
-    <div class="page-header--campaign page-header--baseline max max--xsml shim-pb2">
+    <div class="page-header--baseline max max--xsml shim-pb2">
       <a class="btn btn--center btn--cta" href=""><span class="push-icon">Call to action <svg width="15px" height="15px" focusable="false" role="presentation" class="push-icon__icon"><use xlink:href="#icon-chevron-right" /></svg></span></a>
     </div>
   </header>
