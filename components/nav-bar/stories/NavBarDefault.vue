314256f30b9df93bf61a3227ecb5819041519a31
Jason Smith <jason@waitingallday.com>
2018-02-27 16:36:02 +1100

    add horizontal nav bar component

---
+++ components/nav-bar/stories/NavBarDefault.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="nav--horizontal">
+    <div class="max">
+      <a class="nav__link" href="">The first link</a>
+      <a class="nav__link nav__link--current" href="">Another one</a>
+      <a class="nav__link" href="">A different link</a>
+      <a class="nav__link" href="">The fourth item</a>
+    </div>
+  </nav>
+</template>
---
+++ components/nav-bar/stories/index.js
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/vue';
+import { createStory } from '.storybook/utils';
+
+import NavBarDefault from './NavBarDefault.vue';
+
+storiesOf('Nav Bar', module)
+  .add('Default', createStory(NavBarDefault));
