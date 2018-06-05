40fa48eb4af5bbb6b2a239678608fae55c3095a3
Jason Smith <jason@waitingallday.com>
2018-06-05 13:21:35 +1000

    refactor toggleblock to fix keyboard access, fix accordion failure on empty title

---
+++ components/accordion/stories/AccordionEmpty.vue
@@ -0,0 +1,16 @@
+<template>
+  <SectionWrap bg-color="white">
+    <Accordion>
+      <h3>As one of the world’s leading universities, we aspire to build on our distinguished traditions and create an innovative future.</h3>
+    </Accordion>
+  </SectionWrap>
+</template>
+
+<script>
+import Accordion from '../Accordion.vue';
+
+export default {
+  components: { Accordion },
+  readme: { html: true, source: false },
+};
+</script>
--- components/toggle/ToggleBlock.vue
+++ components/toggle/ToggleBlock.vue
@@ -8,7 +8,7 @@
       :id="`${namespace}-header-${index + 1}`"
       :aria-controls="`${namespace}-panel-${index + 1}`"
       :aria-selected="isActive"
-      @keydown="handleKey"
+      @keydown="group ? group.handleKey($event) : handleKey($event)"
     >
       <slot></slot>
     </component>
@@ -53,7 +53,20 @@ export default {
       isActive: false,
       namespace: '',
       index: 0,
+      focusableElements: [],
     };
+  },
+  computed: {
+    trigger() {
+      let t = this.$refs.header;
+      this.$slots.default.forEach((el) => {
+        if (el.context && el.context.$refs && el.context.$refs.trigger) t = el.context.$refs.trigger;
+      });
+      return t;
+    },
+    group() {
+      return this.$parent.groupParent === undefined ? false : this.$parent.groupParent;
+    },
   },
   created() {
     this.isActive = this.active;
@@ -64,32 +77,69 @@ export default {
     }
   },
   mounted() {
-    if (this.$parent.handleClick) {
-      this.bindClick(this.$parent.handleClick);
+    if (this.group) {
+      this.trigger.addEventListener('click', this.group.handleClick);
     } else {
-      this.bindClick(this.toggle);
+      this.trigger.addEventListener('click', this.toggle);
     }
+
+    // Create array of focusable elements in context
+    const focusableSelectors = [
+      'a[href]',
+      'area[href]',
+      'input:not([disabled])',
+      'select:not([disabled])',
+      'textarea:not([disabled])',
+      'button:not([disabled])',
+      'iframe',
+      'object',
+      'embed',
+      '[contenteditable]',
+      '[tabindex]:not([tabindex^="-"])',
+    ];
+
+    this.focusableElements = [].slice.call(this.$refs.panel.querySelectorAll(focusableSelectors.join()));
+    this.toggleFocusableElements();
   },
   methods: {
     header() { return this.$refs.header; },
     panel() { return this.$refs.panel; },
     getActive() { return this.isActive; },
-    handleKey(e) { if (this.$parent.handleKey) this.$parent.handleKey(e); },
+    toggleFocusableElements() {
+      this.focusableElements.forEach((el) => {
+        el.setAttribute('tabindex', this.isActive ? 0 : -1);
+      });
+    },
+    handleKey(e) {
+      // Don't catch key events when ⌘ or Alt modifier is present
+      if (e.metaKey || e.altKey) return;
+
+      // Allow tab to pass through
+      if (e.keyCode !== 9) e.preventDefault();
+
+      switch (e.keyCode) {
+        // esc
+        case 27:
+          this.isActive = false;
+          break;
+        // enter / space
+        case 13:
+        case 32:
+          this.toggle();
+          break;
+        default: break;
+      }
+    },
     setIndex(v) { this.index = v; },
     setActive(v) {
       this.isActive = v;
+      this.toggleFocusableElements();
       this.$emit('toggle-block-set-active', this.isActive);
     },
     toggle() {
       this.isActive = !this.isActive;
+      this.toggleFocusableElements();
       this.$emit('toggle-block-toggle', this.isActive);
-    },
-    bindClick(fn) {
-      this.trigger = this.$refs.header;
-      this.$slots.default.forEach((el) => {
-        if (el.context && el.context.$refs && el.context.$refs.trigger) this.trigger = el.context.$refs.trigger;
-      });
-      this.trigger.addEventListener('click', fn);
     },
   },
 };
