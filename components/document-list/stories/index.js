df8ec953cb8bc9a2dd5c71c2a0b4d437bbe157b7
Jason Smith <jason@waitingallday.com>
2018-06-07 11:34:49 +1000

    add catch all container variant to simplify cms usage

--- components/document-list/stories/index.js
+++ components/document-list/stories/index.js
@@ -4,8 +4,10 @@ import { createStory } from '.storybook/utils';
 import Story2 from './Story2.vue';
 import Story3 from './Story3.vue';
 import Story4 from './Story4.vue';
+import ContainerDocumentList from './ContainerDocumentList.vue';
 
 storiesOf('Document List', module)
   .add('Document List', createStory(Story2))
   .add('Document thumbnail list', createStory(Story3))
-  .add('Document thumbnail list - four col', createStory(Story4));
+  .add('Document thumbnail list - four col', createStory(Story4))
+  .add('Container document list (for CMS)', createStory(ContainerDocumentList));
