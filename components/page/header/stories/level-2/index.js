821bafab6f26a4c9fd2ef8de5d8a0aef9deda6b9
Jason Smith <jason@waitingallday.com>
2018-06-25 17:20:52 +1000

    remove logo from height calcs

--- components/page/header/stories/level-2/index.js
+++ components/page/header/stories/level-2/index.js
@@ -5,9 +5,11 @@ import Level2Default from './Level2Default.vue';
 import Level2Entity from './Level2Entity.vue';
 import Level2DualEntity from './Level2DualEntity.vue';
 import Level2Campaign from './Level2CampaignOverlay50.vue';
+import Level2CampaignNoLogo from './Level2CampaignNoLogo.vue';
 
 storiesOf('Page / Header / Level 2', module)
   .add('Default', createStory(Level2Default))
   .add('Entity identifier', createStory(Level2Entity))
   .add('Dual entity identifier', createStory(Level2DualEntity))
-  .add('Campaign banner', createStory(Level2Campaign));
+  .add('Campaign banner', createStory(Level2Campaign))
+  .add('Campaign banner (no logo)', createStory(Level2CampaignNoLogo));
