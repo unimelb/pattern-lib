422f183241e1a956d40215f564a086f72a11db5f
Jason Smith <jason@waitingallday.com>
2018-06-29 11:47:31 +1000

    fix inner submenu open on megamenu mobile, parse title entities

--- components/page/megamenu/stories/index.js
+++ components/page/megamenu/stories/index.js
@@ -2,6 +2,8 @@ import { storiesOf } from '@storybook/vue';
 import { createStory } from '.storybook/utils';
 
 import HybridMenu from './HybridMenu.vue';
+import HybridMenuProp from './HybridMenuProp.vue';
 
 storiesOf('Page / Mega menu', module)
-  .add('Menu / hamburger hybrid', createStory(HybridMenu));
+  .add('Menu / hamburger hybrid', createStory(HybridMenu))
+  .add('Mega Menu, populated via prop', createStory(HybridMenuProp));
