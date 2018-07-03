bc4739de4526362831611de627bd3af5b984c7f0
Jason Smith <jason@waitingallday.com>
2018-07-03 16:00:45 +1000

    fix keyboard dismiss on search

--- components/page/search/PageSearchForm.vue
+++ components/page/search/PageSearchForm.vue
@@ -1,6 +1,13 @@
 <template>
   <form action="https://search.unimelb.edu.au" method="get" class="page-header-search__form">
-    <input ref="input" class="page-header-search__input" name="q" type="search" placeholder="Search the University" aria-label="Search the University"><button class="page-header-search__submit" type="submit">
+    <input
+      ref="input"
+      class="page-header-search__input"
+      name="q"
+      type="search"
+      placeholder="Search the University"
+      aria-label="Search the University"
+    ><button class="page-header-search__submit" type="submit">
       <SvgIcon class="page-header-search__icon" name="search" />
       <span class="screenreaders-only">Go</span>
     </button>
