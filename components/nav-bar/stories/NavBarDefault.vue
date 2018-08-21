b19fe605e32d4cb8ababc2bdcdd7cddb971e4c18
Jason Smith <jason@waitingallday.com>
2018-08-21 16:48:48 +1000

    migrate nav bar toggle back into pattern library

--- components/nav-bar/stories/NavBarDefault.vue
+++ components/nav-bar/stories/NavBarDefault.vue
@@ -1,18 +1,16 @@
 <template>
   <nav class="nav--horizontal">
-    <div class="max">
-      <a
-        class="nav__link"
-        href="">The first link</a>
-      <a
-        class="nav__link nav__link--current"
-        href="">Another one</a>
-      <a
-        class="nav__link"
-        href="">A different link</a>
-      <a
-        class="nav__link"
-        href="">The fourth item</a>
-    </div>
+    <a
+      class="nav__link"
+      href="">The first link</a>
+    <a
+      class="nav__link nav__link--current"
+      href="">Another one</a>
+    <a
+      class="nav__link"
+      href="">A different link</a>
+    <a
+      class="nav__link"
+      href="">The fourth item</a>
   </nav>
 </template>
