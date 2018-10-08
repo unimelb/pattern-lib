0479c70885a905f1c737f4a449874da29cfa7241
Dylan Kelly <dylan.kelly@unimelb.edu.au>
2018-10-08 14:22:04 +1100

    Adds alternative colour cycle for reconcilliation site

--- components/cards/stories/FocusBox/index.js
+++ components/cards/stories/FocusBox/index.js
@@ -4,11 +4,13 @@ import { createStory } from '../../../../.storybook/utils';
 import Story1 from './Story1.vue';
 import Story2 from './Story2.vue';
 import Story3 from './Story3.vue';
+import Story4 from './Story4.vue';
 /* ##Import story component here */
 
 /* Focus Boxes */
 storiesOf('Cards/Focus Boxes', module)
   .add('3 Col', createStory(Story1))
   .add('2 Col', createStory(Story2))
-  .add('Colour variations', createStory(Story3));
+  .add('Colour variations', createStory(Story3))
+  .add('Alt colour cycle', createStory(Story4));
 /* ##Story goes here */
