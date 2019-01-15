ee1a22e12ec8f75530e9f97a0f49f5ea48c725fb
Melissa gattoni <meligatt@gmail.com>
2019-01-15 16:54:13 +1100

    Add empty div named liveagent for tealium chat injection

--- components/page/footer/__tests__/PageFooter.test.js
+++ components/page/footer/__tests__/PageFooter.test.js
@@ -1,8 +1,13 @@
-import { mount } from 'vue-test-utils';
+import { mount, shallow } from 'vue-test-utils';
 import PageFooter from '../PageFooter.vue';
 
 describe('PageFooter', () => {
   it('should match snapshot', () => {
     expect(mount(PageFooter).element).toMatchSnapshot();
+  });
+  it('should render an empty div with id named liveagent', () => {
+    const wrapper = shallow(PageFooter);
+    const result = wrapper.find('[id="liveagent"]');
+    expect(result.isEmpty()).toBe(true);
   });
 });
