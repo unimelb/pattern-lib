90971e456989d3b4e1e38f5e8ce61d4196579cfc
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-02-11 21:46:20 +1100

    Fix InPageNav component to work via props

--- components/page/navigation/stories/InPageNavigationDefault.vue
+++ components/page/navigation/stories/InPageNavigationDefault.vue
@@ -1,43 +1,28 @@
 <template>
-  <InPageNavigation
-    :card-links="cardData"
-    card-title="on this page:"/>
+  <div>
+    <InPageNavigation
+      card-title="on this page"
+      jump-nav-class=".data-jump-nav"/>
+    <h1
+      id="h1"
+      class="data-jump-nav">h1</h1>
+    <h2
+      id="h2"
+      class="data-jump-nav">h2</h2>
+    <h3
+      id="h3"
+      class="data-jump-nav">h3</h3>
+  </div>
 </template>
 
 <script>
 import InPageNavigation from '../InPageNavigation.vue';
 import { codeBlock } from '../../../../.storybook/utils';
 
-const out = codeBlock(`  <InPageNavigation card-links="[{
-        title: 'Conservatorium Directorate',
-        url: 'google.com',
-      }, {
-        title: 'Conservatorium Directorate',
-        url: 'google.com',
-      },
-      {
-        title: 'Conservatorium Directorate',
-        url: 'google.com',
-      }]"
-    card-title="on this page:" />`);
+const out = codeBlock(`<InPageNavigation card-title="on this page" jump-nav-class=".data-jump-nav" />`);
 
 export default {
   components: { InPageNavigation },
   readme: { custom: out, source: false },
-  data() {
-    return {
-      cardData: [{
-        title: 'Conservatorium Directorate',
-        url: 'google.com',
-      }, {
-        title: 'Conservatorium Directorate',
-        url: 'google.com',
-      },
-      {
-        title: 'Conservatorium Directorate',
-        url: 'google.com',
-      }],
-    };
-  },
 };
 </script>
