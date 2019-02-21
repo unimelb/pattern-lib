7f3e40c3d708e660a2cb8ee8e5b18c20e1daea6c
Melissa gattoni <meligatt@gmail.com>
2019-02-21 17:39:51 +1100

    Add showcase story

---
+++ components/showcase/Showcase.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="showcase__container">
+    <div>
+      [search input]
+      [search button]
+      [filter checkboxes]
+      <ul>
+        <li
+          v-for="filter in filters"
+          :key="filter.id"
+        >
+          {{ filter.label }}
+        </li>
+      </ul>
+    </div>
+    <div>[results grid]</div>
+    <div>[pagination placeholder]</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    filters: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.showcase__container {
+  border: 1px solid red;
+}
+</style>
