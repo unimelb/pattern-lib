9c13771fe5ad21aa67b02039537af5ece648dc82
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-02-26 16:19:36 +1100

    feat(component): mega menu with top items

---
+++ components/page/megamenu/stories/MenuSecondary.vue
@@ -0,0 +1,124 @@
+<template>
+  <MegaMenu :items="items" :topItems="topItems" active="/random-link"/>
+</template>
+
+<script>
+import MegaMenu from "../MegaMenu.vue";
+
+export default {
+  components: { MegaMenu },
+  data() {
+    return {
+      items: [
+        {
+          title: "Study",
+          href: "/"
+        },
+        {
+          title: "Find a course",
+          href: "/find-a-course"
+        },
+        {
+          title: "Why's Melbourne",
+          href: "/why-melbourne",
+          feature: {
+            title: "Campaign title",
+            text:
+              "This should in theory be a super short para to sell the campaign, but I know <em>someone</em> will put too much text in",
+            img: "https://placeimg.com/360/200/arch",
+            link: "http://google.com"
+          },
+          items: [
+            { title: "Entry requirements1", href: "/random-link" },
+            { title: "Entry requirements2", href: "http://www.google.com" },
+            { title: "Entry requirements3", href: "http://www.google.com" },
+            { title: "Entry requirements4", href: "http://www.google.com" },
+            { title: "Entry requirements5", href: "http://www.google.com" }
+          ]
+        },
+        {
+          title: "Admissions",
+          href: "/admissions",
+          feature: {
+            title: "Different campaign",
+            img: "https://placeimg.com/360/200/nature"
+          },
+          items: [
+            { title: "Entry non requirements6", href: "http://www.google.com" },
+            { title: "Entry non requirements7", href: "http://www.google.com" },
+            {
+              title:
+                "Entry non requirements8 has a particularly long title for no good reason",
+              href: "http://www.google.com"
+            },
+            { title: "Entry non requirements9", href: "http://www.google.com" },
+            {
+              title: "Entry non requirements10",
+              href: "http://www.google.com"
+            },
+            {
+              title: "Entry non requirements11",
+              href: "http://www.google.com"
+            },
+            {
+              title: "Entry non requirements12",
+              href: "http://www.google.com"
+            },
+            {
+              title: "Entry non requirements13",
+              href: "http://www.google.com"
+            },
+            {
+              title: "Entry non requirements14",
+              href: "http://www.google.com"
+            },
+            { title: "Entry non requirements15", href: "http://www.google.com" }
+          ]
+        },
+        {
+          title: "Student experience",
+          href: "/student-experience",
+          feature: {
+            alt: "Screen reader only caption!",
+            img: "https://placeimg.com/360/200/nature",
+            link: "http://google.com"
+          },
+          items: [
+            { title: "Entry requirements21", href: "/2random-link" },
+            { title: "Entry requirements22", href: "http://2www.google.com" },
+            { title: "Entry requirements23", href: "http://2www.google.com" },
+            { title: "Entry requirements24", href: "http://2www.google.com" },
+            { title: "Entry requirements25", href: "http://2www.google.com" }
+          ]
+        },
+        {
+          title: "Connect",
+          href: "/connect"
+        }
+      ],
+      topItems: [
+        {
+          title: "Students",
+          href: "google.com"
+        },
+        {
+          title: "Alumni",
+          href: "google.com"
+        },
+        {
+          title: "Supporters and Partners",
+          href: "google.com"
+        },
+        {
+          title: "Contact us",
+          href: "google.com"
+        },
+        {
+          title: "FAQ",
+          href: "google.com"
+        }
+      ]
+    };
+  }
+};
+</script>
--- components/page/megamenu/stories/index.js
+++ components/page/megamenu/stories/index.js
@@ -1,9 +1,15 @@
-import { storiesOf } from '@storybook/vue';
-import { createStory } from '../../../../.storybook/utils';
+import {
+  storiesOf,
+} from '@storybook/vue';
+import {
+  createStory,
+} from '../../../../.storybook/utils';
 
 import HybridMenu from './HybridMenu.vue';
 import HybridMenuProp from './HybridMenuProp.vue';
+import MenuSecondary from './MenuSecondary.vue';
 
 storiesOf('Page / Mega menu', module)
-  .add('Menu / hamburger hybrid', createStory(HybridMenu))
-  .add('Mega Menu, populated via prop', createStory(HybridMenuProp));
+  .add('Hamburger hybrid', createStory(HybridMenu))
+  .add('Mega Menu, populated via prop', createStory(HybridMenuProp))
+  .add('Menu with top items', createStory(MenuSecondary));
