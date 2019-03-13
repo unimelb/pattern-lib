5747c212ccfe0475004d614cfcf5e926bf7074e3
Dmitry Yarmachok <dyarmachok@gmail.com>
2019-03-13 13:06:15 +0200

    add Story, minor style changes - "Card and card link"

---
+++ components/pathfinder/stories/Link/Story6.vue
@@ -0,0 +1,31 @@
+<template>
+  <SectionWrap>
+    <div class="grid grid--4col grid--center">
+      <ListItem><card-link
+        :thumb="false"
+        :cols="4"
+        title="Test 1"/></ListItem>
+      <ListItem><card-link
+        :thumb="false"
+        :cols="4"
+        title="Test 2"/></ListItem>
+      <ListItem><card-link
+        :thumb="false"
+        :cols="4"
+        title="This is a Longer title over two lines"/></ListItem>
+      <ListItem><card-link
+        :thumb="false"
+        :cols="4"
+        title="Ranking and Statistics"/></ListItem>
+    </div>
+  </SectionWrap>
+</template>
+
+<script>
+import CardLink from '../../../cards/CardLink.vue';
+
+export default {
+  components: { CardLink },
+};
+
+</script>
