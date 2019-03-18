c41881b879b1b01baa98c3fdc719b2aa80560dfe
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-03-18 16:32:30 +1100

    feature(test): add unit test to in page nav

--- components/page/navigation/__tests__/InPageNavigation.test.js
+++ components/page/navigation/__tests__/InPageNavigation.test.js
@@ -1,22 +1,70 @@
 import {
-  mount,
+  shallow,
 } from 'vue-test-utils';
+import {
+  toHaveNoViolations,
+} from 'jest-axe';
 import InPageNavigation from '../InPageNavigation.vue';
 
-describe('mounting component with simple props', () => {
-  let component;
+expect.extend(toHaveNoViolations);
 
-  beforeEach(() => {
-    component = mount(InPageNavigation, {
-      propsData: {
-        title: 'test',
-        headingLevel: 'h2',
-      },
-    });
+describe('InPageNavigation', () => {
+  it('should match snapshot', () => {
+    const result = shallow(InPageNavigation).element;
+    expect(result).toMatchSnapshot();
   });
 
-  it('should render title `test`', () => {
-    const elems = component.findAll('h2');
-    expect(elems.at(0).text()).toContain('test');
+  it('should have default props and correct types title/headingLevel/color', () => {
+    const wrapper = shallow(InPageNavigation);
+    const {
+      title,
+      headingLevel,
+      color,
+    } = wrapper.vm.$options.props;
+
+    expect(title.type).toEqual(String);
+    expect(wrapper.props().title).toBe('');
+
+    expect(headingLevel.type).toEqual(String);
+    expect(wrapper.props().href).toBe(undefined);
+
+    expect(color.type).toEqual(String);
+    expect(wrapper.props().color).toBe('');
+  });
+
+  it('should render title from prop with correct type', () => {
+    const title = 'Test title';
+    const wrapper = shallow(InPageNavigation, {
+      propsData: {
+        title,
+      },
+    });
+
+    expect(typeof wrapper.props().title).toBe('string');
+    expect(wrapper.props().title).toBe(title);
+  });
+
+  it('should render headingLevel from prop with correct type', () => {
+    const headingLevel = 'h2';
+    const wrapper = shallow(InPageNavigation, {
+      propsData: {
+        headingLevel,
+      },
+    });
+
+    expect(typeof wrapper.props().headingLevel).toBe('string');
+    expect(wrapper.props().headingLevel).toBe(headingLevel);
+  });
+
+  it('should render with correct color from prop', () => {
+    const color = 'navy';
+    const wrapper = shallow(InPageNavigation, {
+      propsData: {
+        color,
+      },
+    });
+
+    expect(typeof wrapper.props().color).toBe('string');
+    expect(wrapper.props().color).toBe(color);
   });
 });
