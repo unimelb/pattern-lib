dd4026aaf1efbe66761df6ec457f4d0dc5be7654
Dmitry Yarmachok <dyarmachok@gmail.com>
2019-03-19 18:13:57 +0200

    added - CardStat component, Story: Version of no icon, styles card-stat, unit-tests CardState changed - FocusWrapper, index.css

---
+++ components/cards/stories/FocusBox/Story5.vue
@@ -0,0 +1,49 @@
+<template>
+  <SectionWrap class="bg-alt colour-cycle--alt ">
+    <div class="grid grid--3col">
+      <ListItem>
+        <CardStat
+          :cols="3"
+          stat="590K"
+          element="div"
+          class="card--image-focus--col-brand "
+          vertical
+          color="green"
+          excerpt="should not be repeated within the text of the card"
+        />
+      </ListItem>
+      <ListItem>
+        <CardStat
+          :cols="3"
+          element="div"
+          class="card--image-focus--col-brand "
+          vertical
+          color="teal"
+          stat="123456"
+          excerpt="six characters is the most this heading can fit in a 3 column setup"
+        />
+      </ListItem>
+      <ListItem>
+        <CardStat
+          :cols="3"
+          element="div"
+          class="card--image-focus--col-brand "
+          vertical
+          color="yellow"
+          stat="290%"
+          excerpt="this card should read like a sentence with the stat as the first word"
+        />
+      </ListItem>
+    </div>
+  </SectionWrap>
+</template>
+
+<script>
+
+import CardStat from '../../CardStat.vue';
+
+export default {
+  components: { CardStat },
+};
+
+</script>
--- components/cards/stories/FocusBox/index.js
+++ components/cards/stories/FocusBox/index.js
@@ -5,6 +5,7 @@ import Story1 from './Story1.vue';
 import Story2 from './Story2.vue';
 import Story3 from './Story3.vue';
 import Story4 from './Story4.vue';
+import Story5 from './Story5.vue';
 /* ##Import story component here */
 
 /* Focus Boxes */
@@ -12,5 +13,6 @@ storiesOf('Cards/Focus Boxes', module)
   .add('3 Col', createStory(Story1))
   .add('2 Col', createStory(Story2))
   .add('Colour variations', createStory(Story3))
-  .add('Alt colour cycle', createStory(Story4));
+  .add('Alt colour cycle', createStory(Story4))
+  .add('Version of no icon', createStory(Story5));
 /* ##Story goes here */
