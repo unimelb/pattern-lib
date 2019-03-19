38f618003dd16b114e371fb18a9ab52f07cf7a27
Melissa gattoni <meligatt@gmail.com>
2019-03-19 13:59:27 +1100

    Update mega menu

--- components/page/megamenu/MegaMenuTopNavigation.vue
+++ components/page/megamenu/MegaMenuTopNavigation.vue
@@ -11,32 +11,34 @@
 
 <script>
 export default {
-  name: 'MegaMenuTopNavigation',
+  name: "MegaMenuTopNavigation",
   props: {
     topNavigationItems: {
       type: Array,
-      default: () => [],
-    },
-  },
+      default: () => []
+    }
+  }
 };
 </script>
 
 <style scoped>
-  .mega-top {
-    display: flex;
-  }
+.mega-top {
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 1.2rem;
+}
 
-  .menu-top__item {
-    margin-left: 20px;
-    color: #adcdf0;
-    font-size: 14px;
-    text-decoration: none;
-    text-transform: uppercase;
-  }
+.menu-top__item {
+  margin-left: 20px;
+  color: #adcdf0;
+  font-size: 14px;
+  text-decoration: none;
+  text-transform: uppercase;
+}
 
-  .menu-top__item:hover,
-  .menu-top__item:focus {
-    color: white;
-    text-decoration: underline;
-  }
+.menu-top__item:hover,
+.menu-top__item:focus {
+  color: white;
+  text-decoration: underline;
+}
 </style>
