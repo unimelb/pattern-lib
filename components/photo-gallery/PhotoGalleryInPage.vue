aeb8df291740ebbfd16dd386ba30d1c3caf3b722
codeuri <yuri.varshavsky@uinno.io>
2019-03-21 19:52:28 +0200

    work on gallery

--- components/photo-gallery/PhotoGalleryInPage.vue
+++ components/photo-gallery/PhotoGalleryInPage.vue
@@ -1,43 +1,79 @@
 <template>
   <div class="photo-gallery-in-page">
-    <ListingWrap
-      :cols="cols"
-      class="preview-img-list">
-      <ListItem
-        v-for="image in images"
-        :key="image.id"
+    <div class="photo-gallery-in-page__slider">
+      <div
+        class="arrow-wrapper"
+        role="button"
+        tabindex="0"
+        @click="move('prev')"
+        @keydown.13="move('prev')"
       >
-        <FigureWrap
+        <SvgIcon
+          class="photo-gallery-in-page__chevron"
+          name="chevron-left"
+          width="30"
+          height="30"
+
+        />
+      </div>
+
+      <div class="photo-gallery-in-page--container">
+        <figure >
+          <img
+            :src="selectedItem.src"
+            :alt="selectedItem.title">
+        </figure>
+      </div>
+      <div
+        class="arrow-wrapper"
+        tabindex="0"
+        role="button"
+        @click="move('next')"
+        @keydown.13="move('next')"
+      >
+        <SvgIcon
+          class="photo-gallery-in-page__chevron"
+          name="chevron-right"
+          width="30"
+          height="30"
+        />
+      </div>
+    </div>
+    <figure>
+      <div class="photo-gallery-in-page__thumbnails">
+        <div
+          v-for="(image, index) in images"
+          :key="image.id"
           :caption="captions && item.title"
-          class="photo-gallery-in-page__figure"
-          @click="noPopup === false && open(index, slots, options)"
+          :class="{ active: index === selectedIndex}"
+          :aria-describedby="'caption' + selectedIndex"
+          class="thumb"
+          tabindex="0"
+          role="button"
+          @click="open(index)"
+          @keydown.13="open(index)"
         >
           <img
             :src="image.src"
             :alt="image.title"
           >
-        </FigureWrap>
-      </ListItem>
-    </ListingWrap>
+        </div>
+      </div>
+      <div class="photo-gallery-in-page__images-count">
+        {{ selectedIndex + 1 }} / {{ images.length }}
+      </div>
+      <figcaption
+        :id="'caption' + selectedIndex"
+        class="photo-gallery-in-page__title">{{ selectedItem.title }}</figcaption>
+      <div class="photo-gallery-in-page__description">{{ selectedItem.description }}</div>
+    </figure>
   </div>
 </template>
 
 <script>
-import ListingWrap from '../listing/ListingWrap.vue';
-import FigureWrap from '../figure/FigureWrap.vue';
-import ListItem from '../listing/ListItem.vue';
 
 export default {
-  components: {
-    ListingWrap,
-    ListItem,
-    FigureWrap,
-  },
   props: {
-    cols: {
-      type: String,
-      default: '3',
-    },
     images: {
       type: Array,
       default: () => [],
@@ -54,31 +90,28 @@ export default {
       type: Object,
       default: () => ({}),
     },
-    noPopup: {
-      type: Boolean,
-      default: false,
-    },
   },
-  // computed: {
-  //   slots() {
-  //     return this.$slots.default.map(node => ({
-  //       src: node.data.attrs.src,
-  //       w: node.data.attrs.width,
-  //       h: node.data.attrs.height,
-  //       title: node.data.attrs['data-title'],
-  //       size: node.data.attrs['data-size'],
-  //     }));
-  //   },
-  // },
+  data() {
+    return {
+      selectedItem: this.images ? this.images[0] : {},
+      selectedIndex: 0,
+    };
+  },
   methods: {
-    open(
-      index,
-      items
-    ) {
-      // console.log('open');
+    open(index) {
+      this.selectedItem = this.images[index];
+      this.selectedIndex = index;
     },
-    close() {
-      // console.log('close');
+    move(direction) {
+      const len = this.images.length;
+      const current = this.selectedIndex;
+      const directions = {
+        prev: (current + len - 1) % len,
+        next: (current + 1) % len,
+      };
+      const nextIndex = directions[direction];
+      this.selectedItem = this.images[nextIndex];
+      this.selectedIndex = nextIndex;
     },
   },
 };
