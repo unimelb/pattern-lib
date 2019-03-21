dbd18467ef15dfaefb7aed33c43c05fc8957f45f
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-03-22 10:16:42 +1100

    feature(css): section background color black

--- components/section/stories/Section/Story6.vue
+++ components/section/stories/Section/Story6.vue
@@ -1,21 +1,11 @@
 <template>
-  <main>
-    <SectionWrap
-      nopad
-      bg-color="white">
-      <h2>Section Heading</h2>
-      <p>Lorem ipsum dolor sit amet,
-        <a href>consectetur adipisicing</a> elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.
-      </p>
-    </SectionWrap>
-    <SectionWrap
-      nopad
-      bg-color="alt">
-      <p>Lorem ipsum dolor sit amet,
-        <a href>consectetur adipisicing</a> elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.
-      </p>
-    </SectionWrap>
-  </main>
+  <SectionWrap bg-color="black">
+    <h2>Section Heading</h2>
+    <p>
+      Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit. Adipisci fugiat odio cupiditate ad nisi exercitationem incidunt reprehenderit nemo repellendus ea, eveniet, nostrum sunt earum similique quaerat maxime, iure recusandae inventore.
+    </p>
+  </SectionWrap>
 </template>
 
 <script>
