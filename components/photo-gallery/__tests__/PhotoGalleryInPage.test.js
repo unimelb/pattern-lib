b71eb7865c12bc8370589ad146ec0cfaaada11fc
codeuri <yuri.varshavsky@uinno.io>
2019-03-21 21:35:37 +0200

    unit tests/fix styles

--- components/photo-gallery/__tests__/PhotoGalleryInPage.test.js
+++ components/photo-gallery/__tests__/PhotoGalleryInPage.test.js
@@ -1,10 +1,26 @@
 import { shallow } from 'vue-test-utils';
 import { toHaveNoViolations } from 'jest-axe';
+import sinon from 'sinon';
 import PhotoGalleryInPage from '../PhotoGalleryInPage.vue';
 
 expect.extend(toHaveNoViolations);
 
-describe('GenericCard', () => {
+const imagesMock = [
+  {
+    id: 1,
+    src: 'test',
+    title: 'test1',
+    description: 'test1',
+  },
+  {
+    id: 2,
+    src: 'test',
+    title: 'test2',
+    description: 'test2',
+  },
+];
+
+describe('PhotoGalleryInPage', () => {
   it('should match snapshot', () => {
     const result = shallow(PhotoGalleryInPage).element;
     expect(result).toMatchSnapshot();
@@ -17,122 +33,129 @@ describe('GenericCard', () => {
     } = wrapper.vm.$options.props;
 
     expect(images.type).toBe(Array);
-    expect(wrapper.props().images).toBe('https://via.placeholder.com/400x200');
+    expect(wrapper.props().images).toEqual([{}]);
   });
-  //
-  // it('should render title from prop with correct type', () => {
-  //   const title = 'Test title';
-  //   const wrapper = shallow(GenericCard, {
-  //     propsData: {
-  //       title,
-  //     },
-  //   });
-  //
-  //   expect(typeof wrapper.props().title).toBe('string');
-  //   expect(wrapper.props().title).toBe(title);
-  //   expect(wrapper.find('h3').text()).toBe(title);
-  // });
-  //
-  // it('should render href from prop with correct type', () => {
-  //   const href = '/';
-  //   const wrapper = shallow(GenericCard, {
-  //     propsData: {
-  //       href,
-  //     },
-  //   });
-  //
-  //   expect(typeof wrapper.props().href).toBe('string');
-  //   expect(wrapper.props().href).toBe(href);
-  //   expect(wrapper.find('.card__thumb').attributes().href).toBe(href);
-  // });
-  //
-  // it('should render thumb from prop with correct type', () => {
-  //   const thumb = 'http://';
-  //   const wrapper = shallow(GenericCard, {
-  //     propsData: {
-  //       thumb,
-  //     },
-  //   });
-  //
-  //   expect(typeof wrapper.props().thumb).toBe('string');
-  //   expect(wrapper.props().thumb).toBe(thumb);
-  //   expect(wrapper.find('.card__thumb').attributes().style).toBe('background-image: url(http://);');
-  // });
-  //
-  // it('should render excerpt from prop with correct type', () => {
-  //   const excerpt = 'Lorem ipsum';
-  //   const wrapper = shallow(GenericCard, {
-  //     propsData: {
-  //       excerpt,
-  //     },
-  //   });
-  //
-  //   expect(typeof wrapper.props().excerpt).toBe('string');
-  //   expect(wrapper.props().excerpt).toBe(excerpt);
-  //   expect(wrapper.find('.card__excerpt').text()).toBe(excerpt);
-  // });
-  //
-  // it('should accept cols prop with correct type', () => {
-  //   const cols = 3;
-  //   const wrapper = shallow(GenericCard, {
-  //     propsData: {
-  //       cols,
-  //     },
-  //   });
-  //
-  //   expect(typeof wrapper.props().cols).toBe('number');
-  //   expect(wrapper.props().cols).toBe(cols);
-  // });
-  //
-  // it('should accept cols max = 3 and min = 1', () => {
-  //   const wrapper = shallow(GenericCard);
-  //   const { cols } = wrapper.vm.$options.props;
-  //
-  //   expect(cols.validator && cols.validator(0)).toBeFalsy();
-  //   expect(cols.validator && cols.validator(4)).toBeFalsy();
-  // });
-  //
-  // it('should have card image as link', () => {
-  //   const wrapper = shallow(GenericCard);
-  //   expect(wrapper.find('.card__thumb').element.tagName).toBe('A');
-  // });
-  //
-  // it('should have card title as link', () => {
-  //   const wrapper = shallow(GenericCard);
-  //   expect(wrapper.find('.card__title').element.tagName).toBe('A');
-  // });
-  //
-  // it('should have title container with 3 slots', () => {
-  //   const wrapper = shallow(GenericCard, {
-  //     slots: {
-  //       'sub-title-1': '<div class="sub-1">Sub 1</div>',
-  //       'sub-title-2': '<div class="sub-2">Sub 2</div>',
-  //       'sub-title-3': '<div class="sub-3">Sub 3</div>',
-  //       'sub-title-4': '<div class="sub-4">Sub 4</div>',
-  //     },
-  //   });
-  //   const sub1 = wrapper.find('.sub-1');
-  //   const sub2 = wrapper.find('.sub-2');
-  //   const sub3 = wrapper.find('.sub-3');
-  //   const sub4 = wrapper.find('.sub-4');
-  //   expect(sub1.exists()).toBe(true);
-  //   expect(sub2.exists()).toBe(true);
-  //   expect(sub3.exists()).toBe(true);
-  //   expect(sub4.exists()).toBe(false);
-  // });
-  //
-  // it('should hide title container if no slots', () => {
-  //   const wrapper = shallow(GenericCard);
-  //   expect(wrapper.vm.hasSubTitleSlots).toBe(false);
-  // });
-  //
-  // it('should have link container with links slot', () => {
-  //   const wrapper = shallow(GenericCard, {
-  //     slots: {
-  //       links: '<a>link</a>',
-  //     },
-  //   });
-  //   const link = wrapper.find('a');
-  //   expect(link.exists()).toBe(true);
-  // });
+
+  it('should render images from prop', () => {
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+    expect(wrapper.props().images.length).toBe(2);
+    expect(wrapper.find('img').attributes().src).toBe('test');
+  });
+
+  it('should set active to first image as default', () => {
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+    expect(wrapper.vm.selectedIndex).toBe(0);
+    expect(wrapper.vm.selectedItem.id).toBe(1);
+    expect(wrapper.find('.photo-gallery-in-page--container img').exists()).toBe(true);
+  });
+
+  it('should have click event on arrows', () => {
+    const move = sinon.stub();
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+
+    wrapper.setMethods({ move });
+    wrapper.find('.arrow-wrapper').trigger('click');
+    expect(move.called).toBe(true);
+  });
+
+  it('should have click event on thumb', () => {
+    const open = sinon.stub();
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+
+    wrapper.setMethods({ open });
+    wrapper.find('.thumb').trigger('click');
+    expect(open.called).toBe(true);
+  });
+
+  it('should change image on arrow click', () => {
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+
+    wrapper.find('.arrow-wrapper').trigger('click');
+    expect(wrapper.vm.selectedIndex).toBe(1);
+    expect(wrapper.vm.selectedItem.id).toBe(2);
+  });
+
+  it('should change image on thumb click', () => {
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+    wrapper.findAll('.thumb').at(1).trigger('click');
+    expect(wrapper.findAll('.thumb').at(1).classes()).toContain('active');
+    expect(wrapper.vm.selectedIndex).toBe(1);
+    expect(wrapper.vm.selectedItem.id).toBe(2);
+  });
+
+  it('should have click event on arrows', () => {
+    const move = sinon.stub();
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+
+    wrapper.setMethods({ move });
+    wrapper.find('.arrow-wrapper').trigger('keydown.enter');
+    expect(move.called).toBe(true);
+  });
+
+  it('should have enter event on thumb', () => {
+    const open = sinon.stub();
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+
+    wrapper.setMethods({ open });
+    wrapper.find('.thumb').trigger('keydown.enter');
+    expect(open.called).toBe(true);
+  });
+
+  it('should render current image number', () => {
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+
+    expect(wrapper.find('.photo-gallery-in-page__images-count').text()).toBe('1 / 2');
+    wrapper.find('.arrow-wrapper').trigger('click');
+    expect(wrapper.find('.photo-gallery-in-page__images-count').text()).toBe('2 / 2');
+  });
+
+  it('should render current image title/description', () => {
+    const wrapper = shallow(PhotoGalleryInPage, {
+      propsData: {
+        images: imagesMock,
+      },
+    });
+
+    expect(wrapper.find('.photo-gallery-in-page__title').text()).toBe('test1');
+    expect(wrapper.find('.photo-gallery-in-page__description').text()).toBe('test1');
+    wrapper.find('.arrow-wrapper').trigger('click');
+    expect(wrapper.find('.photo-gallery-in-page__title').text()).toBe('test2');
+    expect(wrapper.find('.photo-gallery-in-page__description').text()).toBe('test2');
+  });
 });
