d419829195b70e01b1fd0fdee1c7e6ab333def3a
codeuri <yuri.varshavsky@uinno.io>
2019-03-24 23:19:01 +0200

    added display captions option

---
+++ components/photo-gallery/stories/PopOverWithCaptions.vue
@@ -0,0 +1,24 @@
+<template>
+  <SectionWrap style="background: #000;">
+    <PhotoGalleryPopOver
+      :images="images"
+      display-caption
+    />
+  </SectionWrap>
+</template>
+
+<script>
+import PhotoGalleryPopOver from '../PhotoGalleryPopOver.vue';
+import sample from '../sample.json';
+import PhotoGalaryPopOverDocs from './photo-gallery-pop-over.md';
+
+export default {
+  components: { PhotoGalleryPopOver },
+  data() {
+    return {
+      images: sample,
+    };
+  },
+  readme: { custom: PhotoGalaryPopOverDocs, source: true, html: true },
+};
+</script>
