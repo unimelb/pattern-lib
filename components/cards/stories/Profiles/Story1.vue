19dc284df6ec67d111b2bba37769b04c066a76d9
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-03-28 15:43:16 +1100

    Changes requested

--- components/cards/stories/Profiles/Story1.vue
+++ components/cards/stories/Profiles/Story1.vue
@@ -1,55 +1,55 @@
 <template>
-  <!-- <SectionWrap bg-color="white"> -->
-  <div>
-    <ListItem>
-      <CardShowcaseProfile
-        thumb="https://via.placeholder.com/800x630"
-        title="Student Name"
-        sub-title="Study Program"
-        bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
-      />
-    </ListItem>
-    <ListItem>
-      <CardShowcaseProfile
-        thumb="https://via.placeholder.com/800x630"
-        title="Student Name"
-        sub-title="Study Program"
-        bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
-      />
-    </ListItem>
-    <ListItem>
-      <CardShowcaseProfile
-        thumb="https://via.placeholder.com/800x630"
-        title="Student Name"
-        sub-title="Study Program"
-        bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
-      />
-    </ListItem>
-    <ListItem>
-      <CardShowcaseProfile
-        thumb="https://via.placeholder.com/800x630"
-        title="Student Name"
-        sub-title="Study Program"
-        bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
-      />
-    </ListItem>
-    <ListItem>
-      <CardShowcaseProfile
-        thumb="https://via.placeholder.com/800x630"
-        title="Student Name"
-        sub-title="Study Program"
-        bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
-      />
-    </ListItem>
-    <!-- </SectionWrap> -->
-  </div>
+  <SectionWrap bg-color="white">
+    <div>
+      <ListItem>
+        <CardShowcaseProfile
+          thumb="https://via.placeholder.com/800x630"
+          title="Student Name"
+          sub-title="Study Program"
+          bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
+        />
+      </ListItem>
+      <ListItem>
+        <CardShowcaseProfile
+          thumb="https://via.placeholder.com/800x630"
+          title="Student Name"
+          sub-title="Study Program"
+          bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
+        />
+      </ListItem>
+      <ListItem>
+        <CardShowcaseProfile
+          thumb="https://via.placeholder.com/800x630"
+          title="Student Name"
+          sub-title="Study Program"
+          bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
+        />
+      </ListItem>
+      <ListItem>
+        <CardShowcaseProfile
+          thumb="https://via.placeholder.com/800x630"
+          title="Student Name"
+          sub-title="Study Program"
+          bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
+        />
+      </ListItem>
+      <ListItem>
+        <CardShowcaseProfile
+          thumb="https://via.placeholder.com/800x630"
+          title="Student Name"
+          sub-title="Study Program"
+          bio="Esse adipisicing irure ullamco aute ut excepteur enim ea adipisicing qui aliqua anim duis mollit. Mollit mollit ex excepteur ad et eiusmod. Ipsum sint id proident cillum. Laboris pariatur tempor exercitation aliquip qui ut qui deserunt incididunt aliqua elit duis. Veniam quis non reprehenderit esse consequat nostrud pariatur in commodo est id occaecat dolor quis. Duis aute sint ea mollit deserunt velit Lorem anim officia Lorem duis fugiat sunt."
+        />
+      </ListItem>
+    </div>
+  </SectionWrap>
 </template>
 
 <script>
-import CardShowcaseProfile from '../../CardShowcaseProfile.vue';
+import CardShowcaseProfile from "../../CardShowcaseProfile.vue";
 
 export default {
   components: { CardShowcaseProfile },
-  readme: { html: true, source: true },
+  readme: { html: true, source: true }
 };
 </script>
