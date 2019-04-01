60aff61c11592a25b8de01206b8c684398366a8e
Yuri <varshavawork@gmail.com>
2019-04-01 08:32:55 +0300

    feature(component): 218 Events card (#993)
    * worked on card events
    
    * fix build and css
    
    * fix test
    
    * refactor .grid--1col .cell .card--generic--full-width .card__inner into &--full-width {}
    
    * add negative margin for links (on desktop) to line with sub-title
    
    * remove third subtitle from story
    
    * add the changes requested
    
    * added divider line grey
    
    * fix border radius and shadow of generic card
    
    * fix tablet portrait viewport

---
+++ components/cards/__tests__/CardEvents.test.js
@@ -0,0 +1,91 @@
+import {
+  shallow,
+  mount,
+} from 'vue-test-utils';
+import {
+  toHaveNoViolations,
+} from 'jest-axe';
+import CardEvents from '../CardEvents.vue';
+
+expect.extend(toHaveNoViolations);
+
+describe('CardEvents', () => {
+  it('should match snapshot', () => {
+    const result = mount(CardEvents).element;
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should have default props and correct types thumb/title/href/excerpt', () => {
+    const wrapper = shallow(CardEvents);
+    const {
+      thumb,
+      title,
+      href,
+      excerpt,
+    } = wrapper.vm.$options.props;
+
+    expect(thumb.type).toBe(String);
+    expect(wrapper.props().thumb).toBe('https://via.placeholder.com/400x200');
+
+    expect(title.type).toBe(String);
+    expect(wrapper.props().title).toBe('');
+
+    expect(href.type).toBe(String);
+    expect(wrapper.props().href).toBe('#');
+
+    expect(excerpt.type).toBe(String);
+    expect(wrapper.props().excerpt).toBe('');
+  });
+
+  it('should render title from prop with correct type', () => {
+    const title = 'Test title';
+    const wrapper = mount(CardEvents, {
+      propsData: {
+        title,
+      },
+    });
+
+    expect(typeof wrapper.props().title).toBe('string');
+    expect(wrapper.props().title).toBe(title);
+    expect(wrapper.find('h3').text()).toBe(title);
+  });
+
+  it('should render href from prop with correct type', () => {
+    const href = '/';
+    const wrapper = mount(CardEvents, {
+      propsData: {
+        href,
+      },
+    });
+
+    expect(typeof wrapper.props().href).toBe('string');
+    expect(wrapper.props().href).toBe(href);
+    expect(wrapper.find('.card__thumb').attributes().href).toBe(href);
+  });
+
+  it('should render thumb from prop with correct type', () => {
+    const thumb = 'http://';
+    const wrapper = mount(CardEvents, {
+      propsData: {
+        thumb,
+      },
+    });
+
+    expect(typeof wrapper.props().thumb).toBe('string');
+    expect(wrapper.props().thumb).toBe(thumb);
+    expect(wrapper.find('.card__thumb').attributes().style).toBe('background-image: url(http://);');
+  });
+
+  it('should render excerpt from prop with correct type', () => {
+    const excerpt = 'Lorem ipsum';
+    const wrapper = mount(CardEvents, {
+      propsData: {
+        excerpt,
+      },
+    });
+
+    expect(typeof wrapper.props().excerpt).toBe('string');
+    expect(wrapper.props().excerpt).toBe(excerpt);
+    expect(wrapper.find('.card__excerpt').text()).toBe(excerpt);
+  });
+});
