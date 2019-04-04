1f6058f320a7dd9dbdf2abaa9c3e6e5ecf86f6f3
Dmitry Yarmachok <dyarmachok@gmail.com>
2019-04-04 18:01:35 +0300

    minor edit table expandable story

--- components/tables/stories/TableExpandableStory.vue
+++ components/tables/stories/TableExpandableStory.vue
@@ -47,7 +47,11 @@
             </tr>
           </template>
           <template slot="content">
-            <h3>test content container</h3>
+            <ButtonIcon
+              size="xsml"
+              class="shim-mb1">I am a child component</ButtonIcon>
+            <h3>As one of the world’s leading universities, we aspire to build on our distinguished traditions and create an innovative future.</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias, cumque, esse incidunt consequatur, accusantium odit blanditiis ipsam dolorem repellendus ut corporis earum, illum a maiores optio voluptate dicta.</p>
           </template>
         </TableExpandable>
       </tbody>
