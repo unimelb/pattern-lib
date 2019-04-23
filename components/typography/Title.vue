b51895ffdd99d25f816aa9a46752c6f2e91ff677
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-04-24 09:27:38 +1000

    remove unnecessary css for typography and update values

---
+++ components/typography/Title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>
+      <slot/>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: ""
+    },
+    headingLevel: {
+      type: Number,
+      default: 1,
+      validator: {}
+    }
+  }
+};
+</script>
