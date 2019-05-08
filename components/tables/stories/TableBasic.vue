b4865bc4add3bc87ef843d4f9c5a7ba39f224c4e
Dmitry Yarmachok <dyarmachok@gmail.com>
2019-05-08 20:37:03 +0300

    Editing the tables-docs

--- components/tables/stories/TableBasic.vue
+++ components/tables/stories/TableBasic.vue
@@ -1,6 +1,6 @@
 <template>
   <tables
-    :table="table"
+    :table="items"
   />
 </template>
 
@@ -8,25 +8,27 @@
 import Tables from '../Tables';
 import TablesDocs from './tables-docs.md';
 
+const table = {
+  thead: ['Subject name', 'Campus', 'Teaching period', 'Credit', 'Level', 'Subject code'],
+  tbody: [
+    {
+      'Subject name': 'Acting for Camera', Campus: 'Southbank', 'Teaching period': 'Winter Term', Credit: '12.5', Level: 'UG3', 'Subject code': 'DRAM30020',
+    },
+    {
+      'Subject name': 'Interactive Art Media 1', Campus: 'Southbank', 'Teaching period': 'Semester 1, Semester 2', Credit: '12.5', Level: 'UG1', 'Subject code': 'FLTV10021',
+    },
+    {
+      'Subject name': 'Art and the Botanical', Campus: 'Parkville', 'Teaching period': 'Summer Term, February, June, Winter Term', Credit: '12.5', Level: 'UG2', 'Subject code': 'FINA20044',
+    },
+  ],
+};
+
 export default {
   components: { Tables },
   readme: { custom: TablesDocs, source: true, html: true },
   data() {
     return {
-      table: {
-        thead: ['Subject name', 'Campus', 'Teaching period', 'Credit', 'Level', 'Subject code'],
-        tbody: [
-          {
-            'Subject name': 'Acting for Camera', Campus: 'Southbank', 'Teaching period': 'Winter Term', Credit: '12.5', Level: 'UG3', 'Subject code': 'DRAM30020',
-          },
-          {
-            'Subject name': 'Interactive Art Media 1', Campus: 'Southbank', 'Teaching period': 'Semester 1, Semester 2', Credit: '12.5', Level: 'UG1', 'Subject code': 'FLTV10021',
-          },
-          {
-            'Subject name': 'Art and the Botanical', Campus: 'Parkville', 'Teaching period': 'Summer Term, February, June, Winter Term', Credit: '12.5', Level: 'UG2', 'Subject code': 'FINA20044',
-          },
-        ],
-      },
+      items: table,
     };
   },
 };
--- components/tables/stories/tables-docs.md
+++ components/tables/stories/tables-docs.md
@@ -1,4 +1,25 @@
 ## Tables
 
+The Tables component receive an Object called `table` which build the Table itself
+
+Example:
+
+```
+table = {
+  thead: ['Subject name', 'Campus', 'Teaching period', 'Credit', 'Level', 'Subject code'],
+  tbody: [
+    {
+      'Subject name': 'Acting for Camera', Campus: 'Southbank', 'Teaching period': 'Winter Term', Credit: '12.5', Level: 'UG3', 'Subject code': 'DRAM30020',
+    },
+    {
+      'Subject name': 'Interactive Art Media 1', Campus: 'Southbank', 'Teaching period': 'Semester 1, Semester 2', Credit: '12.5', Level: 'UG1', 'Subject code': 'FLTV10021',
+    },
+    {
+      'Subject name': 'Art and the Botanical', Campus: 'Parkville', 'Teaching period': 'Summer Term, February, June, Winter Term', Credit: '12.5', Level: 'UG2', 'Subject code': 'FINA20044',
+    },
+  ],
+};
+```
+
 
 ### Usage Example:
