a8b9568b5783d73e15f6f8542e37758fc730f893
TimMansell <mansell.tim@gmail.com>
2019-05-14 17:02:41 +1000

    Update mobile menu actions to work on tablet viewport also

--- components/page/megamenu/MegaMenu.vue
+++ components/page/megamenu/MegaMenu.vue
@@ -253,7 +253,7 @@ export default {
   computed: {
     isMobile() {
       return this.$refs.headerroot
-        ? this.$refs.headerroot.offsetWidth < 768
+        ? this.$refs.headerroot.offsetWidth < 899
         : false;
     },
     isShowTopMenu() {
@@ -376,7 +376,7 @@ export default {
     openInner(e) {
       if (
         this.$refs.headerroot
-        && this.$refs.headerroot.offsetWidth < 768
+        && this.$refs.headerroot.offsetWidth < 899
         && e.target.nextElementSibling
       ) {
         e.preventDefault();
