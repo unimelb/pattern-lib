41c6ac35a8af4e5a073675b457da1d163b4059c4
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-05-15 15:39:28 +1000

    fix(component): social media bar (#1085)

--- components/page/social-media-bar/SocialMediaBar.vue
+++ components/page/social-media-bar/SocialMediaBar.vue
@@ -3,85 +3,73 @@
     :class="className"
     class="social-media-bar">
     <div class="social-media-bar__inner">
-      <div
-        class="social-media-bar__links">
+      <div class="social-media-bar__links">
         <a
           v-if="newsletterLink"
           :href="newsletterLink"
-          class="social-media-bar--link"
-        >
+          class="social-media-bar__link">
           <SvgIcon
             name="envelope"
             width="25"
             height="25"
-            class="social-media-bar--link--icon"
-          />
-          Sign up for e-news
+            class="social-media-bar__icon"/>Sign up for e-news
         </a>
         <a
           v-if="blogLink"
           :href="blogLink"
-          class="social-media-bar--link">
+          class="social-media-bar__link">
           <SvgIcon
             name="newspaper"
             width="25"
             height="25"
-            class="social-media-bar--link--icon"
-          />
-          Read our blog
+            class="social-media-bar__icon"/>Read our blog
         </a>
       </div>
-      <div
-        class="social-media-bar--social">
+      <div class="social-media-bar__social">
         <a
           v-if="facebookLink"
           :href="facebookLink"
-          class="social-media-bar--social--link"
-        >
+          class="social-media-bar__link--social">
           <SvgIcon
             name="facebook"
             width="30"
-            height="30" />
+            height="30"/>
         </a>
         <a
           v-if="twitterLink"
           :href="twitterLink"
-          class="social-media-bar--social--link"
-        >
+          class="social-media-bar__link--social">
           <SvgIcon
             name="twitter"
             width="30"
-            height="30" />
+            height="30"/>
         </a>
         <a
           v-if="linkedinLink"
           :href="linkedinLink"
-          class="social-media-bar--social--link"
-        >
+          class="social-media-bar__link--social">
           <SvgIcon
             name="linkedin"
             width="30"
-            height="30" />
+            height="30"/>
         </a>
         <a
           v-if="instagramLink"
           :href="instagramLink"
-          class="social-media-bar--social--link"
-        >
+          class="social-media-bar__link--social">
           <SvgIcon
             name="instagram"
             width="30"
-            height="30" />
+            height="30"/>
         </a>
         <a
           v-if="vimeoLink"
           :href="vimeoLink"
-          class="social-media-bar--social--link"
-        >
+          class="social-media-bar__link--social">
           <SvgIcon
             name="vimeo"
             width="30"
-            height="30" />
+            height="30"/>
         </a>
       </div>
     </div>
