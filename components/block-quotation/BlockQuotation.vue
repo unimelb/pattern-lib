92193efef69a6999388c6d70ddb44f263a48dd82
Tim Mansell <mansell.tim@gmail.com>
2019-05-16 16:24:31 +1000

    update blockquotation component to allow text underneath site.  Create small quote variation

--- components/block-quotation/BlockQuotation.vue
+++ components/block-quotation/BlockQuotation.vue
@@ -1,12 +1,16 @@
 <template>
   <blockquote
-    :class="{ 'block-quotation--bdr-top': borderTop, 'block-quotation--bdr-btm': borderBottom }"
+    :class="{ 'block-quotation--bdr-top': borderTop, 'block-quotation--bdr-btm': borderBottom , 'block-quotation--small': size==='small'}"
     class="block-quotation"
   >
     <p class="block-quotation__content"><slot/></p>
     <cite
       v-if="author"
-      class="block-quotation__author">{{ author }}</cite>
+      class="block-quotation__author">{{ author }}
+      <div
+        v-if="subCite"
+        class="block-quotation__sub-cite">{{ subCite }}</div>
+    </cite>
   </blockquote>
 </template>
 
@@ -14,6 +18,10 @@
 export default {
   props: {
     author: {
+      type: String,
+      default: '',
+    },
+    subCite: {
       type: String,
       default: '',
     },
@@ -25,6 +33,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    size: {
+      type: String,
+      default: '',
+    }
   },
 };
 </script>
