5c8d5a3d7bd5cd7830c4132cf2b7ff1439bd7313
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-05-22 14:11:08 +1000

    feature(component): events cards vertical

--- components/cards/stories/Events/index.js
+++ components/cards/stories/Events/index.js
@@ -1,10 +1,16 @@
-import { storiesOf } from '@storybook/vue';
-import { createStory } from '../../../../.storybook/utils';
+import {
+  storiesOf,
+} from '@storybook/vue';
+import {
+  createStory,
+} from '../../../../.storybook/utils';
 
 import Story1 from './Story1.vue';
+import Story2 from './Story2.vue';
 /* ##Import story component here */
 
 /* News Listing Cards */
 storiesOf('Cards/Events', module)
-  .add('Full width', createStory(Story1));
+  .add('Full width (horizontal)', createStory(Story1))
+  .add('Vertical', createStory(Story2));
 /* ##Story goes here */
