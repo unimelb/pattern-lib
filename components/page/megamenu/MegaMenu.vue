51deafe2bf71c72640fa02fc9520c08c2cbddb92
Tim Mansell <mansell.tim@gmail.com>
2019-05-30 12:26:44 +1000

    remove redundant code

--- components/page/megamenu/MegaMenu.vue
+++ components/page/megamenu/MegaMenu.vue
@@ -9,15 +9,13 @@
         tabindex="0"
         class="link-img link-reset"
         href="https://www.unimelb.edu.au/">
-        <div class="logo-mask">
-          <img
-            class="page-header__logo"
-            src="../../shared/logo.svg"
-            width="117"
-            height="117"
-            alt="The University of Melbourne homepage"
-          >
-        </div>
+        <img
+          class="page-header__logo"
+          src="../../shared/logo.svg"
+          width="117"
+          height="117"
+          alt="The University of Melbourne homepage"
+        >
       </a>
       <MegaMenuTitle
         v-if="facultyName"
