ef5921411e5f3e39625e5157743e5fda85dd3e5b
Tim Mansell <mansell.tim@gmail.com>
2019-06-04 12:29:34 +1000

    start working on complex responsive table

--- components/tables/stories/index.js
+++ components/tables/stories/index.js
@@ -4,6 +4,7 @@ import { createStory } from '../../../.storybook/utils';
 import TableDefault from './TableDefault.vue';
 import TableResponsive from './TableResponsive.vue';
 import TableComplex from './TableComplex.vue';
+import TableComplexResponsive from './TableComplexResponsive.vue';
 import TableCentered from './TableCentered.vue';
 import TableTight from './TableTight.vue';
 import TableLoose from './TableLoose.vue';
@@ -14,6 +15,7 @@ storiesOf('Tables', module)
   .add('Default', createStory(TableDefault))
   .add('Responsive', createStory(TableResponsive))
   .add('Complex', createStory(TableComplex))
+  .add('Complex Responsive', createStory(TableComplexResponsive))
   .add('Centered', createStory(TableCentered))
   .add('In small section', createStory(TableStatsWrapped))
   .add('Tight padded', createStory(TableTight))
