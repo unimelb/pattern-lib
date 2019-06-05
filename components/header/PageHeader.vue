2f557b6d7fbddccbf2289726cff6685146d30ba4
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-06-05 15:14:59 +1000

    add new logo to page header

--- components/header/PageHeader.vue
+++ components/header/PageHeader.vue
@@ -1,17 +1,7 @@
 <template>
   <header :class="`page-header page-header--l${level}`">
     <div class="page-header__inner max">
-      <a
-        class="link-img link-reset"
-        href="https://www.unimelb.edu.au/">
-        <img
-          class="page-header__logo"
-          src="../../shared/logo.svg"
-          width="117"
-          height="117"
-          alt="The University of Melbourne homepage"
-        >
-      </a>
+      <logo/>
       <div
         v-if="entity"
         class="page-header__entity">
@@ -33,7 +23,12 @@
 </template>
 
 <script>
+import Logo from '../../logo/Logo.vue';
+
 export default {
+  components: {
+    Logo,
+  },
   props: {
     level: {
       type: Number,
--- components/header/PageHeaderMin.vue
+++ components/header/PageHeaderMin.vue
@@ -6,23 +6,20 @@
       class="page-header__shape"
     />
     <div class="page-header__inner max">
-      <a
-        class="link-img link-reset"
-        href="https://www.unimelb.edu.au/">
-        <img
-          class="page-header__logo"
-          src="../../shared/logo.svg"
-          width="85"
-          height="71"
-          alt="The University of Melbourne homepage"
-        >
-      </a>
+      <logo
+        height="96"
+        width="96"/>
     </div>
   </header>
 </template>
 
 <script>
+import Logo from '../../logo/Logo.vue';
+
 export default {
+  components: {
+    Logo,
+  },
   props: {
     image: {
       type: [String, Boolean],
