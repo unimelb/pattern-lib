299d348a859ba5ef676167bd9b5cce01e05d2cc4
Tim Mansell <mansell.tim@gmail.com>
2019-06-11 16:25:33 +1000

    fix scroll notification borders

--- components/tables/TableResponsive.vue
+++ components/tables/TableResponsive.vue
@@ -1,7 +1,39 @@
 <template>
-  <div class="table-responsive">
-    <div class="table-responsive--inner">
+  <div
+    ref="table"
+    :class="classes"
+    class="table-responsive">
+    <div
+      class="table-responsive__inner"
+      @scroll="handleScroll">
       <slot />
     </div>
+    {{ isAtEndOfTable }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isAtEndOfTable: false,
+    };
+  },
+  computed: {
+    classes() {
+      return {
+        'table-responsive--start': this.isAtEndOfTable,
+        'table-responsive--end': !this.isAtEndOfTable,
+      };
+    },
+  },
+  methods: {
+    handleScroll(elem) {
+      this.isAtEndOfTable = this.checkAtEndOfTable(elem);
+    },
+    checkAtEndOfTable(elem) {
+      return elem.target.scrollLeft + window.innerWidth >= this.$refs.table.querySelector('table').getBoundingClientRect().width;
+    },
+  },
+};
+</script>
