49428cf317d487fa7b0088a3cf894ca656bee2ba
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-06-17 14:44:31 +1000

    refactor to use the focus wrapper component

--- components/section/stories/Focus/Story4.vue
+++ components/section/stories/Focus/Story4.vue
@@ -2,86 +2,71 @@
   <main>
     <SectionWrap
       small
-      bg-image="https://picsum.photos/seed/1/1200/540">
-      <CardFocusBox
-        element="div"
-        compact
-        class="card--image-focus--col-brand bg-white"
-        title="Learning and teaching">
-        <h2 class="text-left">
-          Header
-        </h2>
+      bg-image="https://images.unsplash.com/photo-1505639594395-22cc30113a4e?dpr=1&auto=compress,format&fit=crop&w=1200&h=&q=80&cs=tinysrgb&crop="
+    >
+      <FocusWrapper
+        padded
+        class="grid bg-white">
+        <h2 class="text-left">Header</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim accusamus dolorum labore, odit libero maxime nam inventore neque ratione optio quos sapiente atque ex fuga blanditiis alias, ducimus ut? Voluptatibus.</p>
-      </CardFocusBox>
+      </FocusWrapper>
     </SectionWrap>
     <SectionWrap
       small
-      bg-image="https://picsum.photos/seed/2/1200/540">
-      <CardFocusBox
-        element="div"
-        compact
-        color="green"
-        class="card--image-focus--col-brand bg-white"
-        title="Learning and teaching">
-        <h2 class="text-left">
-          Header
-        </h2>
+      bg-image="https://images.unsplash.com/photo-1505639594395-22cc30113a4e?dpr=1&auto=compress,format&fit=crop&w=1200&h=&q=80&cs=tinysrgb&crop="
+    >
+      <FocusWrapper
+        padded
+        class="grid bg-white"
+        color="green">
+        <h2 class="text-left">Header</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim accusamus dolorum labore, odit libero maxime nam inventore neque ratione optio quos sapiente atque ex fuga blanditiis alias, ducimus ut? Voluptatibus.</p>
-      </CardFocusBox>
+      </FocusWrapper>
     </SectionWrap>
     <SectionWrap
       small
-      bg-image="https://picsum.photos/seed/3/1200/540">
-      <CardFocusBox
-        element="div"
-        compact
-        color="yellow"
-        class="card--image-focus--col-brand bg-white"
-        title="Learning and teaching">
-        <h2 class="text-left">
-          Header
-        </h2>
+      bg-image="https://images.unsplash.com/photo-1505639594395-22cc30113a4e?dpr=1&auto=compress,format&fit=crop&w=1200&h=&q=80&cs=tinysrgb&crop="
+    >
+      <FocusWrapper
+        padded
+        class="grid bg-white"
+        color="yellow">
+        <h2 class="text-left">Header</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim accusamus dolorum labore, odit libero maxime nam inventore neque ratione optio quos sapiente atque ex fuga blanditiis alias, ducimus ut? Voluptatibus.</p>
-      </CardFocusBox>
+      </FocusWrapper>
     </SectionWrap>
     <SectionWrap
       small
-      bg-image="https://picsum.photos/seed/4/1200/540">
-      <CardFocusBox
-        element="div"
-        compact
-        color="navy"
-        class="card--image-focus--col-brand bg-white"
-        title="Learning and teaching">
-        <h2 class="text-left">
-          Header
-        </h2>
+      bg-image="https://images.unsplash.com/photo-1505639594395-22cc30113a4e?dpr=1&auto=compress,format&fit=crop&w=1200&h=&q=80&cs=tinysrgb&crop="
+    >
+      <FocusWrapper
+        padded
+        class="grid bg-white"
+        color="blue">
+        <h2 class="text-left">Header</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim accusamus dolorum labore, odit libero maxime nam inventore neque ratione optio quos sapiente atque ex fuga blanditiis alias, ducimus ut? Voluptatibus.</p>
-      </CardFocusBox>
+      </FocusWrapper>
     </SectionWrap>
     <SectionWrap
       small
-      bg-image="https://picsum.photos/seed/5/1200/540">
-      <CardFocusBox
-        element="div"
-        compact
-        color="emerald"
-        class="card--image-focus--col-brand bg-white"
-        title="Learning and teaching">
-        <h2 class="text-left">
-          Header
-        </h2>
+      bg-image="https://images.unsplash.com/photo-1505639594395-22cc30113a4e?dpr=1&auto=compress,format&fit=crop&w=1200&h=&q=80&cs=tinysrgb&crop="
+    >
+      <FocusWrapper
+        padded
+        class="grid bg-white"
+        color="emerald">
+        <h2 class="text-left">Header</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim accusamus dolorum labore, odit libero maxime nam inventore neque ratione optio quos sapiente atque ex fuga blanditiis alias, ducimus ut? Voluptatibus.</p>
-      </CardFocusBox>
+      </FocusWrapper>
     </SectionWrap>
   </main>
 </template>
 
 <script>
-import CardFocusBox from '../../../cards/CardFocusBox.vue';
+import FocusWrapper from '../../../focus-wrapper/FocusWrapper.vue';
 
 export default {
-  components: { CardFocusBox },
+  components: { FocusWrapper },
   readme: { source: true, html: true },
 };
 </script>
