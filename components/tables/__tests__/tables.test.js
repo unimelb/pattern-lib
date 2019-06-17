76f0cbe1ad809d27990ed08bd3dedda878297966
Tim Mansell <mansell.tim@gmail.com>
2019-06-17 11:36:58 +1000

    add unit tests

--- components/tables/__tests__/tables.test.js
+++ components/tables/__tests__/tables.test.js
@@ -2,6 +2,9 @@
 import { shallow } from 'vue-test-utils';
 import { toHaveNoViolations } from 'jest-axe';
 import TableResponsive from '../TableResponsive.vue';
+import TableCompacted from '../TableCompacted.vue';
+
+import BootstrapCMSTables from '../BootstrapCMSTables';
 
 expect.extend(toHaveNoViolations);
 
@@ -9,5 +12,36 @@ describe('Responsive Table', () => {
   it('should match snapshot', () => {
     const result = shallow(TableResponsive).element;
     expect(result).toMatchSnapshot();
+  });
+
+  it('should render a responsive table', () => {
+    const template = `<div>
+      <table></table>
+    </div>`;
+
+    document.body.innerHTML = template;
+
+    BootstrapCMSTables();
+
+    expect(document.querySelectorAll('table-responsive')).toHaveLength(1);
+  });
+});
+
+describe('Compacted Table', () => {
+  it('should match snapshot', () => {
+    const result = shallow(TableCompacted).element;
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render a compacted table', () => {
+    const template = `<div>
+      <table class="table--compacted"></table>
+    </div>`;
+
+    document.body.innerHTML = template;
+
+    BootstrapCMSTables();
+
+    expect(document.querySelectorAll('table-compacted')).toHaveLength(1);
   });
 });
