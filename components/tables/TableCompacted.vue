175dd8baf276d35f2f2734a07551d5e8f31c142a
Tim Mansell <mansell.tim@gmail.com>
2019-06-20 12:23:38 +1000

    fix table__row--info styling for compacted table

--- components/tables/TableCompacted.vue
+++ components/tables/TableCompacted.vue
@@ -13,7 +13,7 @@ export default {
     // const mobileHeading = this.$refs.table.querySelectorAll('.headz');
     const mobileHeadingCell = 0;
 
-    for (let rows = this.$refs.table.querySelectorAll('tr:not(.header)'), i = rows.length - 1; i >= 0; i -= 1) {
+    for (let rows = this.$refs.table.querySelectorAll('tr:not(.table__row--info)'), i = rows.length - 1; i >= 0; i -= 1) {
       for (let cells = rows[i].querySelectorAll('td'), j = cells.length - 1; j >= 0; j -= 1) {
         cells[mobileHeadingCell].classList.add('table__mobile-title');
         rows[i].setAttribute('data-mobile-heading', (cells[mobileHeadingCell].textContent));
