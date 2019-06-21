ac7402d9bf66b6599d6c08bd8147580afad3392d
Tim Mansell <mansell.tim@gmail.com>
2019-06-21 12:55:44 +1000

    add no <thead> story.  Add css to override border=1 on <table>

---
+++ components/tables/stories/TableDefaultNoHead.vue
@@ -0,0 +1,51 @@
+<template>
+  <table-responsive>
+    <table class="table table--striped">
+      <tbody>
+        <tr>
+          <td>
+            Faculty of Architecture, Building and Planning
+          </td>
+          <td>
+            Professor Julie Willis
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Faculty of Arts
+          </td>
+          <td>
+            Professor Mark Considine
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <a href="#">Faculty of Business and Economics</a>
+          </td>
+          <td>
+            Professor Paul Kofman
+            Professor Zeger Degraeve
+            (Dean Melbourne Business School)
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Faculty of Medicine, Dentistry and Health Sciences
+          </td>
+          <td>
+            Professor Shitij Kapur
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </table-responsive>
+</template>
+
+<script>
+import TableResponsive from '../TableResponsive.vue';
+
+export default {
+  components: { TableResponsive },
+  readme: { html: true, source: false },
+};
+</script>
--- components/tables/stories/index.js
+++ components/tables/stories/index.js
@@ -2,6 +2,7 @@ import { storiesOf } from '@storybook/vue';
 import { createStory } from '../../../.storybook/utils';
 
 import TableDefault from './TableDefault.vue';
+import TableDefaultNoHead from './TableDefaultNoHead.vue';
 import TableCompacted from './TableCompacted.vue';
 import TableDefaultNotResponsive from './TableDefaultNotResponsive.vue';
 import TableComplex from './TableComplex.vue';
@@ -14,6 +15,7 @@ import TableStatsWrapped from './TableStatsWrapped.vue';
 
 storiesOf('Tables', module)
   .add('Default', createStory(TableDefault))
+  .add('Default no <thead>', createStory(TableDefaultNoHead))
   .add('Default (Not Responsive)', createStory(TableDefaultNotResponsive))
   .add('Default (Compacted)', createStory(TableCompacted))
   .add('Complex', createStory(TableComplex))
