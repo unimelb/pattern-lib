e3b6509b74bb16a768a4ac2e2e836eb407baaa6e
Tim Mansell <mansell.tim@gmail.com>
2019-06-24 14:56:54 +1000

    add check for manually adding table components in cms

--- components/tables/BootstrapCMSTables.js
+++ components/tables/BootstrapCMSTables.js
@@ -1,21 +1,47 @@
-export default () => {
+const isTableParentAComponent = (element) => {
+  const componentsToCompare = ['table-responsive', 'table-compacted'];
+  const parentName = element.parentElement.tagName.toLowerCase();
+
+  return componentsToCompare.includes(parentName);
+};
+
+const createElement = elementName => document.createElement(elementName);
+
+const hasClass = (element, classToCheck) => element.classList.contains(classToCheck);
+
+const isCompactedTable = element => hasClass(element, 'table--compacted');
+
+const isNormalTable = element => hasClass(element, 'table--is-not-responsive');
+
+const BootstrapCMSTables = () => {
   const tableElements = document.querySelectorAll('table');
   const elem = [].slice.call(tableElements);
 
   elem.forEach((el) => {
+    const hasBeenWrapped = isTableParentAComponent(el);
+    const isACompactedTable = isCompactedTable(el);
+    const isANormalTable = isNormalTable(el);
     let wrapper;
 
-    if (el.classList.contains('table--compacted')) {
-      wrapper = document.createElement('table-compacted');
-    } else {
-      if (el.classList.contains('table--is-not-responsive')) {
-        return;
-      }
+    // Table has manually been wrapped in a table component.
+    if (hasBeenWrapped) {
+      return;
+    }
 
-      wrapper = document.createElement('table-responsive');
+    // If the user has specified that the table is not responsive.
+    if (isANormalTable) {
+      return;
+    }
+
+    if (isACompactedTable) {
+      wrapper = createElement('table-compacted');
+    } else {
+      wrapper = createElement('table-responsive');
     }
 
     el.parentNode.insertBefore(wrapper, el);
     wrapper.appendChild(el);
   });
 };
+
+export default BootstrapCMSTables;
