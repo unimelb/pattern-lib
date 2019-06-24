9af2e1bbe19bf17f46162de45e04fda03c7dc0e3
Tim Mansell <mansell.tim@gmail.com>
2019-06-24 16:29:08 +1000

    change hasInfoHeadings to hasHeadings

--- components/tables/CompactedTable.vue
+++ components/tables/CompactedTable.vue
@@ -11,13 +11,13 @@
 export default {
   data() {
     return {
-      hasInfoHeadings: false,
+      hasHeadings: false,
     };
   },
   computed: {
     classes() {
       return [{
-        'compacted-table--headings': !this.hasInfoHeadings,
+        'compacted-table--headings': !this.hasHeadings,
       }];
     },
   },
@@ -29,11 +29,11 @@ export default {
       const headings = this.$refs.table.querySelectorAll('thead th');
       const mobileHeadingCell = 0;
 
-      this.hasInfoHeadings = this.$refs.table.querySelectorAll('.table__row--info').length > 0;
+      this.hasHeadings = this.$refs.table.querySelectorAll('.table__row--header').length > 0;
 
-      for (let rows = this.$refs.table.querySelectorAll('tr:not(.table__row--info)'), i = rows.length - 1; i >= 0; i -= 1) {
+      for (let rows = this.$refs.table.querySelectorAll('tr:not(.table__row--header)'), i = rows.length - 1; i >= 0; i -= 1) {
         for (let cells = rows[i].querySelectorAll('td'), j = cells.length - 1; j >= 0; j -= 1) {
-          if (!this.hasInfoHeadings) {
+          if (!this.hasHeadings) {
             cells[mobileHeadingCell].classList.add('table__mobile-title');
             rows[i].setAttribute('data-mobile-heading', (cells[mobileHeadingCell].textContent));
           }
