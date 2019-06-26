99f788f5a0bd9a9bc748a95c517f671d930d2330
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-06-26 16:14:39 +1000

    Introduce background helpers section

---
+++ components/helpers/stories/Backgrounds.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <section-wrap>
+      <h1 class="bg-inverted">Class: bg-inverted</h1>
+      <h1 class="bg-inverted-dark">Class: bg-inverted-dark</h1>
+      <h1 class="bg-inverted-darker">Class: bg-inverted-darker</h1>
+      <h1 class="bg-inverted--o95">Class: bg-inverted--o95</h1>
+      <h1 class="bg-alt">Class: bg-alt</h1>
+      <h1 class="bg-alt-darker">Class: bg-alt-darker</h1>
+      <h1 class="bg-light-blue">Class: bg-light-blue</h1>
+      <h1 class="bg-white">Class: bg-white</h1>
+    </section-wrap>
+  </div>
+</template>
---
+++ components/helpers/stories/Colours.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <section-wrap bg-color="white">
+      <h1>Helper Section</h1>
+      <div class="bg-saw-tooth"/>
+    </section-wrap>
+  </div>
+</template>
