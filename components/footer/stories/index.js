1c4fb934e7c8e2d6516cbbe639df1bd5f9da5696
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-06-27 16:53:35 +1000

    chore(tidy): move page componentes to root directory (#1174)

---
+++ components/footer/stories/index.js
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/vue';
+import { createStory } from '../../../.storybook/utils';
+
+import PageFooterDefault from './PageFooterDefault.vue';
+
+storiesOf(' Footer', module)
+  .add('Default', createStory(PageFooterDefault));
---
+++ components/header/PageHeaderMin.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="page-header page-header--l3">
+    <div
+      :style="{ backgroundImage: image}"
+      :class="{'bg-saw-tooth': !image, 'bg-pattern': image}"
+      class="page-header__shape"
+    />
+    <div class="page-header__inner max">
+      <a
+        class="link-img link-reset"
+        href="https://www.unimelb.edu.au/">
+        <img
+          class="page-header__logo"
+          src="../shared/logo-responsive.svg"
+          width="85"
+          height="71"
+          alt="The University of Melbourne homepage"
+        >
+      </a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: [String, Boolean],
+      default: false,
+    },
+  },
+};
+</script>
