1712bf5a0f541c02e22666a354f50541b956ec74
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-06-27 14:35:08 +1000

    remove unnecessary commments

--- .storybook/config.js
+++ .storybook/config.js
@@ -46,11 +46,9 @@ if (process.env.NODE_ENV !== 'production') {
   const VueAxe = require('vue-axe');
   Vue.use(VueAxe, {
     config: {
-      // ...
       rules: [
         { id: 'heading-order', enabled: true },
         { id: 'label-title-only', enabled: true },
-        // and more
       ],
     },
   });
