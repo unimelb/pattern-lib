c2e02d237fdfdf3795a40fa95c7a2197e26dd322
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-07-03 14:47:35 +1000

    feat(component): tags - news tag

---
+++ components/tags/Tags.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tags">
+    <span
+      v-for="item in data"
+      :key="item.id"
+      class="tags__item">
+      {{ item }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
---
+++ components/tags/stories/Story1.vue
@@ -0,0 +1,18 @@
+<template>
+  <Tags :data="tags"/>
+</template>
+
+<script>
+import Tags from '../Tags.vue';
+import SectionWrap from '../../section/SectionWrap.vue';
+
+export default {
+  components: { Tags, SectionWrap },
+  readme: { source: true, html: true },
+  data() {
+    return {
+      tags: ['php', 'ruby', 'javascript', 'python', 'java', 'c#', 'scala', 'closure'],
+    };
+  },
+};
+</script>
