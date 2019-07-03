ab701194a62a6d549bc6fe090a6eb8c12a721996
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-07-03 16:28:07 +1000

    fixes to the tags to accept href

--- components/tags/stories/Story1.vue
+++ components/tags/stories/Story1.vue
@@ -11,7 +11,30 @@ export default {
   readme: { source: true, html: true },
   data() {
     return {
-      tags: ['php', 'ruby', 'javascript', 'python', 'java', 'c#', 'scala', 'closure'],
+      tags: [{
+        href: 'https://www.unimelb.edu.au/',
+        value: 'tag1',
+      },
+      {
+        href: 'https://www.unimelb.edu.au/',
+        value: 'tag2',
+      },
+      {
+        href: 'https://www.unimelb.edu.au/',
+        value: 'tag3',
+      },
+      {
+        href: 'https://www.unimelb.edu.au/',
+        value: 'tag4',
+      },
+      {
+        href: 'https://www.unimelb.edu.au/',
+        value: 'tag5',
+      },
+      {
+        href: 'https://www.unimelb.edu.au/',
+        value: 'tag super hiper ultra mega long',
+      }],
     };
   },
 };
