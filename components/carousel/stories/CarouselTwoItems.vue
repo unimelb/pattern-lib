ba151b64e0d278ba600aa7d3522a4a4bf9ee6d48
Tim Mansell <mansell.tim@gmail.com>
2019-07-04 14:34:58 +1000

    add stories for 1 and two item carousels

---
+++ components/carousel/stories/CarouselTwoItems.vue
@@ -0,0 +1,35 @@
+<template>
+  <Carousel
+    :stories="stories"
+  />
+</template>
+
+<script>
+import Carousel from '../Carousel.vue';
+import CarouselDoc from './carousel-doc.md';
+
+export default {
+  components: { Carousel },
+  data() {
+    return {
+      stories: [{
+        src: 'http://6.hikb.at/wycieczki/sabaudia/pb-1218040.jpg',
+        altText: 'test',
+        title: 'Story 3',
+        description: 'Story 3 description',
+        buttonText: 'Read',
+        buttonHref: 'google.com',
+      },
+      {
+        src: 'http://6.hikb.at/wycieczki/sabaudia/pb-1218040.jpg',
+        altText: 'test',
+        title: 'Story 3',
+        description: 'Story 3 description',
+        buttonText: 'Read',
+        buttonHref: 'google.com',
+      }],
+    };
+  },
+  readme: { custom: CarouselDoc, source: true, html: true },
+};
+</script>
--- components/carousel/stories/index.js
+++ components/carousel/stories/index.js
@@ -2,10 +2,13 @@ import { storiesOf } from '@storybook/vue';
 import { createStory } from '../../../.storybook/utils';
 
 import CarouselDefault from './CarouselDefault.vue';
-import CarouselLong from './CarouselLongTiming';
-
+import CarouselLong from './CarouselLongTiming.vue';
+import CarouselSingle from './CarouselSingle.vue';
+import CarouselTwoItems from './CarouselTwoItems.vue';
 
 /* Section - Focus  */
 storiesOf('Carousel', module)
   .add('Default', createStory(CarouselDefault))
-  .add('Long', createStory(CarouselLong));
+  .add('Long', createStory(CarouselLong))
+  .add('Single', createStory(CarouselSingle))
+  .add('Two Items', createStory(CarouselTwoItems));
