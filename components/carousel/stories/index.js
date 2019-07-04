6de48cdb2a70a295c305e303482b14bee208466b
Tim Mansell <mansell.tim@gmail.com>
2019-07-04 15:26:46 +1000

    remove 1 and 2 item carousel stories

--- components/carousel/stories/index.js
+++ components/carousel/stories/index.js
@@ -3,12 +3,8 @@ import { createStory } from '../../../.storybook/utils';
 
 import CarouselDefault from './CarouselDefault.vue';
 import CarouselLong from './CarouselLongTiming.vue';
-import CarouselSingle from './CarouselSingle.vue';
-import CarouselTwoItems from './CarouselTwoItems.vue';
 
 /* Section - Focus  */
 storiesOf('Carousel', module)
   .add('Default', createStory(CarouselDefault))
-  .add('Long', createStory(CarouselLong))
-  .add('Single', createStory(CarouselSingle))
-  .add('Two Items', createStory(CarouselTwoItems));
+  .add('Long', createStory(CarouselLong));
