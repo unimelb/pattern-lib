b2b029b1e41b776070244a039d7c1ace78779ae3
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-07-15 14:23:29 +1000

    tidy up the story and readme

--- components/cards/stories/GenericCard/Story7.vue
+++ components/cards/stories/GenericCard/Story7.vue
@@ -8,10 +8,18 @@
           <template slot="links">
             <a
               href="/"
-              aria-label="View details of the event name">View generic details ></a>
+              aria-label="View details of the event name">View generic details               <SvgIcon
+                class="link-icon"
+                name="chevron-right"
+                width="10"
+                height="10"/></a>
             <a
               href="/"
-              aria-label="View details of the staff name">View generic staff ></a>
+              aria-label="View details of the staff name">View generic staff               <SvgIcon
+                class="link-icon"
+                name="chevron-right"
+                width="10"
+                height="10"/></a>
           </template>
         </GenericCard>
       </ListItem>
@@ -24,10 +32,18 @@
           <template slot="links">
             <a
               href="/"
-              aria-label="View details of the event name">View generic details ></a>
+              aria-label="View details of the event name">View generic details               <SvgIcon
+                class="link-icon"
+                name="chevron-right"
+                width="10"
+                height="10"/></a>
             <a
               href="/"
-              aria-label="View details of the staff name">View generic staff ></a>
+              aria-label="View details of the staff name">View generic staff               <SvgIcon
+                class="link-icon"
+                name="chevron-right"
+                width="10"
+                height="10"/></a>
           </template>
         </GenericCard>
       </ListItem>
