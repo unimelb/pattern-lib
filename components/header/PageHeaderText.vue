648017f393b1785e740c7ced44c4c04ef3c6cf68
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-08-05 16:29:39 +1000

    feat(component): header text with cta and social list

---
+++ components/header/PageHeaderText.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="page-header-text">
+    <div class="page-header-text__inner">
+      <div class="page-header-text__header">
+        <h4>{{ headerText }}</h4>
+      </div>
+      <div class="page-header-text__body">
+        <h2 class="page-header-text__title">{{ titleText }}</h2>
+        <h4 class="page-header-text__sub-title">{{ subTitleText }}</h4>
+        <p class="page-header-text__description">{{ descriptionText }}</p>
+      </div>
+      <div class="page-header-text__footer">
+        <ButtonIcon
+          v-if="buttonLink"
+          :href="buttonLink"
+          class="page-header-text__button">{{ buttonText }}</ButtonIcon>
+        <SocialList class="page-header-text__social-list"/>
+        <span>{{ socialList }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SocialList from '../social-list/SocialList.vue';
+export default {
+  components: {
+    SocialList,
+  },
+  props: {
+    headerText: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    titleText: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    subTitleText: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    descriptionText: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      default: '',
+    },
+    buttonLink: {
+      type: String,
+      default: '',
+    },
+    socialList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
---
+++ components/header/stories/text/Default.vue
@@ -0,0 +1,25 @@
+<template>
+  <PageHeaderText
+    :social-list="socialList"
+    header-text="Exhibition"
+    title-text="2019 Victorian College of the Arts Graduate Exhibition"
+    sub-title-text="Margaret Lawrence Gallery"
+    description-text="20 Nov 10 - 25 Nov 19"
+    button-text="Book tickets"
+    button-link="www.unimelb.edu.au"/>
+</template>
+
+<script>
+import PageHeaderText from '../../PageHeaderText.vue';
+import sample from './sample.json';
+
+export default {
+  components: { PageHeaderText },
+  readme: { source: true, html: true },
+  data() {
+    return {
+      socialList: sample,
+    };
+  },
+};
+</script>
