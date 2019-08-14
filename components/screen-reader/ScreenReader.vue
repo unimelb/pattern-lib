0da1fc21fdf0c9f1c9811568598e2bffe9dd48bd
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-08-15 09:57:53 +1000

    fix class names and redundant classes

--- components/screen-reader/ScreenReader.vue
+++ components/screen-reader/ScreenReader.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="screen-reader__container">
+  <div class="screen-reader">
     <ButtonIcon
       href="#main"
       no-icon
       size="xsml"
-      class="screen-reader__skippy screen-reader__sr-only screen-reader__sr-only-focusable btn--inverted">Skip to main content</ButtonIcon>
+      class="screen-reader__skippy screenreaders-only screenreaders-only-focusable btn--inverted">Skip to main content</ButtonIcon>
   </div>
 </template>
