65fbee42e903dd9e2655896c2e4d5c92c7ff3b68
Tim Mansell <mansell.tim@gmail.com>
2019-08-16 15:21:57 +1000

    simply thumbnails component

--- components/media-gallery/ThumbnailGallery.vue
+++ components/media-gallery/ThumbnailGallery.vue
@@ -1,87 +1,46 @@
 <template>
   <div class="thumbnails">
-    <ListingWrap :cols="columns">
-      <ListItem
-        v-vpshow
-        v-for="(item, index) in media"
-        :key="item.id"
-        class="thumbnails__item-wrap"
+    <div
+      v-for="(item, index) in media"
+      :key="item.id"
+      :class="thumbnailColumnClass"
+      class="thumbnails__container">
+      <FigureWrap
+        :caption="displayCaption && item.title"
+        :class="{'thumbnails__item': true, 'thumbnails__item--prevent-click': !overlay}"
+        tabindex="0"
       >
-        <FigureWrap
-          :caption="displayCaption && item.title"
-          :class="{'thumbnails__item': true, 'thumbnails__item--prevent-click': !overlay}"
-          tabindex="0"
-        >
-          <div
-            :class="{'thumbnails__item--prevent-click': !overlay}"
-            @click="overlay === true && callback(index)"
-            @keydown.13="callback(index)">
-            <img
-              v-if="item.type === 'image'"
-              :src="item.src"
-              :alt="item.altText"
-              class="thumbnails__image"
-            >
-            <div
-              v-if="item.type === 'video'"
-              class="thumbnails__video"
-            >
-              <VideoEmbed :src="item.src"/>
-            </div>
-          </div>
-        </FigureWrap>
         <div
-          v-if="displayCaption"
-          class="thumbnails__description">{{ item.description }}</div>
-      </ListItem>
-    </ListingWrap>
+          :class="{'thumbnails__item--prevent-click': !overlay}"
+          @click="overlay === true && callback(index)"
+          @keydown.13="callback(index)">
+          <img
+            v-if="item.type === 'image'"
+            :src="item.src"
+            :alt="item.altText"
+            class="thumbnails__image"
+          >
+          <div
+            v-if="item.type === 'video'"
+            class="thumbnails__video"
+          >
+            <VideoEmbed :src="item.src"/>
+          </div>
+        </div>
+      </FigureWrap>
+      <div
+        v-if="displayCaption"
+        class="thumbnails__description">{{ item.description }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import ListingWrap from '../listing/ListingWrap.vue';
 import FigureWrap from '../figure/FigureWrap.vue';
-import ListItem from '../listing/ListItem.vue';
 import VideoEmbed from '../embed/VideoEmbed.vue';
-
-Vue.directive('vpshow', {
-  inViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return !(
-      rect.bottom < 0
-      || rect.right < 0
-      || rect.left > window.innerWidth
-      || rect.top > window.innerHeight
-    );
-  },
-
-  bind(el, binding) {
-    el.classList.add('invisible');
-    el.$onScroll = () => {
-      if (binding.def.inViewport(el)) {
-        el.classList.add('thumbnails__anim-reveal');
-        el.classList.remove('thumbnails__invisible');
-        binding.def.unbind(el, binding);
-      }
-    };
-    document.addEventListener('scroll', el.$onScroll);
-  },
-
-  inserted(el) {
-    el.$onScroll();
-  },
-
-  unbind(el) {
-    document.removeEventListener('scroll', el.$onScroll);
-    delete el.$onScroll;
-  },
-});
 
 export default {
   components: {
-    ListingWrap,
-    ListItem,
     FigureWrap,
     VideoEmbed,
   },
@@ -105,6 +64,13 @@ export default {
     overlay: {
       type: Boolean,
       default: false,
+    },
+  },
+  computed: {
+    thumbnailColumnClass() {
+      const { columns } = this;
+
+      return `thumbnails__container--${columns}-col`;
     },
   },
 };
