996058f4d62199d18f2a34de02fc48322b2a5caa
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-08-21 14:34:05 +1000

    test vue-agile implementation

---
+++ components/media-gallery/stories/MediaGallery2.vue
@@ -0,0 +1,22 @@
+<template>
+  <SectionWrap>
+    <MediaGallery2
+    />
+  </SectionWrap>
+</template>
+
+<script>
+import MediaGallery2 from '../MediaGallery2.vue';
+import sample from '../sample.json';
+import MediaGalleryDocs from './media-gallery.md';
+
+export default {
+  components: { MediaGallery2 },
+  data() {
+    return {
+      media: sample,
+    };
+  },
+  readme: { custom: MediaGalleryDocs, source: true, html: false },
+};
+</script>
--- components/media-gallery/stories/index.js
+++ components/media-gallery/stories/index.js
@@ -9,10 +9,12 @@ import MediaGalleryInPage from './MediaGalleryInPage.vue';
 import MediaGalleryOverlay from './MediaGalleryOverlay.vue';
 import MediaGalleryOverlayWithCaption from './MediaGalleryOverlayWithCaption.vue';
 import MediaGalleryThumbnails from './MediaGalleryThumbnails.vue';
+import MediaGallery2 from './MediaGallery2.vue';
 /* ##Import story component here */
 
 storiesOf('Media Gallery', module)
   .add('In page', createStory(MediaGalleryInPage))
   .add('Overlay', createStory(MediaGalleryOverlay))
   .add('Overlay with captions', createStory(MediaGalleryOverlayWithCaption))
+  .add('Vue Agile example', createStory(MediaGallery2))
   .add('Standalone thumbnails', createStory(MediaGalleryThumbnails));
