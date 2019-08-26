196aeebc06ad2c77debca82aa75fe26e3206c14b
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-08-26 11:41:58 +1000

    fix IE11

--- components/header/stories/event/index.js
+++ components/header/stories/event/index.js
@@ -3,5 +3,5 @@ import { createStory } from 'storybook/utils';
 
 import Default from './Default.vue';
 
-storiesOf(' Header / Event', module)
+storiesOf('Header / Event', module)
   .add('Default', createStory(Default));
--- components/header/stories/split-section/index.js
+++ components/header/stories/split-section/index.js
@@ -3,5 +3,5 @@ import { createStory } from '../../../../.storybook/utils';
 
 import Default from './Default.vue';
 
-storiesOf(' Header / Split Section', module)
+storiesOf('Header / Split Section', module)
   .add('Default', createStory(Default));
