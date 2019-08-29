dcd0667ee24f773ddd138cd9719a259645a7ed49
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-08-29 15:19:14 +1000

    feature(component): media gallery (#1215)
    * photo gallery in-page setup
    
    * work on gallery
    
    * work on gallery
    
    * unit tests
    
    * unit tests/fix styles
    
    * add component to lib/added markdown
    
    * work on pop over gallery
    
    * added to target folder
    
    * added display captions option
    
    * photo gallery in-page setup
    
    * work on gallery
    
    * work on gallery
    
    * unit tests
    
    * unit tests/fix styles
    
    * add component to lib/added markdown
    
    * work on pop over gallery
    
    * added to target folder
    
    * added display captions option
    
    * fix build issues
    
    * export component/move to new folder/fix thumb nails
    
    * PR fixes
    
    * PR fixes
    
    * lint fixes
    
    * PR fixes that depends on In page component
    
    * fix lint/snapshots
    
    * photo gallery in-page setup
    
    * work on gallery
    
    * work on gallery
    
    * unit tests
    
    * unit tests/fix styles
    
    * add component to lib/added markdown
    
    * work on pop over gallery
    
    * added to target folder
    
    * added display captions option
    
    * photo gallery in-page setup
    
    * work on gallery
    
    * work on gallery
    
    * unit tests
    
    * unit tests/fix styles
    
    * work on pop over gallery
    
    * fix build issues
    
    * export component/move to new folder/fix thumb nails
    
    * PR fixes
    
    * PR fixes
    
    * lint fixes
    
    * PR fixes that depends on In page component
    
    * fix lint/snapshots
    
    * fix conflicts
    
    * fix conflicts
    
    * work on conflict/removed awesome-swiper/added new swiper lib
    
    * work on conflict/integrate new swipe functionality/added stories/merge component
    
    * unit tests
    
    * MD file/thumbnails story
    
    * fix gallery issues
    
    * fix gallery issues
    
    * fix gallery issues
    
    * fix gallery issues
    
    * fix storie and export
    
    * fix build
    
    * fix PR issues
    
    * split out css into seperate files
    
    * refactor thumnails to be more BEMlike
    
    * work media-gallery according PR comments
    
    * refactor component/ work on tests
    
    * refactor component/ work on tests
    
    * add back in missing photo-gallery index.css
    
    * work on new specs/test/styles
    
    * some fixes to the PR
    
    * pr fixes
    
    * scroll thumbnail carousel
    
    * video stop on navigation
    
    * align thumbnails
    
    * fix stories and export
    
    * update to show different size images
    
    * fix video thumbnail mobile
    
    * pr fixes
    
    * tweak classes for more bem like and fix keyboard navigation
    
    * simply thumbnails component
    
    * simplify MediaGallery component
    
    * fix SvgIcon aria label
    
    * accessibili
    
    * add infinite loop
    
    * remove infinite loop
    
    * fix thumbnails width
    
    * changes to match designs
    
    * css improvements
    
    * fix image layouts
    
    * migrate overlay css to media-gallery
    
    * fix  close overlay position
    
    * disable body scroll when overlay is open
    
    * mobile and tablet style fixes
    
    * JS refactor and improvements
    
    * fix issue with mobile viewport swiping not moving thumbnails
    
    * fix spacing between the featured image and thumbnails
    
    * fix ie11 win10 thumbnails broken
    
    * remove MediaGallery from vue build
    
    * update ie11 scrollbar styling
    
    * add IE scrollbar global styling
    
    * fix thumbnails ie11
    
    * add width
    
    * IE11 fix
    
    * IE11 fix
    
    * add deleted json files
    
    * update documentation and improve variable names
    
    * fix unit tests
    
    * fix unit tests
    
    * fix video not playing in thumbnails
    
    * add z-index so white border overlays video thumbnail

---
+++ components/media-gallery/__tests__/MediaGallery.test.js
@@ -0,0 +1,282 @@
+import {
+  shallow,
+  mount,
+} from 'vue-test-utils';
+import {
+  toHaveNoViolations,
+} from 'jest-axe';
+import sinon from 'sinon';
+import MediaGallery from '../MediaGallery.vue';
+
+expect.extend(toHaveNoViolations);
+
+const itemsMock = [{
+  src: 'test',
+  title: 'test1',
+  description: 'test1',
+  type: 'image',
+  altText: 'alt text 1',
+},
+{
+  src: 'test2',
+  title: 'test2',
+  description: 'test2',
+  type: 'video',
+  altText: 'alt text 2',
+},
+];
+
+describe('MediaGallery Common', () => {
+  it('should match snapshot', () => {
+    const result = shallow(MediaGallery).element;
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should have default props and correct types items, useOverlay, columns, displayCaption', () => {
+    const wrapper = shallow(MediaGallery);
+    const {
+      items, useOverlay, columns, displayCaption,
+    } = wrapper.vm.$options.props;
+
+    expect(columns.type).toBe(String);
+    expect(wrapper.props().columns).toBe('4');
+    expect(items.type).toBe(Array);
+    expect(useOverlay.type).toBe(Boolean);
+    expect(displayCaption.type).toBe(Boolean);
+    expect(wrapper.props().items).toEqual([]);
+  });
+
+  it('should accept cols prop with correct type', () => {
+    const columns = '3';
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        columns,
+      },
+    });
+
+    expect(typeof wrapper.props().columns).toBe('string');
+    expect(wrapper.props().columns).toBe(columns);
+  });
+
+  it('should render media image in slider from prop', () => {
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+    expect(wrapper.props().items.length).toBe(2);
+    expect(wrapper.find('img').attributes().src).toBe('test');
+  });
+
+  it('should render media video in slider from prop', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+    expect(wrapper.props().items.length).toBe(2);
+    expect(wrapper.find('iframe').exists()).toBe(true);
+  });
+
+  it('should set active to first media as default', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+    expect(wrapper.vm.selectedIndex).toBe(0);
+  });
+
+  it('should have click event on arrows', () => {
+    const move = sinon.stub();
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+
+    wrapper.setMethods({
+      move,
+    });
+    wrapper.find('.media-gallery__button').trigger('click');
+    expect(move.called).toBe(true);
+  });
+
+  it('should have click event on thumb', () => {
+    const open = sinon.stub();
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+
+    wrapper.setMethods({
+      open,
+    });
+    wrapper.find('.media-gallery__thumb-image').trigger('click');
+    expect(open.called).toBe(true);
+  });
+
+  it('should change media on arrow click', () => {
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+
+    Element.prototype.scrollIntoView = jest.fn();
+
+    wrapper.find('.media-gallery__button').trigger('click');
+    expect(wrapper.vm.selectedIndex).toBe(1);
+  });
+
+  it('should have click event on arrows', () => {
+    const move = sinon.stub();
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+
+    wrapper.setMethods({
+      move,
+    });
+    wrapper.find('.media-gallery__button').trigger('keydown.enter');
+    expect(move.called).toBe(true);
+  });
+
+  it('should render current image title/description', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+
+    Element.prototype.scrollIntoView = jest.fn();
+
+    expect(wrapper.vm.selectedIndex).toBe(0);
+    expect(wrapper.find('.media-gallery__title').text()).toBe('test1');
+    expect(wrapper.find('.media-gallery__description').text()).toBe('test1');
+    wrapper.find('.media-gallery__button').trigger('click');
+    expect(wrapper.find('.media-gallery__title').text()).toBe('test2');
+    expect(wrapper.find('.media-gallery__description').text()).toBe('test2');
+  });
+});
+
+describe('MediaGallery InPage', () => {
+  it('should render current media number', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+
+    Element.prototype.scrollIntoView = jest.fn();
+
+    expect(wrapper.find('.media-gallery__count').text()).toBe('1 / 2');
+    wrapper.find('.media-gallery__button').trigger('click');
+    expect(wrapper.find('.media-gallery__count').text()).toBe('2 / 2');
+  });
+
+  it('should change image on thumb click', () => {
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+    wrapper.findAll('.media-gallery__thumb').at(1).trigger('click');
+    expect(wrapper.findAll('.media-gallery__thumb').at(1).classes()).toContain('active');
+    expect(wrapper.vm.selectedIndex).toBe(1);
+  });
+
+  it('should render embed if image type', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+    wrapper.find('.media-gallery__button').trigger('click');
+    wrapper.find('.media-gallery__button').trigger('click');
+    expect(wrapper.find('.media-gallery__thumb-image').exists()).toBe(true);
+  });
+
+  it('should render embed if video type', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+      },
+    });
+    wrapper.find('.media-gallery__button').trigger('click');
+    expect(wrapper.find('.embed').exists()).toBe(true);
+  });
+});
+
+describe('MediaGallery overlay', () => {
+  it('should have overlay classes', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+        useOverlay: true,
+      },
+    });
+
+    expect(wrapper.find('.media-gallery').classes()).toContain('media-gallery--hide');
+    expect(wrapper.find('.media-gallery__count').exists()).toBe(true);
+    expect(wrapper.find('.media-gallery__close').exists()).toBe(true);
+    expect(wrapper.find('.media-gallery__slider').classes()).toContain('media-gallery__slider--overlay');
+    expect(wrapper.find('.media-gallery__container--overlay').exists()).toBe(true);
+  });
+
+  it('should render current media number', () => {
+    const wrapper = shallow(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+        useOverlay: true,
+      },
+    });
+
+    expect(wrapper.find('.media-gallery__count').text()).toBe('1 / 2');
+    wrapper.find('.media-gallery__button').trigger('click');
+    expect(wrapper.find('.media-gallery__count').text()).toBe('2 / 2');
+  });
+
+  it('should have thumbnails component', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+        useOverlay: true,
+      },
+    });
+
+    expect(wrapper.find('.thumbnails').exists()).toBe(true);
+  });
+
+  it('should have img thumbnails open callback', () => {
+    const openThumb = sinon.stub();
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+        useOverlay: true,
+      },
+    });
+
+    wrapper.setMethods({
+      openThumb,
+    });
+    wrapper.find('.thumbnails__item div').trigger('click');
+    expect(openThumb.called).toBe(true);
+  });
+
+  it('should open slider pop over on thumb click', () => {
+    const wrapper = mount(MediaGallery, {
+      propsData: {
+        items: itemsMock,
+        useOverlay: true,
+      },
+    });
+
+    wrapper.find('.thumbnails__item div').trigger('click');
+    expect(wrapper.vm.openState).toBe(true);
+    expect(wrapper.find('.media-gallery').classes()).toContain('media-gallery--show');
+  });
+});
---
+++ components/media-gallery/stories/MediaGalleryOverlay2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-inverted">
+    <MediaGallery
+      :items="items"
+      columns="2"
+      use-overlay
+    />
+  </div>
+</template>
+
+<script>
+import MediaGallery from '../MediaGallery.vue';
+import sample from '../sample.json';
+import MediaGalleryDocs from './media-gallery-overlay.md';
+
+export default {
+  components: { MediaGallery },
+  data() {
+    return {
+      items: sample,
+    };
+  },
+  readme: { custom: MediaGalleryDocs, source: false, html: false },
+};
+</script>
