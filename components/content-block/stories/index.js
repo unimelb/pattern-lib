c38a22c39a36fb820fb121bffd5e5bddbcf3c87f
Tim Mansell <mansell.tim@gmail.com>
2019-08-30 11:08:43 +1000

    Create story listing all heading elements that can be affected by

--- components/content-block/stories/index.js
+++ components/content-block/stories/index.js
@@ -4,6 +4,7 @@ import { createStory } from 'storybook/utils';
 import ContentBlockDefault from './ContentBlockDefault.vue';
 import ContentBlockSmall from './ContentBlockSmall.vue';
 import ContentBlockLargeInverted from './ContentBlockLargeInverted.vue';
+import ContentBlockHeadings from './ContentBlockHeadings.vue';
 import ContentBlockShortAlt from './ContentBlockShortAlt.vue';
 import ContentBlockNews from './ContentBlockNews.vue';
 import ContentBlockEvent from './ContentBlockEvent.vue';
@@ -13,6 +14,7 @@ storiesOf('Content Block', module)
   .add('Default (medium)', createStory(ContentBlockDefault))
   .add('Small', createStory(ContentBlockSmall))
   .add('Large + inverted', createStory(ContentBlockLargeInverted))
+  .add('Headings', createStory(ContentBlockHeadings))
   .add('Short + light', createStory(ContentBlockShortAlt))
   .add('News block, isolated', createStory(ContentBlockNews))
   .add('Event block, isolated', createStory(ContentBlockEvent))
