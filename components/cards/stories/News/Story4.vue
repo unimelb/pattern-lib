bfba5747c2883bffbe13ab5fa7405843cf92cbca
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-09-03 09:48:35 +1000

    update story

---
+++ components/cards/stories/News/Story4.vue
@@ -0,0 +1,35 @@
+<template>
+  <SectionWrap class="bg-alt">
+    <div class="grid grid--3col">
+      <ListItem>
+        <CardNews title="Test 1"/>
+      </ListItem>
+      <ListItem>
+        <CardNews title="Test 2"/>
+      </ListItem>
+      <ListItem>
+        <CardNews title="Test 3"/>
+      </ListItem>
+    </div>
+    <div class="grid grid--3col shim-mt1">
+      <ListItem>
+        <ButtonIcon>Read More</ButtonIcon>
+      </ListItem>
+      <ListItem>
+        <ButtonIcon icon="microphone">Podcast</ButtonIcon>
+      </ListItem>
+      <ListItem>
+        <ButtonIcon icon="twitter">Twitter</ButtonIcon>
+      </ListItem>
+    </div>
+  </SectionWrap>
+</template>
+
+<script>
+import CardNews from '../../CardNews.vue';
+
+export default {
+  components: { CardNews },
+  readme: { html: true, source: true },
+};
+</script>
--- components/cards/stories/News/index.js
+++ components/cards/stories/News/index.js
@@ -4,11 +4,13 @@ import { createStory } from 'storybook/utils';
 import Story1 from './Story1.vue';
 import Story2 from './Story2.vue';
 import Story3 from './Story3.vue';
+import Story4 from './Story4.vue';
 /* ##Import story component here */
 
 /* News Listing Cards */
 storiesOf('Cards/News', module)
   .add('3 Column', createStory(Story1))
   .add('1 Column', createStory(Story2))
-  .add('Buttons Underneath', createStory(Story3));
+  .add('Buttons Underneath', createStory(Story3))
+  .add('Buttons Underneath (Deprecated)', createStory(Story4));
 /* ##Story goes here */
