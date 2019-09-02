3fd7371f0961455d8d2972706b664665759ad983
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-09-02 10:27:49 +1000

    move classes to .card class

--- components/cards/GenericCard.vue
+++ components/cards/GenericCard.vue
@@ -6,6 +6,7 @@
       :href="href"
       :style="{ backgroundImage: `url(${thumb})` }"
       :aria-label="'Image for' + title"
+      :class="[cols === 1 ? 'card__thumb--full-height' : '']"
       class="card__thumb"
     />
     <div class="card__inner">
