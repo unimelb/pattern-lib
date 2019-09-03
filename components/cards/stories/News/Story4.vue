ee5fb44b98fa34a65ee641443c7fc69d2e03a1c4
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-09-03 10:18:29 +1000

    feat(component): news tag 3 column (#1266)
    * feat(component): news tag 3 column

--- components/cards/stories/News/Story4.vue
+++ components/cards/stories/News/Story4.vue
@@ -1,126 +1,35 @@
 <template>
   <SectionWrap class="bg-alt">
-    <div class="grid grid--1col">
+    <div class="grid grid--3col">
       <ListItem>
-        <CardNewsTag
-          :tags="tags"
-          thumb="https://via.placeholder.com/400x200"
-          title="Test 1"
-          href="#"
-          excerpt="Lorem ipsum dolor sit amet, consectetur."
-        >
-          <div
-            slot="sub-title-1"
-            class="sub-title">
-            <span>Sub-title 1</span>
-          </div>
-          <template slot="links">
-            <a
-              href="/"
-              aria-label="View article">
-              View article
-              <SvgIcon
-                class="link-icon"
-                name="chevron-right"
-                width="10"
-                height="10"/>
-            </a>
-          </template>
-        </CardNewsTag>
+        <CardNews title="Test 1"/>
       </ListItem>
       <ListItem>
-        <CardNewsTag
-          :tags="tags"
-          thumb="https://via.placeholder.com/800x400"
-          title="Lorem ipsum dolor sit amet, consectetur"
-          href="#"
-          excerpt="Lorem ipsum dolor sit amet, consectetur."
-        >
-          <div
-            slot="sub-title-1"
-            class="sub-title">
-            <span>Sub-title 1</span>
-          </div>
-          <template slot="links">
-            <a
-              href="/"
-              aria-label="View article">
-              View article
-              <SvgIcon
-                class="link-icon"
-                name="chevron-right"
-                width="10"
-                height="10"/>
-            </a>
-          </template>
-        </CardNewsTag>
+        <CardNews title="Test 2"/>
       </ListItem>
       <ListItem>
-        <CardNewsTag
-          :tags="tags"
-          thumb="https://via.placeholder.com/800x600"
-          title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae magni natus quas placeat adipisci optio libero quibusdam vitae nobis."
-          href="#"
-          excerpt="Lorem ipsum dolor sit amet, consectetur."
-        >
-          <div
-            slot="sub-title-1"
-            class="sub-title">
-            <span>Sub-title 1</span>
-          </div>
-          <template slot="links">
-            <a
-              href="/"
-              aria-label="View article">
-              View article
-              <SvgIcon
-                class="link-icon"
-                name="chevron-right"
-                width="10"
-                height="10"/>
-            </a>
-          </template>
-        </CardNewsTag>
+        <CardNews title="Test 3"/>
+      </ListItem>
+    </div>
+    <div class="grid grid--3col shim-mt1">
+      <ListItem>
+        <ButtonIcon>Read More</ButtonIcon>
+      </ListItem>
+      <ListItem>
+        <ButtonIcon icon="microphone">Podcast</ButtonIcon>
+      </ListItem>
+      <ListItem>
+        <ButtonIcon icon="twitter">Twitter</ButtonIcon>
       </ListItem>
     </div>
   </SectionWrap>
 </template>
 
 <script>
-import SvgIcon from '../../../icons/SvgIcon.vue';
-import CardNewsTag from '../../CardNewsTag.vue';
-import news from './news.md';
+import CardNews from '../../CardNews.vue';
 
 export default {
-  components: { CardNewsTag, SvgIcon },
-  readme: { custom: news, source: true, html: true },
-  data() {
-    return {
-      tags: [{
-        href: 'https://www.unimelb.edu.au/',
-        value: 'tag1',
-      },
-      {
-        href: 'https://www.unimelb.edu.au/',
-        value: 'tag2',
-      },
-      {
-        href: 'https://www.unimelb.edu.au/',
-        value: 'tag3',
-      },
-      {
-        href: 'https://www.unimelb.edu.au/',
-        value: 'tag4',
-      },
-      {
-        href: 'https://www.unimelb.edu.au/',
-        value: 'tag5',
-      },
-      {
-        href: 'https://www.unimelb.edu.au/',
-        value: 'tag super hiper ultra mega long',
-      }],
-    };
-  },
+  components: { CardNews },
+  readme: { html: true, source: true },
 };
 </script>
