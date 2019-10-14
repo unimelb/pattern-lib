d21088b74511be07969b156ef4870f86e7cfe360
Tim Mansell <mansell.tim@gmail.com>
2019-10-14 15:47:26 +1100

    fix toggleblock  unit tests

--- components/toggle/__tests__/ToggleBlock.test.js
+++ components/toggle/__tests__/ToggleBlock.test.js
@@ -7,15 +7,13 @@ import ToggleBlock from '../ToggleBlock.vue';
 expect.extend(toHaveNoViolations);
 
 describe('ToggleBlock', () => {
-  it.skip('should match snapshot', () => {
+  it('should match snapshot', () => {
     const result = shallow(ToggleBlock).element;
     expect(result).toMatchSnapshot();
   });
 
-  it.skip('Component throws no accessibility violations', (done) => {
-    const html = mount(ToggleBlock, {
-      parent: '<h1>test</h1>',
-    }).html();
+  it('Component throws no accessibility violations', (done) => {
+    const html = mount(ToggleBlock).html();
     // pass anything that outputs html to axe
     return axe(html).then((response) => {
       expect(response).toHaveNoViolations();
