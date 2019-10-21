b30468a495fe482d035f4bb75a2c94cb3f791c79
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2019-10-21 16:19:26 +1100

    fix(component): card news tag image on safari

--- components/cards/GenericCard.vue
+++ components/cards/GenericCard.vue
@@ -5,7 +5,6 @@
       :href="href"
       :style="{ backgroundImage: `url(${thumb})` }"
       :aria-label="'Image for' + title"
-      :class="[cols === 1 ? 'card__thumb--full-height' : '']"
       class="card__thumb" />
     <div class="card__inner">
       <h3 class="card__header">
