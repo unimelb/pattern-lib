121def7fd9841e5981f1a3a8c13f55ec96c39cb8
Tim Mansell <mansell.tim@gmail.com>
2019-11-28 11:16:32 +1100

    update styled-select to not use svgicon

--- components/forms/StyledSelect.vue
+++ components/forms/StyledSelect.vue
@@ -6,23 +6,12 @@
       @change="callback">
       <slot />
     </select>
-    <div
-      v-if="icon"
-      :class="iconClasses">
-      <SvgIcon
-        width="15px"
-        height="15px"
-        name="chevron-down" />
-    </div>
   </div>
 </template>
 
 <script>
-import SvgIcon from '../icons/SvgIcon.vue';
-
 export default {
   name: 'StyledSelect',
-  components: { SvgIcon },
   props: {
     id: {
       type: [String, Boolean],
@@ -36,26 +25,12 @@ export default {
       type: Function,
       default: () => {},
     },
-    icon: {
-      type: Boolean,
-      default: false,
-    },
   },
   data: () => ({
     extraclasses: '',
     baseclass: 'styled-select',
     blueclass: 'styled-select--blue',
   }),
-  computed: {
-    iconClasses() {
-      return [
-        'styled-select__icon',
-        {
-          'styled-select__icon--blue': this.blue,
-        },
-      ];
-    },
-  },
   mounted() {
     this.extraclasses = this.$el.className;
     this.$el.className = `${this.baseclass}${this.blue ? ` ${this.blueclass}` : ''}`;
