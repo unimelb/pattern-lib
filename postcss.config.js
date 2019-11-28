8934ff255f441b8ccda88c8210e2773a72561ee7
Tim Mansell <mansell.tim@gmail.com>
2019-11-28 13:08:19 +1100

    chore(update) Update styled-select to not use svgicon (#1356)
    * add styled-select component to bundle
    
    * install postcss-inline-svg
    
    * update to allow variant of select for cms
    
    * install postcss-assets
    
    * update styled-select to not use svgicon
    
    * remove redundant icon prop
    
    * update to not use ../ in image paths

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const isDev = process.env.NODE_ENV !== 'production';
 const isMinified = process.env.MINIFY_CSS === 'true';
 
+const imagePaths = [
+  'components/icons/sprite/',
+  'components/',
+];
+
 module.exports = ({ options }) => ({
   plugins: {
     'postcss-import': {},
+    'postcss-assets': {
+      loadPaths: imagePaths,
+    },
     'postcss-url': {},
+    'postcss-inline-svg': {
+      paths: imagePaths,
+    },
     'postcss-nested': {},
     'postcss-cssnext': {
       browsers: [
