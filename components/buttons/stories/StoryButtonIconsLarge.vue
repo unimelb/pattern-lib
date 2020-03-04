3424adbcae31b41ea74a6154008cdb65457563d3
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2020-03-04 14:01:15 +1100

    feat(component): button icon sizes (#1407)
    * feat(component): button icon sizes
    
    * fix button icon sizes to receive large
    
    * PR changes
    
    * update snapshots

---
+++ components/buttons/stories/StoryButtonIconsLarge.vue
@@ -0,0 +1,13 @@
+<template>
+  <ButtonIcon
+    icon-size="lg"
+    icon="chevron-right">
+    I am a button with a large icon
+  </ButtonIcon>
+</template>
+
+<script>
+export default {
+  readme: { source: true, html: true },
+};
+</script>
