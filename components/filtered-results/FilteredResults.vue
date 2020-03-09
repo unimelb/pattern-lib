6cf95698c6ce851491e01411c821bab2ebf0770f
alexx-dev <61265590+alexx-dev@users.noreply.github.com>
2020-03-09 02:39:20 +0200

    Feat/1689 fac results (#1397)
    * - 1689 - created filter-results component (in progress)
    
    * - 1689 - renamed component
    
    * - 1689 - added color schemes for cards
    
    * - 1689 - added emit for change action
    
    * - 1689 - added hover for card
    
    * - 1689 - fixed padding for items wrapper
    
    * - 1689 - added text update functionality based on selected filters
    
    * - 1689 - added cursor point for card
    
    * - 1689 - added callback function which can be triggered upon clicking on change text
    
    * - 1689 - created tests
    
    * - 1689 - fixed issues from PR
    
    * - 1689 - updated test FilteredResultsTitle.test.js
    
    * 1689:
    - removed FiltederCard.vue
    - added slot to FilteredResults.vue
    - updated stories to show how to use component
    - removed unnescessery css
    
    * 1689:
    - tests updates
    
    * 1689:
    - tests updates
    
    * 1689:
    - tests updated
    - templates functionality updated
    
    * fix linting issue for netlify build
    
    * 1689:
    - tests updated
    - components updated
    
    * 1689:
    - added state when message is invisible
    
    Co-authored-by: alexey_chuykov <alexey.chuykov@herobrands.com.au>
    Co-authored-by: Tim <mansell.tim@gmail.com>

---
+++ components/filtered-results/FilteredResults.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="filtered-results">
+    <div class="filtered-results__inner">
+      <FilteredResultsTitle
+        :items="items"
+        :filters="filters"
+        :secondary-message="secondaryMessage"
+        :callback="callback" />
+      <div class="filtered-results__content">
+        <div
+          v-if="items > 0"
+          class="filtered-results__items">
+          <slot />
+        </div>
+        <div
+          v-else
+          class="filtered-results__no-items">
+          <h3>No results</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FilteredResultsTitle from './FilteredResultsTitle.vue';
+
+export default {
+  components: {
+    FilteredResultsTitle,
+  },
+  props: {
+    items: {
+      type: Number,
+      default: () => 0,
+      required: true,
+    },
+    filters: {
+      type: Number,
+      default: () => 0,
+      required: true,
+    },
+    secondaryMessage: {
+      type: String,
+      default: () => '',
+      required: false,
+    },
+    callback: {
+      type: Function,
+      default: () => false,
+      required: false,
+    },
+  },
+};
+</script>
---
+++ components/filtered-results/stories/FilteredResultsDefault.vue
@@ -0,0 +1,14 @@
+<template>
+  <FilteredResults
+    :items="-1"
+    :filters="0"
+    :callback="() => false" />
+</template>
+
+<script>
+import FilteredResults from '../FilteredResults.vue';
+
+export default {
+  components: { FilteredResults },
+};
+</script>
