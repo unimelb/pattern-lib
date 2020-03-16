cb871d0f1e2873a7f8428207df9e082288a97a12
Tim <mansell.tim@gmail.com>
2020-03-16 16:03:44 +1100

    button and carousel tests

---
+++ cypress/integration/snapshots/button.snapshot.js
@@ -0,0 +1,79 @@
+const mainCategory = 'Buttons';
+
+const stories = [
+  {
+    category: 'Types',
+    pages: [
+      'Primary (default)',
+      'Secondary',
+      'Inverted',
+      'CTA',
+      'Campaign',
+      'Event',
+      'Event alt',
+      '<button> element',
+    ],
+  },
+  {
+    category: 'Icons',
+    pages: [
+      'Icons',
+      'No Icon',
+      'Icon on left',
+      'Large icon',
+    ],
+  },
+  {
+    category: 'Adjacent',
+    pages: [
+      'Default',
+      'Full width',
+      'Prev/next aligned buttons',
+    ],
+  },
+  {
+    category: 'Width',
+    pages: [
+      'Default',
+      'Wide',
+      'Extra wide',
+      'Full width',
+    ],
+  },
+  {
+    category: 'Sizes',
+    pages: [
+      'Default',
+      'Extra small',
+      'small',
+    ],
+  },
+  {
+    category: 'CMS',
+    pages: [
+      'Default (btn--icon)',
+      'Inverted',
+      'Inverse (btn--icon-before)',
+    ],
+  },
+];
+
+const viewports = Cypress.config('viewports');
+
+describe(mainCategory, () => {
+  stories.forEach((story) => {
+    const { category } = story;
+
+    describe(category, () => {
+      story.pages.forEach((page) => {
+        viewports.forEach((vp) => {
+          it(`${page} matches ${vp} snapshot`, () => {
+            // const { category } = story;
+
+            cy.checkSnapshot(`${mainCategory}/${category}`, `${category}-${page}`, vp);
+          });
+        });
+      });
+    });
+  });
+});
