3585f8400b3cd0d25405c4689a239529730561d0
Tim <mansell.tim@gmail.com>
2020-03-18 12:19:10 +1100

    fix snapshot not taking whole page

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,12 +2,14 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 Cypress.Commands.overwrite('log', (subject, message) => cy.task('log', message));
 
-Cypress.Commands.add('checkSnapshot', (category, story, vp) => {
-  cy.visit(`/?selectedKind=${category}&selectedStory=${story}${Cypress.env('params')}`);
+Cypress.Commands.add('checkSnapshot', ({
+  url, category, story, vp,
+}) => {
+  cy.visit(`iframe.html?selectedKind=${url}&selectedStory=${story}`);
 
   cy.viewport(vp);
 
-  cy.get('#storybook-preview-iframe').matchImageSnapshot(`${story}.${vp}`);
+  cy.get('#root').matchImageSnapshot(`${category}-${story}.${vp}`);
 });
 
 Cypress.Commands.add('setMobileViewPort', () => {
@@ -23,5 +25,5 @@ addMatchImageSnapshotCommand({
   failureThreshold: 0.05, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
   customDiffDir: 'cypress/snapshots/__diff_output__',
-  capture: 'fullPage', // capture viewport in screenshot
+  capture: 'viewport', // capture viewport in screenshot
 });
