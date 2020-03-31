116711222a71dc85ee02b60d00b0beda3509bb01
Denis Onishchenko <denis.onishchenko@uinno.io>
2020-03-31 11:47:17 +0300

    FAC Filters (#1687)
    - ErrorBox component

---
+++ components/error-box/ErrorBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="messages.length">
+    <div
+      v-for="(message, messageIndex) in messages"
+      :key="messageIndex">
+      {{ message }}
+    </div>
+  </div>
+</template>
+
+<script>
+// TODO add styling
+export default {
+  name: 'ErrorBox',
+  props: {
+    messages: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
