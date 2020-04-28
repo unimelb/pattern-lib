abda4cc22196cebd23bb4162d01d3b57381a18eb
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-04-28 10:48:30 +1000

    fix lint

--- components/helpers/stories/Texts.vue
+++ components/helpers/stories/Texts.vue
@@ -1,17 +1,41 @@
 <template>
   <section-wrap bg-color="white">
     <h1>Texts helpers classes:</h1>
-    <p class="text-center">Class: Text center</p>
-    <p class="text-left">Class: Text left</p>
-    <p class="text-right">Class: Text right</p>
-    <p class="text-italic">Class: Text italic</p>
-    <p class="text-bold">Class: Text bold</p>
-    <p class="text-bigger">Class: Text bigger</p>
-    <p class="text-smaller">Class: Text smaller</p>
-    <p class="text-underborder">Class: Text underborder</p>
-    <p class="emsp-r">Class: emsp-r</p>
-    <p class="emsp-l">Class: emsp-l</p>
-    <p class="text-ellipsis">Class: text-ellipsis</p>
-    <p class="text-nowrap">Class: text-nowrap</p>
+    <p class="text-center">
+      Class: Text center
+    </p>
+    <p class="text-left">
+      Class: Text left
+    </p>
+    <p class="text-right">
+      Class: Text right
+    </p>
+    <p class="text-italic">
+      Class: Text italic
+    </p>
+    <p class="text-bold">
+      Class: Text bold
+    </p>
+    <p class="text-bigger">
+      Class: Text bigger
+    </p>
+    <p class="text-smaller">
+      Class: Text smaller
+    </p>
+    <p class="text-underborder">
+      Class: Text underborder
+    </p>
+    <p class="emsp-r">
+      Class: emsp-r
+    </p>
+    <p class="emsp-l">
+      Class: emsp-l
+    </p>
+    <p class="text-ellipsis">
+      Class: text-ellipsis
+    </p>
+    <p class="text-nowrap">
+      Class: text-nowrap
+    </p>
   </section-wrap>
 </template>
