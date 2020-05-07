595d2e5b2d8307f744daefc361566d9f0e4baf98
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-05-07 11:25:35 +1000

    introduce card course list component

---
+++ components/cards/stories/Course/Story2.vue
@@ -0,0 +1,100 @@
+<template>
+  <CardCourseList :items="items" />
+</template>
+
+<script>
+import CardCourseList from '../../CardCourseList.vue';
+import CardCourseListReadme from './card-course-list.md';
+
+export default {
+  components: { CardCourseList },
+  readme: { custom: CardCourseListReadme, source: true, html: false },
+  data() {
+    return {
+      items: [
+        {
+          title: 'Bachelor of Design',
+          category: 'bachelor',
+          type: 'bachelor',
+          link: '/find/courses/undergraduate/bachelor-of-design',
+        },
+        {
+          title: 'Bachelor of Fine Arts (Degree with Honours)',
+          category: 'degree',
+          type: 'degree',
+          link:
+            '/find/courses/undergraduate/bachelor-of-fine-arts-degree-with-honours',
+        },
+        {
+          title: 'Bachelor of Fine Arts (Design and Production)',
+          category: 'diploma',
+          type: 'diploma',
+          link:
+            '/find/courses/undergraduate/bachelor-of-fine-arts-design-and-production',
+        },
+        {
+          title: "Melbourne Chancellor's Scholarship",
+          category: 'certificate',
+          type: 'certificate',
+          link:
+            "/find/courses/undergraduate/melbourne-chancellor's-scholarship",
+        },
+        {
+          title: 'Bachelor of Science',
+          category: 'coursework',
+          type: 'coursework',
+          link: '/find/courses/undergraduate/bachelor-of-science',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'research',
+          type: 'research',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'major',
+          type: 'major',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'phd',
+          type: 'phd',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'specialisation',
+          type: 'specialisation',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'interest',
+          type: 'interest',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'pathway',
+          type: 'pathway',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'short-course',
+          type: 'short-course',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+        {
+          title: 'Bachelor of Science (Extended)',
+          category: 'honours',
+          type: 'honours',
+          link: '/find/courses/undergraduate/bachelor-of-science-extended',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ components/cards/stories/Course/card-course-list.md
@@ -0,0 +1,30 @@
+## Card Course List
+
+### How this works
+
+The component `Card Course List` receive a prop called `items`
+
+Eg:
+
+```html
+<card-course-list items="object"></card-course-list>
+```
+
+- Items object structure  example:
+
+```js
+[
+  {
+    title: 'Bachelor of Design',
+    category: 'bachelor',
+    type: 'bachelor',
+    link: '/find/courses/undergraduate/bachelor-of-design',
+  }
+]
+```
+
+#### Colours for the course types available
+
+The colours for each card is set by the attribute type.
+
+- type: `bachelor`, `degree`, `certificate`, `coursework`, `research`, `major`, `minor`, `phd`, `specialisation`, `interest`, `pathway`, `short-course`, `honours`.
