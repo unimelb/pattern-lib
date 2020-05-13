7e305cf06a5aea3f366b48734a5d8bd37bb6a227
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-05-13 16:42:43 +1000

    feat(components): button 2 icons + card campaign + banner campaign

---
+++ components/buttons/stories/StoryButtonCampaignSecondary.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ButtonTwoIcons
+      text="Button 2 icons example"
+      href="www.unimelb.edu.au"
+      right-icon="chevron-left"
+      left-icon="chevron-right"
+      color="teal" />
+    <ButtonTwoIcons
+      text="Button 2 icons example"
+      href="www.unimelb.edu.au"
+      right-icon="chevron-left"
+      left-icon="info"
+      color="orange" />
+    <ButtonTwoIcons
+      text="Button 2 icons example"
+      href="www.unimelb.edu.au"
+      right-icon="chevron-left"
+      left-icon="location"
+      color="green" />
+    <ButtonTwoIcons
+      href="www.google.com"
+      left-icon="chevron-right"
+      right-icon="chevron-left"
+      disabled />
+  </div>
+</template>
+
+<script>
+import ButtonTwoIcons from '../ButtonTwoIcons.vue';
+
+export default {
+  components: {
+    ButtonTwoIcons,
+  },
+  readme: { source: true, html: true },
+};
+</script>
