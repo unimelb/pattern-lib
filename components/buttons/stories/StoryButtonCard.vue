baa4932708a14632295d2eccfa2716cfa7629c2c
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-07-30 13:05:18 +1000

    feat(component): button card

---
+++ components/buttons/stories/StoryButtonCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <SectionWrap>
+    <div class="grid grid--2col">
+      <ListItem>
+        <h1>BG white</h1>
+        <ButtonCard
+          top-label="29th May"
+          label="Something to go here and here about the event"
+          link="www.unimelb.edu.au" />
+      </ListItem>
+      <ListItem>
+        <h1>BG inverted</h1>
+        <ButtonCard
+          inverted
+          label="Excepteur velit proident mollit proident excepteur."
+          link="www.unimelb.edu.au" />
+      </ListItem>
+    </div>
+  </SectionWrap>
+</template>
+
+<script>
+import ButtonCard from '../ButtonCard.vue';
+
+export default {
+  components: {
+    ButtonCard,
+  },
+  readme: { source: true, html: true },
+};
+</script>
