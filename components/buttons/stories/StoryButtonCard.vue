193de4b7453221836353d8ed3ed11092984bc545
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-07-31 11:12:01 +1000

    added hover state, margins and story examples

--- components/buttons/stories/StoryButtonCard.vue
+++ components/buttons/stories/StoryButtonCard.vue
@@ -2,15 +2,27 @@
   <SectionWrap bg-whte>
     <h1>Examples</h1>
     <h2>BG white</h2>
-    <ButtonCard
-      top-label="29th May"
-      label="Something to go here and here about the event here and here about the event"
-      link="www.unimelb.edu.au" />
+    <div class="story-example-css">
+      <ButtonCard
+        top-label="29th May"
+        label="Something to go here and here about the event here and here about the event"
+        link="https://www.unimelb.edu.au" />
+      <ButtonCard
+        label="Something to go here and here about the event here and here about the event"
+        link="https://www.unimelb.edu.au" />
+    </div>
     <h2>BG inverted</h2>
-    <ButtonCard
-      inverted
-      label="Emollit proident excepteur proident excepteur."
-      link="www.unimelb.edu.au" />
+    <div class="story-example-css">
+      <ButtonCard
+        inverted
+        label="Emollit proident excepteur proident excepteur."
+        link="https://www.unimelb.edu.au" />
+      <ButtonCard
+        inverted
+        top-label="29th May"
+        label="Emollit proident excepteur proident excepteur."
+        link="https://www.unimelb.edu.au" />
+    </div>
   </SectionWrap>
 </template>
 
@@ -24,3 +36,14 @@ export default {
   readme: { source: true, html: true },
 };
 </script>
+
+<style>
+.story-example-css {
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 481px) {
+    flex-direction: row;
+  }
+}
+</style>
