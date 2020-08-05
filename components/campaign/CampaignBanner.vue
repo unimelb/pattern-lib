139c87f926a402b8fed5acb6309a96554d7e151b
dev <dev@localhost>
2020-08-05 14:55:59 +0300

    campaign banner

---
+++ components/campaign/CampaignBanner.vue
@@ -0,0 +1,67 @@
+<template>
+  <section
+    class="campaign-banner"
+    :style="{}">
+    <img
+      :src="imgSrc"
+      :alt="backgroundAltText">
+    <div class="campaign-banner__container">
+      <h1>redefine<br>possible</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur</p>
+      <ButtonIcon
+        class="btn--secondary"
+        :href="href"
+        size="sml">
+        LEARN MORE
+      </ButtonIcon>
+    </div>
+  </section>
+</template>
+
+<script>
+import ButtonIcon from '../buttons/ButtonIcon.vue';
+
+export default {
+  components: { ButtonIcon },
+  props: {
+    background: {
+      type: Object,
+      required: true,
+    },
+    backgroundAltText: {
+      type: String,
+      default: 'banner background',
+    },
+    href: {
+      type: String,
+      default: '#',
+    },
+  },
+  data: () => ({
+    imgSrc: '',
+    breakpoints: {
+      wide: 768,
+      narrow: 480,
+    },
+  }),
+  mounted() {
+    this.resolveProperBackground();
+  },
+  methods: {
+    resolveProperBackground() {
+      const width = document.documentElement.clientWidth;
+
+      switch (true) {
+        case width <= this.breakpoints.narrow:
+          this.imgSrc = this.background.small;
+          break;
+        case width > this.breakpoints.narrow && width <= this.breakpoints.wide:
+          this.imgSrc = this.background.medium;
+          break;
+        default:
+          this.imgSrc = this.background.large;
+      }
+    },
+  },
+};
+</script>
--- components/campaign/stories/index.js
+++ components/campaign/stories/index.js
@@ -8,8 +8,10 @@ import {
 import Default from './Default.vue';
 import DarkBg from './DarkBg.vue';
 import BannerButtons from './BannerButtons.vue';
+import CampaignBanner2021 from './CampaignBanner2021';
 
 storiesOf('Campaign', module)
   .add('Default', createStory(Default))
   .add('Dark Background', createStory(DarkBg))
-  .add('Banner with 3 buttons / card', createStory(BannerButtons));
+  .add('Banner with 3 buttons / card', createStory(BannerButtons))
+  .add('Campaign Banner 2021', createStory(CampaignBanner2021));
