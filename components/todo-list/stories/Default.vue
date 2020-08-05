e5df00a89e41109bf27ec46e3289bc8ad3510b73
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-08-05 14:20:15 +1000

    fix multiple stories and its components css / responsiviness

--- components/todo-list/stories/Default.vue
+++ components/todo-list/stories/Default.vue
@@ -20,6 +20,30 @@
                             link: 'https://www.unimelb.edu.au',
                             inverted: true,
                           }]" />
+    <TodoList
+      img="http://lorempixel.com/640/480/people"
+      aria-label="Duis nulla id sint exercitation sint nisi reprehenderit nulla nulla enim consequat id velit aliqua."
+      heading="Pariatur cupidatat incididunt."
+      description="In mollit adipisicing commodo tempor proident dolore consectetur in culpa. Non esse reprehenderit et occaecat proident nisi. Do nulla elit cupidatat esse non in nulla."
+      :button-card-data="[{
+                            label: 'Velit quis non anim deserunt anim aliqua enim.',
+                            link: 'https://www.unimelb.edu.au',
+                          },
+                          {
+                            label: 'Velit quis non anim deserunt anim aliqua enim.',
+                            link: 'https://www.unimelb.edu.au',
+                            inverted: true,
+                          }]" />
+    <TodoList
+      img="http://lorempixel.com/640/480/people"
+      aria-label="Duis nulla id sint exercitation sint nisi reprehenderit nulla nulla enim consequat id velit aliqua."
+      heading="Pariatur cupidatat incididunt."
+      description="In mollit adipisicing commodo tempor proident dolore consectetur in culpa. Non esse reprehenderit et occaecat proident nisi. Do nulla elit cupidatat esse non in nulla."
+      :button-card-data="[{
+        topLabel: '29th May',
+        label: 'Velit quis non anim deserunt anim aliqua enim.',
+        link: 'https://www.unimelb.edu.au',
+      }]" />
   </div>
 </template>
 
