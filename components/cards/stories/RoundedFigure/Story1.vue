41d8794715c1fd19af909ff45306d7c182f69b57
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-08-05 16:39:32 +1000

    fix css and remove fixed width

--- components/cards/stories/RoundedFigure/Story1.vue
+++ components/cards/stories/RoundedFigure/Story1.vue
@@ -1,35 +1,60 @@
 <template>
-  <SectionWrap>
-    <div class="grid grid--3col">
-      <ListItem>
-        <CardRoundedFigure
-          thumb="https://randomuser.me/api/portraits/lego/0.jpg"
-          aria-label="Provide aria label for the thumb"
-          title="Support"
-          text="Still have question?"
-          button-label="Open Day Q&A"
-          href="#" />
-      </ListItem>
-      <ListItem>
-        <CardRoundedFigure
-          thumb="https://randomuser.me/api/portraits/lego/3.jpg"
-          aria-label="Provide aria label for the thumb"
-          title="Support"
-          text="Still have question?"
-          button-label="Open Day Q&A"
-          href="#" />
-      </ListItem>
-      <ListItem>
-        <CardRoundedFigure
-          thumb="https://randomuser.me/api/portraits/lego/2.jpg"
-          aria-label="Provide aria label for the thumb"
-          title="Support"
-          text="Still have question?"
-          button-label="Open Day Q&A"
-          href="#" />
-      </ListItem>
-    </div>
-  </SectionWrap>
+  <div class="grid grid--6col">
+    <ListItem>
+      <CardRoundedFigure
+        thumb="https://randomuser.me/api/portraits/lego/0.jpg"
+        aria-label="Provide aria label for the thumb"
+        title="Support"
+        text="Still have question?"
+        button-label="Open Day Q&A"
+        href="#" />
+    </ListItem>
+    <ListItem>
+      <CardRoundedFigure
+        thumb="https://randomuser.me/api/portraits/lego/3.jpg"
+        aria-label="Provide aria label for the thumb"
+        title="Support"
+        text="Still have question?"
+        button-label="Open Day Q&A"
+        href="#" />
+    </ListItem>
+    <ListItem>
+      <CardRoundedFigure
+        thumb="https://randomuser.me/api/portraits/lego/2.jpg"
+        aria-label="Provide aria label for the thumb"
+        title="Support"
+        text="Still have question?"
+        button-label="Open Day Q&A"
+        href="#" />
+    </ListItem>
+    <ListItem>
+      <CardRoundedFigure
+        thumb="https://randomuser.me/api/portraits/lego/0.jpg"
+        aria-label="Provide aria label for the thumb"
+        title="Support"
+        text="Still have question?"
+        button-label="Open Day Q&A"
+        href="#" />
+    </ListItem>
+    <ListItem>
+      <CardRoundedFigure
+        thumb="https://randomuser.me/api/portraits/lego/3.jpg"
+        aria-label="Provide aria label for the thumb"
+        title="Support"
+        text="Still have question?"
+        button-label="Open Day Q&A"
+        href="#" />
+    </ListItem>
+    <ListItem>
+      <CardRoundedFigure
+        thumb="https://randomuser.me/api/portraits/lego/2.jpg"
+        aria-label="Provide aria label for the thumb"
+        title="Support"
+        text="Still have question?"
+        button-label="Open Day Q&A"
+        href="#" />
+    </ListItem>
+  </div>
 </template>
 
 <script>
