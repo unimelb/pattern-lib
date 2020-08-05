715099467159c12ea16c37453f82a5d0127c2bea
dev <dev@localhost>
2020-08-05 14:55:59 +0300

    campaign banner

---
+++ components/campaign/stories/CampaignBanner2021.vue
@@ -0,0 +1,23 @@
+<template>
+  <CampaignBanner
+    :background="backgroundImages"
+    background-alt-text="woman diver" />
+</template>
+
+<script>
+import CampaignBanner from '../CampaignBanner.vue';
+import largeBackground from './static/1440w_campaign_banner.jpg';
+import mediumBackground from './static/768w_campaign_banner.jpg';
+import smallBackground from './static/320w_campaign_banner.jpg';
+
+export default {
+  components: { CampaignBanner },
+  data: () => ({
+    backgroundImages: {
+      large: largeBackground,
+      medium: mediumBackground,
+      small: smallBackground,
+    },
+  }),
+};
+</script>
