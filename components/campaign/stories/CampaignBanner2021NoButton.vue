a00d398b353c4bf953bbf7634b95a3113fc9fa24
dev <dev@localhost>
2020-08-07 10:31:46 +0300

    add readme

--- components/campaign/stories/CampaignBanner2021NoButton.vue
+++ components/campaign/stories/CampaignBanner2021NoButton.vue
@@ -4,18 +4,20 @@
     :background="backgroundImages"
     background-alt-text="woman diver"
     href="#main"
-    text="Lorem ipsum dolor sit amet, consectetur" />
+    text="Lorem ipsum dolor sit amet, consectetur"
+    button-text="learn more" />
 </template>
 
 <script>
 import CampaignBanner from '../CampaignBanner.vue';
+import readme from './campaign-banner.md';
 import largeBackground from './static/1440w_campaign_banner.jpg';
 import mediumBackground from './static/768w_campaign_banner.jpg';
 import smallBackground from './static/320w_campaign_banner.jpg';
 
 export default {
   components: { CampaignBanner },
-  readme: { source: true, html: true },
+  readme: { custom: readme, source: true, html: false },
   data: () => ({
     backgroundImages: {
       large: largeBackground,
