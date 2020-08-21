626429b31930d15c27fc23f0f3a9f6ba9a3183bd
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-08-21 14:41:43 +1000

    chore(component): card image link

---
+++ components/cards/CardImageLink.vue
@@ -0,0 +1,47 @@
+<template>
+  <a
+    :href="href"
+    class="card-image-link">
+    <div class="card-image-link__wrapper">
+      <div class="card-image-link__image-wrapper">
+        <div
+          role="img"
+          :style="{ backgroundImage: `url(${thumb})` }"
+          :aria-label="ariaLabel"
+          class="card-image-link__image" />
+      </div>
+      <div class="card-image-link__inner">
+        <h3 class="card-image-link__heading">
+          {{ heading }}
+        </h3>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+import { IMAGE_PLACEHOLDER_BIG } from '../../utils/placeholders';
+
+export default {
+  name: 'CardLink',
+  props: {
+    thumb: {
+      type: String,
+      default: IMAGE_PLACEHOLDER_BIG,
+    },
+    ariaLabel: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    heading: {
+      type: String,
+      default: '',
+    },
+    href: {
+      type: String,
+      default: '#',
+    },
+  },
+};
+</script>
