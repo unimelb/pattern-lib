53513b2999ab14ed3579d1cd3122bde481c67c0d
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-08-24 14:25:07 +1000

    fix section two col test

--- components/section/__tests__/SectionTwoCol.test.js
+++ components/section/__tests__/SectionTwoCol.test.js
@@ -1,5 +1,5 @@
 import {
-  shallow,
+  mount,
 } from 'vue-test-utils';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import SectionTwoCol from '../SectionTwoCol.vue';
@@ -8,12 +8,12 @@ expect.extend(toHaveNoViolations);
 
 describe('SectionTwoCol', () => {
   it('should match snapshot', () => {
-    const result = shallow(SectionTwoCol).element;
+    const result = mount(SectionTwoCol).element;
     expect(result).toMatchSnapshot();
   });
 
   it('Component throws no accessibility violations', (done) => {
-    const html = shallow(SectionTwoCol).html();
+    const html = mount(SectionTwoCol).html();
     // pass anything that outputs html to axe
     return axe(html).then((response) => {
       expect(response).toHaveNoViolations();
