26de74563896e71916f4512f652a3e987ab68752
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-08-24 14:36:15 +1000

    fix page search test

--- components/search/__tests__/PageSearch.test.js
+++ components/search/__tests__/PageSearch.test.js
@@ -1,5 +1,5 @@
 import {
-  shallow,
+  mount,
 } from 'vue-test-utils';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import PageSearch from '../PageSearch.vue';
@@ -7,13 +7,13 @@ import PageSearch from '../PageSearch.vue';
 expect.extend(toHaveNoViolations);
 
 describe('PageSearch', () => {
-  it.skip('should match snapshot', () => {
-    const result = shallow(PageSearch).element;
+  it('should match snapshot', () => {
+    const result = mount(PageSearch).element;
     expect(result).toMatchSnapshot();
   });
 
-  it.skip('Component throws no accessibility violations', (done) => {
-    const html = shallow(PageSearch).html();
+  it('Component throws no accessibility violations', (done) => {
+    const html = mount(PageSearch).html();
     // pass anything that outputs html to axe
     return axe(html).then((response) => {
       expect(response).toHaveNoViolations();
