4a0624928ec59491544b196675cf335ddd44ff82
Guilherme Epifanio <gui.epifanio@gmail.com>
2020-09-08 14:07:00 +1000

    fix unit test

--- components/section/__tests__/SplitSectionQuote.test.js
+++ components/section/__tests__/SplitSectionQuote.test.js
@@ -1,5 +1,5 @@
 import {
-  shallow,
+  mount,
 } from 'vue-test-utils';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import SplitSectionQuote from '../SplitSectionQuote.vue';
@@ -8,12 +8,12 @@ expect.extend(toHaveNoViolations);
 
 describe('SplitSectionQuote', () => {
   it('should match snapshot', () => {
-    const result = shallow(SplitSectionQuote).element;
+    const result = mount(SplitSectionQuote).element;
     expect(result).toMatchSnapshot();
   });
 
   it('Component throws no accessibility violations', (done) => {
-    const html = shallow(SplitSectionQuote).html();
+    const html = mount(SplitSectionQuote).html();
     // pass anything that outputs html to axe
     return axe(html).then((response) => {
       expect(response).toHaveNoViolations();
