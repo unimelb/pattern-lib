27cbd64b874cbc3257c5e3d054bf1a92fa3f3014
Guilherme Epifanio <gui.epifanio@gmail.com>
2021-02-16 14:55:24 +1100

    feat(component): modal button with video

--- components/modal/ModalDialog.vue
+++ components/modal/ModalDialog.vue
@@ -14,6 +14,7 @@
     <div
       :id="`modal-dialog-1${_uid}`"
       ref="container"
+      :class="isVideo ? 'modal-dialog--video' : ''"
       class="modal-dialog"
       aria-hidden="true"
       @click="closeContainer"
@@ -23,6 +24,7 @@
         ref="modal"
         :aria-labelledby="`modal-dialog-title-1${_uid}`"
         class="modal-dialog__modal"
+        :class="isVideo ? 'modal-dialog__modal--video' : ''"
         role="dialog">
         <div role="document">
           <!-- eslint-disable vue/no-v-html -->
@@ -34,6 +36,7 @@
           <br>
           <button
             class="modal-dialog__close"
+            :class="isVideo ? 'modal-dialog__close--video' : ''"
             aria-label="Close Dialog"
             type="button"
             @click="closeDialog">
@@ -51,6 +54,7 @@
 // modal-dialog-close-container
 
 import focusableElements from '../../utils/focusable-elements';
+import pauseVideo from '../../helpers/pauseVideo';
 
 export default {
   name: 'ModalDialog',
@@ -63,6 +67,10 @@ export default {
       type: String,
       default: '',
       required: true,
+    },
+    isVideo: {
+      type: Boolean,
+      default: false,
     },
   },
   data() {
@@ -102,6 +110,7 @@ export default {
       this.$emit('modal-dialog-open-dialog');
     },
     closeDialog() {
+      pauseVideo();
       const { container, modal } = this.$refs;
 
       // Hide container
