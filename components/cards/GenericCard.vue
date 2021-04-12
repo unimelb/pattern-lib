c202dd338fb4f38c8ee1ba238c3ec8dc1f1d5c7f
Dmitry Yarmachok <dmitry.yarmachok@uinno.io>
2021-04-12 20:42:47 +0300

    fix accessibility GenericCard

--- components/cards/GenericCard.vue
+++ components/cards/GenericCard.vue
@@ -6,6 +6,7 @@
       :style="{ backgroundImage: `url(${thumb})` }"
       :aria-label="'Image for' + title"
       :class="[cols === 1 ? 'card__thumb--full-height' : '']"
+      :title="title"
       class="card__thumb" />
     <div class="card__inner">
       <h3 class="card__header">
