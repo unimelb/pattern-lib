d76b761516d0a108f801b800dd5b6faf734b97a4
Lucas Mounsey <lucas.mounsey@august.com.au>
2021-05-04 15:17:53 +1000

    Move component to `embed` folder, other fixes

--- components/embed/stories/VideoPortrait.vue
+++ components/embed/stories/VideoPortrait.vue
@@ -3,10 +3,10 @@
     :video="{
       url: 'https://www.youtube.com/embed/MH5pJ4jokpI',
       duration: 28,
-      preview: 'https://player.vimeo.com/external/336863136.sd.mp4?s=234cbb2adf6f8c523093bc0627e7d8bdb697db94&amp;profile_id=165',
+      preview: 'https://study.unimelb.edu.au/__data/assets/file/0022/265405/Byrd_Video.mp4',
     }"
     :poster="{
-      src: 'https://www.brown.edu/sites/g/files/dprerj316/files/styles/portrait_sfts_desktop/public/Byrd_Advice.jpg?h=30f5acd3&itok=hcyHpFiN',
+      src: 'https://study.unimelb.edu.au/__data/assets/image/0021/265404/Byrd_Advice.jpg',
       alt: '',
     }"
     orientation="portrait"
--- components/video/index.js
+++
@@ -1,5 +0,0 @@
-import Video from './Video.vue';
-import VideoFullWidth from './VideoFullWidth.vue';
-
-export { Video, VideoFullWidth };
-export default Video;
