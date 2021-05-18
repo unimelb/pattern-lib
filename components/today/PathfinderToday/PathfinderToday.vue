e4b6ec87cd451e8ce0fd0af9e3da9401a70c2dfe
Guilherme Epifanio <gui.epifanio@gmail.com>
2021-05-18 11:06:03 +1000

    fix tests

--- components/today/PathfinderToday/PathfinderToday.vue
+++ components/today/PathfinderToday/PathfinderToday.vue
@@ -6,32 +6,33 @@
     }">
     <div class="max">
       <slot name="before" />
-      <ul
-        role="navigation"
-        class="pathfinder-today__list grid grid--lg">
-        <li
-          v-for="(item, index) in items"
-          :key="index"
-          class="pathfinder-today__list-item cell cell--lg cell--tab-1of2 cell--wide-1of4">
-          <div
-            class="pathfinder-today__link">
-            <a
-              :id="item.id"
-              :href="item.href">
-              <span
-                class="pathfinder-today__link-title heading-sm heading-sm--link">{{ item.title }}</span>
-              <span class="pathfinder-today__link-icon">
-                <SvgIcon
-                  name="arrow-right"
-                  aria-hidden="true" />
-              </span>
-            </a>
-            <span class="pathfinder-today__link-description label-s">{{
-              item.description
-            }}</span>
-          </div>
-        </li>
-      </ul>
+      <div role="navigation">
+        <ul
+          class="pathfinder-today__list grid grid--lg">
+          <li
+            v-for="(item, index) in items"
+            :key="index"
+            class="pathfinder-today__list-item cell cell--lg cell--tab-1of2 cell--wide-1of4">
+            <div
+              class="pathfinder-today__link">
+              <a
+                :id="item.id"
+                :href="item.href">
+                <span
+                  class="pathfinder-today__link-title heading-sm heading-sm--link">{{ item.title }}</span>
+                <span class="pathfinder-today__link-icon">
+                  <SvgIcon
+                    name="arrow-right"
+                    aria-hidden="true" />
+                </span>
+              </a>
+              <span class="pathfinder-today__link-description label-s">{{
+                item.description
+              }}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
       <slot name="after" />
     </div>
   </div>
