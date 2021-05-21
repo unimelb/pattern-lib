2ad9fe46317e2399f769b1126d9b7a0a82fa5250
Lucas Mounsey <lucas_mounsey@live.com>
2021-05-21 12:29:29 +1000

    Update nav links

--- components/megamenu/MegaMenuAlt.vue
+++ components/megamenu/MegaMenuAlt.vue
@@ -126,13 +126,14 @@ export default {
         { text: 'Home', href: '/home-page' },
         { text: 'Study at Melbourne', href: 'https://study.unimelb.edu.au/' },
         { text: 'Research and innovation', href: 'https://research.unimelb.edu.au/' },
-        { text: 'About us', href: 'https://about.unimelb.edu.au/' },
+        { text: 'Safety and respect', href: 'https://students.unimelb.edu.au/campus-life/policy-and-conduct/respect-at-the-university' },
         { text: 'Virtual Campus', href: 'https://study.unimelb.edu.au/discover/virtual-tour' },
       ],
       secondaryLinks: [
         { text: 'Current students', href: 'https://students.unimelb.edu.au/' },
         { text: 'Staff', href: 'https://staff.unimelb.edu.au/' },
         { text: 'Library', href: 'https://library.unimelb.edu.au/' },
+        { text: 'Alumni', href: 'https://www.unimelb.edu.au/alumni' },
       ],
     };
   },
