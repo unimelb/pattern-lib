98e044a9a4a53fa0ce4daf61348b491a15fc20d0
Guilherme Epifanio <gui.epifanio@gmail.com>
2021-05-21 16:54:03 +1000

    feat(component): page header home

---
+++ components/today/stories/Header/PageHeaderHome.vue
@@ -0,0 +1,33 @@
+<template>
+  <PageHeaderHome
+    title="Effective Leadership Communication"
+    introduction="Become an authentic, persuasive and engaging communicator to enhance your effectiveness as a leader."
+    :img="{ src: placeholder, alt: 'Business leaders at a conference' }"
+    :bg-src="placeholderBg" />
+</template>
+
+<script>
+import PageHeaderHome from 'components/header/PageHeaderHome.vue';
+
+import placeholderBg from './static/hero-bg.jpg';
+import placeholder from './static/hero-feature.png';
+
+import Docs from './page-header-home.md';
+
+export default {
+  components: {
+    PageHeaderHome,
+  },
+  readme: {
+    custom: Docs,
+    source: true,
+    html: false,
+  },
+  data() {
+    return {
+      placeholder,
+      placeholderBg,
+    };
+  },
+};
+</script>
---
+++ components/today/stories/Header/page-header-home.md
@@ -0,0 +1,6 @@
+
+## Page header home
+
+```html
+<page-header-home title="Effective Leadership Communication" introduction="Become an authentic, persuasive and engaging communicator to enhance your effectiveness as a leader." :img="{ src: 'link to the image', alt: 'Business leaders at a conference' }" bg-src="link to the image"></page-header-home>
+```
