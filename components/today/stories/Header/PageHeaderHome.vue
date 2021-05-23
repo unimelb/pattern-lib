0880f7ab02cb2c93709bd381cf843d005be68797
Guilherme Epifanio <gui.epifanio@gmail.com>
2021-05-24 09:06:29 +1000

    update docs

--- components/today/stories/Header/PageHeaderHome.vue
+++ components/today/stories/Header/PageHeaderHome.vue
@@ -3,11 +3,18 @@
     title="Effective Leadership Communication"
     introduction="Become an authentic, persuasive and engaging communicator to enhance your effectiveness as a leader."
     :img="{ src: placeholder, alt: 'Business leaders at a conference' }"
-    :bg-src="placeholderBg" />
+    :bg-src="placeholderBg">
+    <PathfinderToday
+      slot="after"
+      inline
+      dark
+      :items="pathfinderLinks" />
+  </PageHeaderHome>
 </template>
 
 <script>
 import PageHeaderHome from 'components/header/PageHeaderHome.vue';
+import PathfinderToday from 'components/today/PathfinderToday/PathfinderToday.vue';
 
 import placeholderBg from './static/hero-bg.jpg';
 import placeholder from './static/hero-feature.png';
@@ -16,7 +23,7 @@ import Docs from './page-header-home.md';
 
 export default {
   components: {
-    PageHeaderHome,
+    PageHeaderHome, PathfinderToday,
   },
   readme: {
     custom: Docs,
@@ -27,6 +34,28 @@ export default {
     return {
       placeholder,
       placeholderBg,
+      pathfinderLinks: [
+        {
+          title: 'Undergraduate courses',
+          description: 'Flexible first degrees that you can tailor to your career goals, or explore new possibilities.',
+          href: '/landing-page/',
+        },
+        {
+          title: 'Graduate courses',
+          description: 'Study in small classes led by experts alongside others who share your interests and goals.',
+          href: '/landing-page/',
+        },
+        {
+          title: 'Graduate research',
+          description: 'Access dedicated research centres, advanced  facilities, generous scholarships and more.',
+          href: '/landing-page/',
+        },
+        {
+          title: 'Short courses and microcredentials',
+          description: 'Upskill, expand your knowledge and take the next step in your career with focused courses.',
+          href: '/landing-page/',
+        },
+      ],
     };
   },
 };
--- components/today/stories/Header/page-header-home.md
+++ components/today/stories/Header/page-header-home.md
@@ -2,5 +2,37 @@
 ## Page header home
 
 ```html
-<page-header-home title="Effective Leadership Communication" introduction="Become an authentic, persuasive and engaging communicator to enhance your effectiveness as a leader." :img="{ src: 'link to the image', alt: 'Business leaders at a conference' }" bg-src="link to the image"></page-header-home>
+<page-header-home 
+  title="Effective Leadership Communication" 
+  introduction="Become an authentic, persuasive and engaging communicator to enhance your effectiveness as a leader." 
+  :img="{ src: 'link to the image', alt: 'Business leaders at a conference' }" 
+  bg-src="link to the image">
+    <pathfinder-today
+    slot="after"
+    inline
+    dark
+    :items="[
+          {
+            title: 'Undergraduate courses',
+            description: 'Flexible first degrees that you can tailor to your career goals, or explore new possibilities.',
+            href: '/landing-page/',
+          },
+          {
+            title: 'Graduate courses',
+            description: 'Study in small classes led by experts alongside others who share your interests and goals.',
+            href: '/landing-page/',
+          },
+          {
+            title: 'Graduate research',
+            description: 'Access dedicated research centres, advanced  facilities, generous scholarships and more.',
+            href: '/landing-page/',
+          },
+          {
+            title: 'Short courses and microcredentials',
+            description: 'Upskill, expand your knowledge and take the next step in your career with focused courses.',
+            href: '/landing-page/',
+          },
+        ]">
+    </pathfinder-today>
+</page-header-home>
 ```
