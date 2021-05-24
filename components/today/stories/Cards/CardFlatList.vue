7e7359ae657bfc7df25cef5189892fe8819c2637
Guilherme Epifanio <gui.epifanio@gmail.com>
2021-05-24 13:39:05 +1000

    update images

--- components/today/stories/Cards/CardFlatList.vue
+++ components/today/stories/Cards/CardFlatList.vue
@@ -22,19 +22,19 @@ export default {
           title: 'Research facilities',
           description: 'Our bold new engineering and design campus at Fishermans Bend, where students and researchers collaborate with industry to tackle real world challenges.',
           cta: { href: 'https://www.unimelb.edu.au/', text: 'More about labs and workshops' },
-          img: { src: 'https://picsum.photos/seed/1/540/360', alt: 'Image for example purposes.' },
+          img: { src: 'https://picsum.photos/seed/2/540/360', alt: 'Image for example purposes.' },
         },
         {
           title: 'Libraries, museums, galleries and collections',
           description: 'Access leading cultural collections and workspaces through our network of libraries and museums, as well as qualified library staff.',
           cta: { href: 'https://www.unimelb.edu.au/', text: 'Explore our cultural facilities' },
-          img: { src: 'https://picsum.photos/seed/1/540/360', alt: 'Image for example purposes.' },
+          img: { src: 'https://picsum.photos/seed/3/540/360', alt: 'Image for example purposes.' },
         },
         {
           title: 'Accommodation',
           description: 'There is a range of student accommodation  available to you, including University owned and partnered accommodation and traditional residential colleges.',
           cta: { href: 'https://www.unimelb.edu.au/', text: 'Discover accommodation' },
-          img: { src: 'https://picsum.photos/seed/1/540/360', alt: 'Image for example purposes.' },
+          img: { src: 'https://picsum.photos/seed/4/540/360', alt: 'Image for example purposes.' },
         },
       ],
     };
