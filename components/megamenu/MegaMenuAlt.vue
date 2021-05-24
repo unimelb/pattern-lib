74e25e99c84269c5daf692050776468d94bcf127
Guilherme Epifanio <gui.epifanio@gmail.com>
2021-05-24 10:29:21 +1000

    update content and make it flexible

--- components/megamenu/MegaMenuAlt.vue
+++ components/megamenu/MegaMenuAlt.vue
@@ -118,33 +118,27 @@ export default {
     PageSearchForm,
     Trap,
   },
+  props: {
+    primaryLinks: {
+      type: Array,
+      required: false,
+      default() {
+        return [];
+      },
+    },
+    secondaryLinks: {
+      type: Array,
+      required: false,
+      default() {
+        return [];
+      },
+    },
+  },
   data() {
     return {
       isMobileOpen: false,
       isSearchOpen: false,
-      primaryLinks: [
-        { text: 'Home', href: '/home-page' },
-        { text: 'Study at Melbourne', href: 'https://study.unimelb.edu.au/' },
-        { text: 'Research and innovation', href: 'https://research.unimelb.edu.au/' },
-        { text: 'Safety and respect', href: 'https://students.unimelb.edu.au/campus-life/policy-and-conduct/respect-at-the-university' },
-        { text: 'Virtual Campus', href: 'https://study.unimelb.edu.au/discover/virtual-tour' },
-      ],
-      secondaryLinks: [
-        { text: 'Current students', href: 'https://students.unimelb.edu.au/' },
-        { text: 'Staff', href: 'https://staff.unimelb.edu.au/' },
-        { text: 'Library', href: 'https://library.unimelb.edu.au/' },
-        { text: 'Alumni', href: 'https://www.unimelb.edu.au/alumni' },
-      ],
     };
-  },
-  computed: {
-    mobileMenuLinks() {
-      return [
-        ...this.primaryLinks,
-        ...this.secondaryLinks,
-        { text: 'Contact', href: 'https://www.unimelb.edu.au/contact' },
-      ];
-    },
   },
   methods: {
     openMobileMenu() {
--- components/megamenu/stories/mega-menu-alt.md
+++ components/megamenu/stories/mega-menu-alt.md
@@ -1,32 +1,20 @@
-<template>
-  <MegaMenuAlt
-    :primary-links="primaryLinks"
-    :secondary-links="secondaryLinks" />
-</template>
+## Mega Menu Alt
 
-<script>
-import MegaMenuAlt from '../MegaMenuAlt.vue';
-import MegaMenuAltDocs from './mega-menu-alt.md';
 
-export default {
-  components: { MegaMenuAlt },
-  readme: { custom: MegaMenuAltDocs, source: true, html: false },
-  data() {
-    return {
-      primaryLinks: [
+```html
+  <mega-menu-alt
+    :primary-links="[
         { text: 'Home', href: '/home-page' },
         { text: 'Study at Melbourne', href: 'https://study.unimelb.edu.au/' },
         { text: 'Research and innovation', href: 'https://research.unimelb.edu.au/' },
         { text: 'Safety and respect', href: 'https://students.unimelb.edu.au/campus-life/policy-and-conduct/respect-at-the-university' },
         { text: 'Virtual Campus', href: 'https://study.unimelb.edu.au/discover/virtual-tour' },
-      ],
-      secondaryLinks: [
+      ]"
+    :secondary-links="[
         { text: 'Current students', href: 'https://students.unimelb.edu.au/' },
         { text: 'Staff', href: 'https://staff.unimelb.edu.au/' },
         { text: 'Library', href: 'https://library.unimelb.edu.au/' },
         { text: 'Alumni', href: 'https://www.unimelb.edu.au/alumni' },
-      ],
-    };
-  },
-};
-</script>
+      ]">
+  </mega-menu-alt>
+```
