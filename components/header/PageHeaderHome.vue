3e00c3ecaa6b807d9fc1ff088808f687a65b24cd
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2021-05-25 11:43:35 +1000

    feat(component): page header home (#1633)
    * feat(component): page header home
    
    * update docs
    
    * update content
    
    * Remove introduction and update pathfinder component
    
    * Update links and title
    
    Co-authored-by: Lucas Mounsey <lucas.mounsey@august.com.au>

---
+++ components/header/PageHeaderHome.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page-header-home">
+    <client-only v-if="bgSrc">
+      <progressive-img
+        :src="bgSrc"
+        alt=""
+        no-ratio
+        class="page-header-home__bg" />
+    </client-only>
+    <div
+      v-if="img"
+      class="page-header-home__img">
+      <client-only>
+        <progressive-img
+          :src="img.src"
+          :alt="img.alt || ''"
+          no-ratio
+          class="page-header-home__img-inner" />
+      </client-only>
+    </div>
+    <div class="max page-header-home__inner">
+      <div class="page-header-home__content">
+        <div class="page-header-home__content-inner">
+          <!-- eslint-disable vue/no-v-html -->
+          <h1
+            class="heading-xxl page-header-home__title"
+            v-html="title" />
+          <!-- eslint-enable vue/no-v-html -->
+          <slot />
+          <div
+            v-if="$slots.actions"
+            class="page-header-home__actions">
+            <slot name="actions" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="$slots.after"
+      class="page-header-home__after">
+      <slot name="after" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    img: {
+      type: Object,
+      required: false,
+      default() {
+        return null;
+      },
+    },
+    bgSrc: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+};
+</script>
--- components/today/stories/Header/index.js
+++ components/today/stories/Header/index.js
@@ -4,8 +4,10 @@ import { createStory } from 'storybook/utils';
 import Default from './Default.vue';
 import DefaultDark from './DefaultDark.vue';
 import Study from './Study.vue';
+import PageHeaderHome from './PageHeaderHome.vue';
 
 storiesOf('Today/Header', module)
   .add('Default', createStory(Default))
   .add('Default (Dark)', createStory(DefaultDark))
-  .add('Study', createStory(Study));
+  .add('Study', createStory(Study))
+  .add('Page Header Home', createStory(PageHeaderHome));
