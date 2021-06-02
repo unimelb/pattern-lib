dbfbb6dab6ea1cd8bf9a507c3d2372f36083909a
Guilherme Epifanio <gui.epifanio@gmail.com>
2021-06-02 13:36:18 +1000

    fix today section 3 up to have a new link and sticky to the bottom

--- components/today/stories/Section/ThreeUp.vue
+++ components/today/stories/Section/ThreeUp.vue
@@ -4,7 +4,7 @@
       Level up with Melbourne MicroCerts
     </h2>
     <div class="grid grid--lg">
-      <div class="cell cell--lg cell--tab-1of3 shim-mb0 shim-mt0 content-max-width">
+      <div class="cell cell--lg cell--tab-1of3 shim-mb0 shim-mt0 content-max-width section-alt__inner-flex-items">
         <div class="section-alt__inner-svg-icon shim-mb067">
           <SvgIcon
             name="briefcase"
@@ -14,8 +14,14 @@
           Industry-ready skills
         </h3>
         <p>Develop your leadership communication skills aligned with current industry best practice. <a href="#">Learn more about MicroCerts</a>.</p>
+        <ButtonIcon
+          href="https://study.unimelb.edu.au/"
+          class="btn--text"
+          aria-label="Learn more about MicroCerts">
+          Learn more about MicroCerts
+        </ButtonIcon>
       </div>
-      <div class="cell cell--lg cell--tab-1of3 shim-mb0 shim-mt0 content-max-width">
+      <div class="cell cell--lg cell--tab-1of3 shim-mb0 shim-mt0 content-max-width section-alt__inner-flex-items">
         <div class="section-alt__inner-svg-icon shim-mb067">
           <SvgIcon
             name="handshake"
@@ -25,8 +31,14 @@
           Taught by leading experts
         </h3>
         <p>Your teachers are internationally recognised professionals with years of on the ground experience within top organisations.</p>
+        <ButtonIcon
+          href="https://study.unimelb.edu.au/"
+          class="btn--text"
+          aria-label="Study with us">
+          Study with us
+        </ButtonIcon>
       </div>
-      <div class="cell cell--lg cell--tab-1of3 shim-mb0 shim-mt0 content-max-width">
+      <div class="cell cell--lg cell--tab-1of3 shim-mb0 shim-mt0 content-max-width section-alt__inner-flex-items">
         <div class="section-alt__inner-svg-icon shim-mb067">
           <SvgIcon
             name="check"
@@ -36,6 +48,12 @@
           Get a digital certificate
         </h3>
         <p>Receive an industry recognised digital certificate to showcase in your professional networks. <a href="#">See an example certificate</a>.</p>
+        <ButtonIcon
+          href="https://study.unimelb.edu.au/"
+          class="btn--text"
+          aria-label="See an example certificate">
+          See an example certificate
+        </ButtonIcon>
       </div>
     </div>
   </SectionAlt>
